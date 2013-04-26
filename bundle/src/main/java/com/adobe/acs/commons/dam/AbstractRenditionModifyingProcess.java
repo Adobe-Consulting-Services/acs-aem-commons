a246c73f003fc79f731197955cda18384af01f51
Justin Edelson <justin@justinedelson.com>
2013-04-26 16:08:46 -0400

    init

---
+++ bundle/src/main/java/com/adobe/acs/commons/dam/AbstractRenditionModifyingProcess.java
@@ -0,0 +1,133 @@
+package com.adobe.acs.commons.dam;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.imageio.IIOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.dam.api.Asset;
+import com.day.cq.dam.api.Rendition;
+import com.day.cq.dam.commons.process.AbstractAssetWorkflowProcess;
+import com.day.cq.dam.commons.util.PrefixRenditionPicker;
+import com.day.cq.workflow.WorkflowException;
+import com.day.cq.workflow.WorkflowSession;
+import com.day.cq.workflow.exec.WorkItem;
+import com.day.cq.workflow.metadata.MetaDataMap;
+import com.day.image.Layer;
+
+/**
+ * Abstract asset workflow which performs some action on a particular rendition (which was presumably created by an earlier workflow process).
+ * 
+ * Arguments:
+ * <ul>
+ * <li>renditionName - The name of the rendition to modify.</li>
+ * </ul>
+ *
+ */
+public abstract class AbstractRenditionModifyingProcess extends AbstractAssetWorkflowProcess {
+
+    private static final String ARG_QUALITY = "quality";
+
+    private static final String ARG_RENDITION_NAME = "renditionName";
+
+    /**
+     * Logger instance for this class.
+     */
+    protected final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public final void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaData)
+            throws WorkflowException {
+        String[] args = buildArguments(metaData);
+
+        final String renditionName = getValuesFromArgs(ARG_RENDITION_NAME, args).size() > 0 ? getValuesFromArgs(
+                ARG_RENDITION_NAME, args).get(0) : null;
+
+        // image quality: from 0 t0 100%
+        final String qualityStr = getValuesFromArgs(ARG_QUALITY, args).size() > 0 ? getValuesFromArgs(ARG_QUALITY, args)
+                .get(0) : "60";
+
+        if (renditionName == null) {
+            log.warn("Rendition name was not configured in arguments. Skipping.");
+            return;
+        }
+
+        final Asset asset = getAssetFromPayload(workItem, workflowSession.getSession());
+        final Rendition rendition = asset.getRendition(new PrefixRenditionPicker(renditionName));
+
+        if (rendition == null) {
+            log.warn("Rendition name {} was not available for asset {}. Skipping.", renditionName, asset);
+            return;
+        }
+
+        Layer layer = null;
+        try {
+            layer = new Layer(rendition.getStream());
+
+            layer = processLayer(layer, rendition, workflowSession, args);
+
+            String mimetype = layer.getMimeType();
+            double quality = (mimetype.equals("image/gif")) ? getQuality(255, qualityStr) : getQuality(1.0, qualityStr);
+
+            saveImage(asset, rendition, layer, mimetype, quality);
+        } catch (IIOException e) {
+            log.warn("Unable to load image layer from " + rendition.getPath(), e);
+        } catch (IOException e) {
+            log.warn("Unable to load image layer from " + rendition.getPath(), e);
+        } finally {
+            if (layer != null) {
+                layer.dispose();
+                layer = null;
+            }
+        }
+
+    }
+
+    private String[] buildArguments(MetaDataMap metaData) {
+        // the 'old' way, ensures backward compatibility
+        String processArgs = metaData.get("PROCESS_ARGS", String.class);
+        if (processArgs != null && !processArgs.equals("")) {
+            return processArgs.split(",");
+        } else {
+            return new String[0];
+        }
+    }
+
+    private void saveImage(Asset asset, Rendition toReplace, Layer layer, String mimetype, double quality)
+            throws IOException {
+        File tmpFile = File.createTempFile(getTempFileSpecifier(), "." + getExtension(mimetype));
+        OutputStream out = FileUtils.openOutputStream(tmpFile);
+        InputStream is = null;
+        try {
+            layer.write(mimetype, quality, out);
+            is = FileUtils.openInputStream(tmpFile);
+            asset.addRendition(toReplace.getName(), is, mimetype);
+        } finally {
+            IOUtils.closeQuietly(out);
+            IOUtils.closeQuietly(is);
+            FileUtils.deleteQuietly(tmpFile);
+        }
+    }
+
+    protected String getExtension(String mimetype) {
+        return mimeTypeService.getExtension(mimetype);
+    }
+
+    protected double getQuality(double base, String qualityStr) {
+        int q = Integer.valueOf(qualityStr);
+        double res = base * q / 100;
+        return res;
+    }
+
+    protected abstract String getTempFileSpecifier();
+
+    protected abstract Layer processLayer(Layer layer, Rendition rendition, WorkflowSession workflowSession,
+            String[] args);
+}
---
+++ bundle/src/main/java/com/adobe/acs/commons/dam/impl/AddWatermarkToRenditionProcess.java
@@ -0,0 +1,92 @@
+package com.adobe.acs.commons.dam.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.IIOException;
+import javax.jcr.Session;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+
+import com.adobe.acs.commons.dam.AbstractRenditionModifyingProcess;
+import com.day.cq.dam.api.Rendition;
+import com.day.cq.workflow.WorkflowSession;
+import com.day.image.Layer;
+
+/**
+ * Workflow process which adds a watermark to a rendition. Watermarks will always be anchored in the bottom left corner.
+ * 
+ * Arguments:
+ * <ul>
+ * <li>watermark - The repository path of the watermark.</li>
+ * </ul>
+ */
+@Component(metatype = false)
+@Service
+@Property(name = "process.label", value = "Add Watermark to Rendition")
+public class AddWatermarkToRenditionProcess extends AbstractRenditionModifyingProcess {
+
+    private static final String ARG_WATERMARK = "watermark";
+
+    private static final String WATERMARK_SPECIFIER = ARG_WATERMARK;
+
+    private void addWatermark(Layer layer, Layer watermark) {
+        layer.blit(watermark, 0, layer.getHeight() - watermark.getHeight(), watermark.getWidth(),
+                watermark.getHeight(), 0, 0);
+    }
+
+    private Layer getLayer(String path, Session session) {
+        if (path != null) {
+            ResourceResolver resolver = getResourceResolver(session);
+            Resource resource = resolver.getResource(path);
+            if (resource != null) {
+                InputStream inStream = resource.adaptTo(InputStream.class);
+                if (inStream != null) {
+                    try {
+                        return new Layer(inStream);
+                    } catch (IIOException e) {
+                        log.warn("Unable to load image layer from " + path, e);
+                    } catch (IOException e) {
+                        log.warn("Unable to load image layer from " + path, e);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected String getTempFileSpecifier() {
+        return WATERMARK_SPECIFIER;
+    }
+
+    @Override
+    protected Layer processLayer(Layer layer, Rendition rendition, WorkflowSession workflowSession, String[] args) {
+        final String watermarkPath = getValuesFromArgs(ARG_WATERMARK, args).size() > 0 ? getValuesFromArgs(
+                ARG_WATERMARK, args).get(0) : null;
+
+        if (watermarkPath != null) {
+            Layer watermark = null;
+            try {
+                watermark = getLayer(watermarkPath, workflowSession.getSession());
+
+                if (watermark != null) {
+                    addWatermark(layer, watermark);
+                }
+            } finally {
+                if (watermark != null) {
+                    watermark.dispose();
+                    watermark = null;
+                }
+            }
+        } else {
+            log.info("No watermark specified. Skipping.");
+        }
+        return layer;
+    }
+
+}
