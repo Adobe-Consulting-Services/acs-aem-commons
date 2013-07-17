c1a99935dc263e1126eb06ef6155d3135fdaf7f6
David Gonzalez <dgonzale@adobe.com>
2013-07-16 23:49:02 -0400

    QueryBuilder-based ContentFinder View Handler; allows customs content finder tabs to be created that leverage QB instead of GQL for defining queries

---
+++ bundle/src/main/java/com/adobe/acs/commons/contentfinder/querybuilder/impl/viewhandler/QueryBuilderViewHandler.java
@@ -0,0 +1,144 @@
+package com.adobe.acs.commons.contentfinder.querybuilder.impl.viewhandler;
+
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.wcm.core.contentfinder.ViewHandler;
+import com.day.cq.wcm.core.contentfinder.ViewQuery;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.framework.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.Session;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Component(
+        label = "ACS AEM Commons - GQL to Querybuilder View Handler",
+        description = "Leverage Querybuilder to run ContentFinder queries",
+        metatype = false,
+        immediate = false
+)
+@Properties({
+        @Property(
+                label = "Servlet Paths",
+                name = "sling.servlet.paths",
+                value = "/bin/wcm/contentfinder/qb/view",
+                propertyPrivate = true
+        ),
+        @Property(
+                label = "Vendor",
+                name = Constants.SERVICE_VENDOR,
+                value = "ACS",
+                propertyPrivate = true
+        )
+})
+@Service
+public class QueryBuilderViewHandler extends ViewHandler {
+    private final Logger log = LoggerFactory.getLogger(QueryBuilderViewHandler.class);
+
+    @Override
+    protected ViewQuery createQuery(SlingHttpServletRequest slingRequest, Session session, String queryString) throws Exception {
+        final ResourceResolver resolver = slingRequest.getResourceResolver();
+        final QueryBuilder qb = resolver.adaptTo(QueryBuilder.class);
+        Map<String, String> map;
+
+        if (GQLToQueryBuilderConverter.convertToQueryBuilder(slingRequest)) {
+            map = this.convertToQueryBuilderParams(slingRequest, queryString);
+            log.debug("Forced QueryBuilder Parameter Map: {}", map);
+        } else {
+            map = this.getQueryBuilderParams(slingRequest, queryString);
+            log.debug("Converted QueryBuilder Parameter Map: {}", map);
+        }
+
+        final Query query = qb.createQuery(PredicateGroup.create(map), session);
+        return new QueryBuilderViewQuery(query);
+    }
+
+
+    /**
+     * Assume query should be treated as a QueryBuilder query, rather than a GQL query
+     * <p/>
+     * This intelligently converts default Fulltext and Limit parameters to QueryBuilder equivalents
+     *
+     * @param request
+     * @param queryString
+     * @return
+     */
+    private Map<String, String> getQueryBuilderParams(final SlingHttpServletRequest request, final String queryString) {
+        Map<String, String> map = new LinkedHashMap<String, String>();
+
+        for (final String key : (Set<String>) request.getParameterMap().keySet()) {
+            // Skip known content finder parameters that are unused for QueryBuilder
+            if (!ArrayUtils.contains(ContentFinderConstants.QUERYBUILDER_BLACKLIST, key)) {
+                final String val = request.getParameter(key);
+                if(StringUtils.isNotBlank(val)) {
+                    map.put(key, val);
+                }
+            } else {
+                log.debug("Rejecting property [ {} ] due to blacklist match", key);
+            }
+        }
+
+        map = GQLToQueryBuilderConverter.addFulltext(request, map, queryString);
+        map = GQLToQueryBuilderConverter.addLimitAndOffset(request, map);
+
+        return map;
+    }
+
+
+    private Map<String, String> convertToQueryBuilderParams(final SlingHttpServletRequest request, final String queryString) {
+        Map<String, String> map = new LinkedHashMap<String, String>();
+
+        int userDefinedPropertyCount = 0;
+
+        for (final String key : request.getRequestParameterMap().keySet()) {
+            if (StringUtils.equals(key, ContentFinderConstants.CF_PATH)) {
+                log.debug("Converting path...");
+                map = GQLToQueryBuilderConverter.addPath(request, map);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_TYPE)) {
+                log.debug("Converting type...");
+                map = GQLToQueryBuilderConverter.addType(request, map);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_NAME)) {
+                log.debug("Converting name...");
+                map = GQLToQueryBuilderConverter.addName(request, map);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_TAGS)) {
+                log.debug("Converting tags...");
+                map = GQLToQueryBuilderConverter.addTags(request, map);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_MIMETYPE)) {
+                log.debug("Converting mimeType...");
+                map = GQLToQueryBuilderConverter.addMimeType(request, map);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_FULLTEXT)) {
+                log.debug("Converting fulltext...");
+                map = GQLToQueryBuilderConverter.addFulltext(request, map, queryString);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_ORDER)) {
+                log.debug("Converting order...");
+                map = GQLToQueryBuilderConverter.addOrder(request, map, queryString);
+            } else if (StringUtils.equals(key, ContentFinderConstants.CF_LIMIT)) {
+                log.debug("Converting limit...");
+                map = GQLToQueryBuilderConverter.addLimitAndOffset(request, map);
+            } else {
+                // User Defined Properties
+                log.debug("Converting property [ {} ]...", key);
+                map = GQLToQueryBuilderConverter.addProperty(request, map, key, userDefinedPropertyCount);
+                userDefinedPropertyCount++;
+            }
+        }
+
+        /**
+         * AND all groups together
+         */
+        map.put("p.or", "false");
+
+        return map;
+    }
+}
---
+++ bundle/src/main/java/com/adobe/acs/commons/contentfinder/querybuilder/impl/viewhandler/QueryBuilderViewQuery.java
@@ -0,0 +1,65 @@
+package com.adobe.acs.commons.contentfinder.querybuilder.impl.viewhandler;
+
+import com.adobe.acs.commons.contentfinder.querybuilder.impl.ContentFinderHitBuilder;
+import com.day.cq.search.Query;
+import com.day.cq.search.result.Hit;
+import com.day.cq.search.result.SearchResult;
+import com.day.cq.wcm.core.contentfinder.ViewQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class QueryBuilderViewQuery implements ViewQuery {
+    private static final Logger log = LoggerFactory.getLogger(QueryBuilderViewQuery.class);
+
+    private final Query query;
+
+    public QueryBuilderViewQuery(final Query query) {
+        this.query = query;
+    }
+
+    @Override
+    public Collection<com.day.cq.wcm.core.contentfinder.Hit> execute() {
+        final List<com.day.cq.wcm.core.contentfinder.Hit> hits = new ArrayList<com.day.cq.wcm.core.contentfinder.Hit>();
+
+        if (this.query == null) {
+            return hits;
+        }
+
+        final SearchResult result = this.query.getResult();
+
+        // iterating over the results
+        for (Hit hit : result.getHits()) {
+            try {
+                hits.add(createHit(hit));
+            } catch (RepositoryException e) {
+                log.error("Could not return required information for Content Finder result: {}", hit.toString());
+            }
+        }
+
+        return hits;
+    }
+
+    /**
+     * ViewQuery integration
+     *
+     * @param hit
+     * @return
+     * @throws javax.jcr.RepositoryException
+     */
+    private com.day.cq.wcm.core.contentfinder.Hit createHit(final Hit hit) throws RepositoryException {
+        final Map<String, Object> map = ContentFinderHitBuilder.buildGenericResult(hit);
+        final com.day.cq.wcm.core.contentfinder.Hit cfHit = new com.day.cq.wcm.core.contentfinder.Hit();
+
+        for (final String key : map.keySet()) {
+            cfHit.set(key, map.get(key));
+        }
+
+        return cfHit;
+    }
+}
