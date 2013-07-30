000cbc9c81fed4a4bc3d7742567246ba3a1ab0e3
David Gonzalez <dgonzale@adobe.com>
2013-07-30 19:44:27 -0400

    Added XSS protection to form data and errors

---
+++ bundle/src/main/java/com/adobe/acs/commons/forms/helpers/AbstractFormHelper.java
@@ -0,0 +1,7 @@
+package com.adobe.acs.commons.forms.helpers;
+
+/**
+ * Do not directly implement
+ */
+public interface AbstractFormHelper {
+}
--- bundle/src/main/java/com/adobe/acs/commons/forms/helpers/impl/AbstractFormHelperImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/forms/helpers/impl/AbstractFormHelperImpl.java
@@ -1,12 +1,16 @@
 package com.adobe.acs.commons.forms.helpers.impl;
 
 import com.adobe.acs.commons.forms.Form;
+import com.adobe.acs.commons.forms.helpers.AbstractFormHelper;
 import com.adobe.acs.commons.forms.helpers.FormHelper;
+import com.adobe.granite.xss.XSSAPI;
 import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.*;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.request.RequestParameter;
 import org.apache.sling.api.request.RequestParameterMap;
 import org.apache.sling.api.resource.Resource;
+import org.osgi.framework.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,17 +19,28 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
-
-public abstract class AbstractFormHelperImpl implements FormHelper {
+@Component(label = "ACS AEM Commons - Abstract Form Helper", description = "Abstract Form Helper. Do not use directly; instead use the PRGFormHelper or ForwardFormHelper.", enabled = true, metatype = false, immediate = false)
+@Properties({ @Property(label = "Vendor", name = Constants.SERVICE_VENDOR, value = "ACS", propertyPrivate = true) })
+@Service(value = AbstractFormHelper.class)
+public class AbstractFormHelperImpl implements FormHelper {
 	private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Reference
+    protected XSSAPI xssApi;
+
+    @Override
+    public Form getForm(String formName, HttpServletRequest request) {
+        throw new UnsupportedOperationException("Do not call AbstractFormHelper.getForm(..) direct. This is an abstract service.");
+    }
 
     @Override
 	public String getFormInputsHTML(final Form form, final String... keys)
 			throws IOException {
+        // The form obj's data and errors should be xssProtected before being passed into this method
 		String html = "";
 
         html += "<input type=\"hidden\" name=\"" + FORM_NAME_INPUT + "\" value=\""
-                + form.getName() + "\"/>\n";
+                + xssApi.encodeForHTMLAttr(form.getName()) + "\"/>\n";
 
 		for (final String key : keys) {
 			if (form.has(key)) {
@@ -39,15 +54,9 @@ public abstract class AbstractFormHelperImpl implements FormHelper {
 
     @Override
     public String getAction(final Resource resource) {
-        return resource.getPath() + ".post";
+        return resource.getPath() + "." + FormHelper.SELECTOR + FormHelper.EXTENSION;
     }
 
-    /**
-     *
-     * @param formName
-     * @param request
-     * @return
-     */
     protected boolean doHandlePost(final String formName, final HttpServletRequest request) {
         if(StringUtils.equalsIgnoreCase("POST", request.getMethod())) {
             // Form should have a hidden input with the name this.getLookupKey(..) and value formName
@@ -57,30 +66,28 @@ public abstract class AbstractFormHelperImpl implements FormHelper {
         }
     }
 
-    /**
-     *
-     * @param formName
-     * @param request
-     * @return
-     */
     protected Form getPostForm(final String formName,
                             final HttpServletRequest request) {
         final Map<String, String> map = new HashMap<String, String>();
 
         if(request instanceof SlingHttpServletRequest) {
-            log.debug("Request is a SlingHttpServletRequest");
+            log.debug("Attempting to get form [ {} ] from SlingHttpServletRequest", formName);
 
             final SlingHttpServletRequest slingRequest = (SlingHttpServletRequest) request;
             final RequestParameterMap requestMap = slingRequest.getRequestParameterMap();
 
             for (final String key : requestMap.keySet()) {
-                if(StringUtils.equals(key, this.getPostLookupKey(formName))) { continue; }
+                // POST LookupKey formName param does not matter
+                if(StringUtils.equals(key, this.getPostLookupKey(null))) { continue; }
 
                 final RequestParameter[] values = requestMap.getValues(key);
 
                 if (values == null || values.length == 0) {
+                    log.debug("Value did not exist for key: {}", key);
+
                     continue;
                 } else if (values.length == 1) {
+                    log.debug("Adding to form data: {} ~> {}", key, values[0].toString());
                     map.put(key, values[0].getString());
                 } else {
                     for (int i = 0; i < values.length; i++) {
@@ -89,7 +96,7 @@ public abstract class AbstractFormHelperImpl implements FormHelper {
                 }
             }
         } else {
-            log.debug("Request is NOT a SlingHttpServletRequest");
+            log.debug("Attempting to get form [ {} ] from HttpServletRequest", formName);
 
             for(final Object obj : request.getParameterMap().keySet()) {
                 if(obj instanceof String) {
@@ -104,11 +111,6 @@ public abstract class AbstractFormHelperImpl implements FormHelper {
         return new Form(formName, map);
     }
 
-    /**
-     *
-     * @param formName
-     * @return
-     */
     protected String getPostLookupKey(final String formName) {
         return FORM_NAME_INPUT;
     }
@@ -133,12 +135,63 @@ public abstract class AbstractFormHelperImpl implements FormHelper {
     }
 
     /**
+     * Protect a Form in is entirety (data and errors)
+     *
+     * @param form
+     * @return
+     */
+    protected Form getProtectedForm(final Form form) {
+        return new Form(form.getName(),
+                this.getProtectedData(form.getData()),
+                this.getProtectedErrors(form.getErrors()));
+    }
+
+    /**
+     * Protect a Map representing Form Data
+     *
+     * @param data
+     * @return
+     */
+    protected Map<String, String> getProtectedData(final Map<String, String> data) {
+        final Map<String, String> protectedData = new HashMap<String, String>();
+
+        // Protect data for HTML Attributes
+        for (final String key : data.keySet()) {
+            protectedData.put(key, xssApi.encodeForHTMLAttr(data.get(key)));
+        }
+
+        return protectedData;
+    }
+
+    /**
+     * Protect a Map representing Form Errors
+     *
+     * @param errors
+     * @return
+     */
+    protected Map<String, String> getProtectedErrors(final Map<String, String> errors) {
+        final Map<String, String> protectedErrors = new HashMap<String, String>();
+
+        // Protect data for HTML
+        for (final String key : errors.keySet()) {
+            protectedErrors.put(key, xssApi.encodeForHTML(errors.get(key)));
+        }
+
+        return protectedErrors;
+    }
+
+    /**
      * Internal method for encoding URL data
      *
      * @param unencoded
      * @return
      */
     protected String encode(String unencoded) {
+        if(StringUtils.isBlank(unencoded)) {
+            log.debug("Data to encode is blank.");
+            return "";
+        }
+
         try {
             return java.net.URLEncoder.encode(unencoded, "UTF-8");
         } catch (UnsupportedEncodingException e) {
@@ -153,6 +206,11 @@ public abstract class AbstractFormHelperImpl implements FormHelper {
      * @return
      */
     protected String decode(String encoded) {
+        if(StringUtils.isBlank(encoded)) {
+            log.debug("Data to decode is blank.");
+            return "";
+        }
+
         try {
             return java.net.URLDecoder.decode((encoded), "UTF-8");
         } catch (UnsupportedEncodingException e) {
