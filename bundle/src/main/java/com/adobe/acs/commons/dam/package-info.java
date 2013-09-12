975eb8c4705d13b07ae6b2936c7ed1e4dcbdb24e
Justin Edelson <jedelson@adobe.com>
2013-09-12 11:29:30 -0400

    #checkstyle

--- bundle/src/main/java/com/adobe/acs/commons/dam/package-info.java
+++ bundle/src/main/java/com/adobe/acs/commons/dam/package-info.java
@@ -1,7 +1,7 @@
 @Version("1.0.0")
-package com.adobe.acs.commons.dam;
-
 /**
  * This package provides DAM utility classes.
  */
+package com.adobe.acs.commons.dam;
+
 import aQute.bnd.annotation.Version;
