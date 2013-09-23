df98eda6425b96f23aeb05ebaf5a925686d3e456
David Gonzalez <dgonzale@adobe.com>
2013-09-23 16:34:42 -0400

    Added better error handling to avoid infinite delegation cycles Renamed OverlayServlet to DelegatingServlet

---
+++ bundle/src/main/java/com/adobe/acs/commons/util/impl/DelegatingServletFactoryImpl.java
@@ -0,0 +1,202 @@
+package com.adobe.acs.commons.util.impl;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.*;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.request.RequestDispatcherOptions;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.osgi.framework.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Component(
+        label = "ACS Commons - Overlay Servlet",
+        description = "Overlay Servlet enabling the unobtrusive delegate of Resource Types.",
+        configurationFactory = true,
+        policy = ConfigurationPolicy.REQUIRE,
+        metatype = true,
+        immediate = false)
+@Properties({
+        @Property(
+                label = "Vendor",
+                name = Constants.SERVICE_VENDOR,
+                value = "ACS Commons",
+                propertyPrivate = true
+        ),
+        @Property(
+                label = "Source Resource Types",
+                description = "Requests matching the \"Source resource types, selectors, extensions and methods\" will be overlayed using the \"Target Resource Type\"",
+                name = "sling.servlet.resourceTypes",
+                cardinality = Integer.MAX_VALUE,
+                value = {""}),
+        @Property(
+                label = "Source Selectors",
+                description = "Requests matching the \"Source resource types, selectors, extensions and methods\" will be overlayed using the \"Target Resource Type\"",
+                name = "sling.servlet.selectors",
+                cardinality = Integer.MAX_VALUE,
+                value = {""}),
+        @Property(
+                label = "Source Extensions",
+                description = "Requests matching the \"Source resource types, selectors, extensions and methods\" will be overlayed using the \"Target Resource Type\"",
+                name = "sling.servlet.extensions",
+                cardinality = Integer.MAX_VALUE,
+                value = {"html"}),
+        @Property(
+                label = "Source HTTP Methods",
+                description = "Requests matching the \"Source resource types, selectors, extensions and methods\" will be overlayed using the \"Target Resource Type\"",
+                name = "sling.servlet.methods",
+                cardinality = Integer.MAX_VALUE,
+                value = {"GET"}
+        )
+})
+@Service(Servlet.class)
+public class DelegatingServletFactoryImpl extends SlingAllMethodsServlet {
+    protected static final Logger log = LoggerFactory.getLogger(DelegatingServletFactoryImpl.class);
+    private static final String REQUEST_ATTR_DELEGATION_HISTORY = DelegatingServletFactoryImpl.class.getName() + "_History";
+
+
+    private static final String DEFAULT_TARGET_RESOURCE_TYPE = "";
+    private String targetResourceType = DEFAULT_TARGET_RESOURCE_TYPE;
+    @Property(label = "Target Resource Type",
+            description = "The resource type to proxy requests to.",
+            value = DEFAULT_TARGET_RESOURCE_TYPE)
+    public static final String PROP_TARGET_RESOURCE_TYPE = "prop.target-resource-type";
+
+    /** Safe HTTP Methods **/
+
+    public void doGeneric(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    public void doGet(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    public void doHead(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    public void doOptions(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    public void doTrace(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    /** Un-Safe HTTP Methods **/
+
+    public void doDelete(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    public void doPost(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    public void doPut(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        this.delegate(request, response);
+    }
+
+    /**
+     * Delegates request through to the target resource type
+     *
+     * @param request
+     * @param response
+     */
+    private void delegate(final SlingHttpServletRequest request, final SlingHttpServletResponse response) throws ServletException {
+        final RequestDispatcherOptions options = new RequestDispatcherOptions();
+
+        if(this.isCyclic(request, targetResourceType)) {
+            log.error("Delegation Servlet creating a cycle for Target Resource Type: {}", targetResourceType);
+            throw new ServletException("Cyclic delegation detected for " + targetResourceType);
+        }
+
+        if(StringUtils.isNotBlank(targetResourceType)) {
+            log.debug("Delegating Request resource type with: {}", targetResourceType);
+            options.setForceResourceType(targetResourceType);
+        } else {
+            log.warn("Delegating Servlet's \"Target Resource Type\" is blank or null");
+        }
+
+        try {
+            this.setDelegationHistory(request, targetResourceType);
+            request.getRequestDispatcher(request.getResource(), options).forward(request, response);
+        } catch (ServletException e) {
+            log.error("Could not properly re-route request to delegate resource type: {}", targetResourceType);
+        } catch (IOException e) {
+            log.error("Could not properly re-route request to delegate resource type: {}", targetResourceType);
+        }
+    }
+
+
+    /**
+     * Determines if the Request is or will be cyclic
+     *
+     * @param request
+     * @param targetResourceType
+     * @return true is Request will cause a and infinite delegation cycle
+     */
+    private boolean isCyclic(final SlingHttpServletRequest request, final String targetResourceType) {
+        if(StringUtils.isNotBlank(targetResourceType)) {
+            log.debug("Delegating Servlet's \"Target Resource Type\" is blank or null");
+            return true;
+        }
+
+        final Set<String> history = this.getDelegationHistory(request);
+        if(history.contains(targetResourceType)) {
+            log.debug("Delegating Servlet's \"Target Resource Type\" is been forwarded to previously");
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retrieves the delegation history from the Request
+     *
+     * @param request
+     * @return the delegation history set (of resource types previously targeted by this Servlet)
+     */
+    private Set<String> getDelegationHistory(final SlingHttpServletRequest request) {
+        Set<String> history = new HashSet<String>();
+        final Object tmp = request.getAttribute(REQUEST_ATTR_DELEGATION_HISTORY);
+        if(history.getClass().isInstance(tmp)) {
+            return (Set<String>)tmp;
+        } else {
+            return history;
+        }
+    }
+
+    /**
+     * Sets the targetResourceType as part of the delegation history and adds update history set to the Request
+     *
+     * @param request
+     * @param targetResourceType
+     */
+    private void setDelegationHistory(final SlingHttpServletRequest request, final String targetResourceType) {
+        final Set<String> history = this.getDelegationHistory(request);
+        history.add(targetResourceType);
+        request.setAttribute(REQUEST_ATTR_DELEGATION_HISTORY, history);
+    }
+
+    @Activate
+    protected void activate(final Map<String, String> config) {
+        targetResourceType = PropertiesUtil.toString(config.get(PROP_TARGET_RESOURCE_TYPE), "");
+
+        if(StringUtils.isBlank(targetResourceType)) {
+            throw new IllegalArgumentException("Target Resource Type must NOT be blank");
+        }
+
+        log.debug("Target Resource Type: {}", targetResourceType);
+    }
+}
