2ec43ad19e9ebf7cc331a560e459768e7a57b8bf
Justin Edelson <jedelson@adobe.com>
2013-09-24 14:33:50 -0400

    Refactoring code so that there's no intermediate widget node; just rtePlugins. allowing "includeDefault" configuration

--- bundle/src/main/java/com/adobe/acs/commons/wcm/impl/RTEConfigurationServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/wcm/impl/RTEConfigurationServlet.java
@@ -16,11 +16,11 @@ import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.apache.sling.commons.json.JSONException;
 import org.apache.sling.commons.json.JSONObject;
-import org.apache.sling.commons.json.io.JSONWriter;
 import org.apache.sling.commons.json.jcr.JsonItemWriter;
 import org.apache.sling.commons.osgi.PropertiesUtil;
 
@@ -28,11 +28,20 @@ import com.adobe.acs.commons.util.PathInfoUtil;
 import com.adobe.granite.xss.XSSAPI;
 
 /**
- * Servlets which allows for dynamic selection of RTE configuration. To use in a 
- * component, specify the xtype of <pre>slingscriptinclude</pre> and set the script
- * to <pre>rte.CONFIGNAME.FIELDNAME.json.jsp</pre>. This will iterate through nodes
- * under /etc/rteconfig to find a matching site (by regex). Then, look for a node named
- * CONFIGNAME and use that configuration.
+ * Servlets which allows for dynamic selection of RTE configuration. To use in a component, specify the xtype of
+ * 
+ * <pre>
+ * slingscriptinclude
+ * </pre>
+ * 
+ * and set the script to
+ * 
+ * <pre>
+ * rte.CONFIGNAME.FIELDNAME.json.jsp
+ * </pre>
+ * 
+ * . This will iterate through nodes under /etc/rteconfig to find a matching site (by regex). Then, look for a node
+ * named CONFIGNAME and use that configuration.
  */
 @SuppressWarnings("serial")
 @SlingServlet(extensions = "json", selectors = "rte", resourceTypes = "sling/servlet/default")
@@ -43,7 +52,7 @@ public class RTEConfigurationServlet extends SlingSafeMethodsServlet {
     @Reference
     private XSSAPI xssApi;
 
-    private static final String DEFAULT_CONFIG = "/libs/foundation/components/text/dialog/items/tab1/items/text";
+    private static final String DEFAULT_CONFIG = "/libs/foundation/components/text/dialog/items/tab1/items/text/rtePlugins";
 
     private static final String DEFAULT_ROOT_PATH = "/etc/rteconfig";
 
@@ -52,46 +61,13 @@ public class RTEConfigurationServlet extends SlingSafeMethodsServlet {
 
     private String rootPath;
 
-    @Activate
-    protected void activate(Map<String, Object> props) {
-        rootPath = PropertiesUtil.toString(props.get(PROP_ROOT_PATH), DEFAULT_ROOT_PATH);
-    }
-
-    @Override
-    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,
-            IOException {
-        String componentPath = request.getResource().getPath();
-
-        String configName = PathInfoUtil.getSelector(request, 1, DEFAULT_CONFIG_NAME);
-
-        // the actual property name
-        String rteName = PathInfoUtil.getSelector(request, 2, "text");
-
-        Resource root = request.getResourceResolver().getResource(rootPath);
-        if (root != null) {
-            Iterator<Resource> children = root.listChildren();
-            while (children.hasNext()) {
-                Resource child = children.next();
-                if (matches(componentPath, child)) {
-                    Resource config = child.getChild(configName);
-                    if (config == null) {
-                        config = child.getChild(DEFAULT_CONFIG_NAME);
-                    }
-                    if (config != null) {
-                        try {
-                            writeConfigResource(config, rteName, response);
-                        } catch (JSONException e) {
-                            throw new ServletException(e);
-                        }
-
-                        return;
-                    }
-                }
-            }
-        }
-
-        returnDefault(rteName, request, response);
-
+    private JSONObject createEmptyRTE(String rteName) throws JSONException {
+        JSONObject object = new JSONObject();
+        object.put("xtype", "richtext");
+        object.put("name", "./" + xssApi.encodeForJSString(rteName));
+        object.put("hideLabel", true);
+        object.put("jcr:primaryType", "cq:Widget");
+        return object;
     }
 
     private boolean matches(String componentPath, Resource resource) {
@@ -116,54 +92,133 @@ public class RTEConfigurationServlet extends SlingSafeMethodsServlet {
                 return;
             }
 
-            writeConfigResource(root, rteName, response);
+            writeConfigResource(root, rteName, true, response);
         } catch (JSONException e) {
             throw new ServletException(e);
         }
     }
 
-    private void writeConfigResource(Resource resource, String rteName, SlingHttpServletResponse response)
-            throws IOException, JSONException, ServletException {
-        Node node = resource.adaptTo(Node.class);
-        if (node == null) {
-            writeEmptyRTE(rteName, response);
-            return;
+    private JSONObject toJSONObject(Resource resource) throws JSONException, ServletException {
+        JSONObject config = null;
+        {
+            Node node = resource.adaptTo(Node.class);
+            if (node != null) {
+
+                JsonItemWriter writer = new JsonItemWriter(null);
+                StringWriter string = new StringWriter();
+                try {
+                    writer.dump(node, string, -1);
+                } catch (RepositoryException e) {
+                    throw new ServletException(e);
+                }
+                config = new JSONObject(string.toString());
+
+            }
+        }
+        return config;
+    }
+
+    /**
+     * Load the default configuration and "underlay" it under the provided
+     * configuration so that the provided configuration overwrites the default
+     * configuration.
+     * 
+     * @param resolver the resource resolver
+     * @param config the config to underlay
+     * @return the underlayed configuration
+     * @throws JSONException
+     * @throws ServletException
+     */
+    private JSONObject underlayDefault(ResourceResolver resolver, JSONObject config) throws JSONException, ServletException {
+        JSONObject defaultStructure = toJSONObject(resolver.getResource(DEFAULT_CONFIG));
+        if (defaultStructure != null) {
+            Iterator<String> keys = config.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                defaultStructure.put(key, config.get(key));
+            }
+            return defaultStructure;
+        } else {
+            return config;
+        }
+    }
+
+    private void writeConfigResource(Resource resource, String rteName, boolean isDefault,
+            SlingHttpServletResponse response) throws IOException, JSONException, ServletException {
+        JSONObject widget = createEmptyRTE(rteName);
+
+        // these two size properties seem to be necessary to get the size correct
+        // in a component dialog
+        widget.put("width", 430);
+        widget.put("height", 200);
+        
+        JSONObject config = toJSONObject(resource);
+
+        if (config == null) {
+            config = new JSONObject();
         }
 
-        JsonItemWriter writer = new JsonItemWriter(null);
-        StringWriter string = new StringWriter();
-        try {
-            writer.dump(node, string, -1);
-        } catch (RepositoryException e) {
-            throw new ServletException(e);
+        if (config.optBoolean("includeDefault")) {
+            config = underlayDefault(resource.getResourceResolver(), config);
         }
-        JSONObject config = new JSONObject(string.toString());
-        config.put("name", "./" + xssApi.encodeForJSString(rteName));
+
+        widget.put("rtePlugins", config);
 
         JSONObject parent = new JSONObject();
         parent.put("xtype", "dialogfieldset");
         parent.put("border", false);
         parent.put("padding", 0);
-
-        // these two size properties seem to be necessary to get the size correct
-        // in a component dialog
-        config.put("width", 430);
-        config.put("height", 200);
-        parent.accumulate("items", config);
+        parent.accumulate("items", widget);
         parent.write(response.getWriter());
     }
 
     private void writeEmptyRTE(String rteName, SlingHttpServletResponse response) throws IOException, JSONException {
-        JSONWriter writer = new JSONWriter(response.getWriter());
-        writer.object();
-        writer.key("xtype");
-        writer.value("richtext");
-        writer.key("name");
-        writer.value("./" + xssApi.encodeForJSString(rteName));
-        writer.value("hideLabel");
-        writer.value(true);
-        writer.key("jcr:primaryType");
-        writer.value("cq:Widget");
+        JSONObject rte = createEmptyRTE(rteName);
+        rte.write(response.getWriter());
+    }
+
+    @Activate
+    protected void activate(Map<String, Object> props) {
+        rootPath = PropertiesUtil.toString(props.get(PROP_ROOT_PATH), DEFAULT_ROOT_PATH);
+    }
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException,
+            IOException {
+        String componentPath = request.getResource().getPath();
+
+        String configName = PathInfoUtil.getSelector(request, 1, DEFAULT_CONFIG_NAME);
+
+        // the actual property name
+        String rteName = PathInfoUtil.getSelector(request, 2, "text");
+
+        Resource root = request.getResourceResolver().getResource(rootPath);
+        if (root != null) {
+            Iterator<Resource> children = root.listChildren();
+            while (children.hasNext()) {
+                Resource child = children.next();
+                if (matches(componentPath, child)) {
+                    boolean isDefault = false;
+                    Resource config = child.getChild(configName);
+                    if (config == null) {
+                        config = child.getChild(DEFAULT_CONFIG_NAME);
+                        isDefault = true;
+                    }
+                    if (config != null) {
+                        try {
+                            writeConfigResource(config, rteName, isDefault, response);
+                        } catch (JSONException e) {
+                            throw new ServletException(e);
+                        }
+
+                        return;
+                    }
+                }
+            }
+        }
+
+        returnDefault(rteName, request, response);
+
     }
 
 }
