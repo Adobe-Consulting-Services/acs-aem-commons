64c2259f3924e8abd2da7f84a77917bcf8fea7aa
David Gonzalez <davidjgonzalez@gmail.com>
2013-11-25 22:57:13 -0500

    Set configuration policy to require so the Forms filters are only in the filter stack if explicitly enabled by via osgi config nodes.

--- bundle/src/main/java/com/adobe/acs/commons/forms/impl/FormsPostRedirectGetSlingFilterImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/forms/impl/FormsPostRedirectGetSlingFilterImpl.java
@@ -20,32 +20,51 @@
 package com.adobe.acs.commons.forms.impl;
 
 import com.adobe.acs.commons.forms.helpers.PostFormHelper;
-import com.adobe.acs.commons.forms.helpers.PostRedirectGetFormHelper;
 import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.sling.SlingFilter;
-import org.apache.felix.scr.annotations.sling.SlingFilterScope;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.request.RequestDispatcherOptions;
-import org.apache.sling.api.request.RequestParameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.IOException;
 
-@SlingFilter(
-        label = "ACS AEM Commons - Post-Redirect-Get Multi-Step Forms Filter",
-        description = "Include Filter that handles internal routing of Multi-Step Post-Redirect-Get Form submissions.",
-        metatype = false,
-        generateComponent = true,
-        generateService = true,
-        order = 0,
-        scope = SlingFilterScope.INCLUDE)
+@Component(
+        label = "ACS AEM Commons - Forms Filter - Post-Redirect-Get",
+        description = "Include Filter that handles internal routing of multi-step POST-Redirect-GET Form submissions.",
+        policy = ConfigurationPolicy.REQUIRE,
+        metatype = true
+)
+@Properties({
+        @Property(
+                name = "sling.filter.scope",
+                value = "include",
+                propertyPrivate = true
+        ),
+        @Property(
+                name = "filter.order",
+                intValue = 0,
+                propertyPrivate = true
+        )
+})
+@Service
 public class FormsPostRedirectGetSlingFilterImpl implements Filter {
     private static final Logger log = LoggerFactory.getLogger(FormsPostRedirectGetSlingFilterImpl.class);
-    private static final String REQUEST_ATTR_PREVIOUSLY_PROCESSED = FormsPostRedirectGetSlingFilterImpl.class.getName() + "__Previously_Processed";
+
+    private static final String REQUEST_ATTR_PREVIOUSLY_PROCESSED =
+            FormsPostRedirectGetSlingFilterImpl.class.getName() + "__Previously_Processed";
 
     @Reference
     private PostFormHelper formHelper;
@@ -55,9 +74,10 @@ public class FormsPostRedirectGetSlingFilterImpl implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        if(!(servletRequest instanceof  SlingHttpServletRequest) ||
-                !(servletResponse instanceof SlingHttpServletResponse)) {
+    public final void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                               FilterChain filterChain) throws IOException, ServletException {
+        if (!(servletRequest instanceof SlingHttpServletRequest)
+                || !(servletResponse instanceof SlingHttpServletResponse)) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
@@ -76,22 +96,22 @@ public class FormsPostRedirectGetSlingFilterImpl implements Filter {
          *  - Include is not a product of a previous forward by this Filter
          */
 
-        if(!StringUtils.equals("GET", slingRequest.getMethod()) ||
-                !formHelper.hasValidSuffix(slingRequest)) {
+        if (!StringUtils.equals("GET", slingRequest.getMethod())
+                || !formHelper.hasValidSuffix(slingRequest)) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
 
         /* Ensure there is a valid form selector as part of Query Params */
-        final String formSelector = formHelper.getFormSelector(slingRequest); //this.getParameter(slingRequest, PostRedirectGetFormHelper.QUERY_PARAM_FORM_SELECTOR);
-        if(formSelector == null) {
+        final String formSelector = formHelper.getFormSelector(slingRequest);
+        if (formSelector == null) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
 
         /* Ensure this is not a product of a previous forward; This is to be absolutely sure we are not hitting an
          * infinite loop condition */
-        if(slingRequest.getAttribute(REQUEST_ATTR_PREVIOUSLY_PROCESSED) != null) {
+        if (slingRequest.getAttribute(REQUEST_ATTR_PREVIOUSLY_PROCESSED) != null) {
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
@@ -101,11 +121,15 @@ public class FormsPostRedirectGetSlingFilterImpl implements Filter {
         options.setReplaceSelectors(formSelector);
         options.setReplaceSuffix(slingRequest.getRequestPathInfo().getSuffix());
 
-        if(log.isDebugEnabled()) {
-            log.debug("Post-Redirect-Get Form Filter; Internal forward to resource: {} ", slingRequest.getResource());
-            log.debug("Post-Redirect-Get Form Filter; Internal forward to path: {} ", slingRequest.getResource().getPath());
-            log.debug("Post-Redirect-Get Filter; Internal forward w/ replace selectors: {} ", options.getReplaceSelectors());
-            log.debug("Post-Redirect-Get Filter; Internal forward w/ suffix: {} ", options.getReplaceSuffix());
+        if (log.isDebugEnabled()) {
+            log.debug("Post-Redirect-Get Form Filter; Internal forward to resource: {} ",
+                    slingRequest.getResource());
+            log.debug("Post-Redirect-Get Form Filter; Internal forward to path: {} ",
+                    slingRequest.getResource().getPath());
+            log.debug("Post-Redirect-Get Filter; Internal forward w/ replace selectors: {} ",
+                    options.getReplaceSelectors());
+            log.debug("Post-Redirect-Get Filter; Internal forward w/ suffix: {} ",
+                    options.getReplaceSuffix());
         }
 
         // Avoid accidental infinite loops with API consumers doing their own Fws and Includes
@@ -115,12 +139,5 @@ public class FormsPostRedirectGetSlingFilterImpl implements Filter {
 
     @Override
     public void destroy() {
-    }
-
-    private String getParameter(SlingHttpServletRequest slingRequest, String param) {
-        final RequestParameter requestParameter =
-                slingRequest.getRequestParameter(param);
-        if(requestParameter == null) { return null; }
-        return StringUtils.stripToNull(requestParameter.getString());
     }
 }
