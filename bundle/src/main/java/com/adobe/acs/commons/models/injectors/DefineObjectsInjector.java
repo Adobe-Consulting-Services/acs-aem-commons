4329a92bb842b144c3c80ceae8251619862af8ab
Matthias Schoger <matthias@schoger.net>
2014-03-24 15:58:44 +0100

    Updated documentation fixed typo

--- bundle/src/main/java/com/adobe/acs/commons/models/injectors/DefineObjectsInjector.java
+++ bundle/src/main/java/com/adobe/acs/commons/models/injectors/DefineObjectsInjector.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 - 2014 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.models.injectors;
 
 import com.adobe.granite.xss.XSSAPI;
@@ -45,6 +64,9 @@ import java.lang.reflect.Type;
  * <li> session the current session
  * <li> xssApi cross site scripting provider for the current request
  * </ul>
+ *
+ * Note: This Injector requires at least org.apache.sling.models.impl version 1.0.2
+ *
  */
 @Component
 @Service
@@ -67,7 +89,7 @@ public class DefineObjectsInjector implements Injector {
         switch(nameEnum) {
             case RESOURCE:
                 return getResource(adaptable);
-            case RECOURCE_RESOLVER:
+            case RESOURCE_RESOLVER:
                 return getResourceResolver(adaptable);
             case COMPONENT_CONTEXT:
                 return getComponentContext(adaptable);
--- bundle/src/main/java/com/adobe/acs/commons/models/injectors/ObjectType.java
+++ bundle/src/main/java/com/adobe/acs/commons/models/injectors/ObjectType.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 - 2014 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.models.injectors;
 
 /**
@@ -6,7 +25,7 @@ package com.adobe.acs.commons.models.injectors;
 enum ObjectType {
 
     RESOURCE("resource"),
-    RECOURCE_RESOLVER("resourceResolver"),
+    RESOURCE_RESOLVER("resourceResolver"),
     COMPONENT_CONTEXT("componentContext"),
     PAGE_MANAGER("pageManager"),
     CURRENT_PAGE("currentPage"),
