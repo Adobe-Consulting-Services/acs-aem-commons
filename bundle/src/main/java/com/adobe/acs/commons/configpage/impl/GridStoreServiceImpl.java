da333c8482e8e3eadaaa7b2c3eee8c28508921ec
Gopal Kalyanaraman <kalyanar@adobe.com>
2014-03-25 16:37:48 +0530

    added license check

--- bundle/src/main/java/com/adobe/acs/commons/configpage/impl/GridStoreServiceImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/configpage/impl/GridStoreServiceImpl.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 - 2014 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.configpage.impl;
 
 import java.util.List;
@@ -18,6 +37,7 @@ import org.slf4j.LoggerFactory;
 
 import com.adobe.acs.commons.configpage.GridOperationFailedException;
 import com.adobe.acs.commons.configpage.GridStoreService;
+import com.day.cq.dam.api.cache.BufferedImageCache.Entry;
 @Component(label = "ACS AEM Commons - Edit Grid - Grid store service",
         description = "Facilitates updating/deleting a grid row",
         immediate = false,
@@ -58,8 +78,8 @@ public class GridStoreServiceImpl implements GridStoreService {
         try {
        for(Map<String,String> row :rows){
            Node rowNode = JcrUtils.getOrCreateUniqueByPath(gridNode  , row.get(COLUMN_UID), JcrConstants.NT_UNSTRUCTURED);
-           for(String key :row.keySet()){
-               rowNode.setProperty(key,row.get(key));
+           for(Map.Entry entry : row.entrySet()){
+               rowNode.setProperty((String)entry.getKey(),(String)entry.getValue());
            }
        }
            session.save();
