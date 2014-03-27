bed7369d9f7ddb69037fc333d521486df5e891dd
upasanac <upasana.chaube@gmail.com>
2014-03-27 19:17:00 +0530

    Amended review comments changes. Removed sample email template inside /etc/notification. Added Licence headers, doc comments, used the consistent style logging and other logical changes suggested.

--- bundle/src/main/java/com/adobe/acs/commons/email/EmailService.java
+++ bundle/src/main/java/com/adobe/acs/commons/email/EmailService.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.email;
 
 import java.util.List;
@@ -5,11 +24,19 @@ import java.util.Map;
 import javax.mail.internet.InternetAddress;
 
 /**
+ * A service interface for sending a generic template based Email Notification.
  * 
- * @author Upasana Chaube, 2014
- *
  */
 public interface EmailService {
 	
+	/**
+	 * API that sends an email to a given list of recipients using an Email template.
+	 * It uses the CQ Mail Service configuration. Hence it should be configured in the felix console.
+	 * 
+	 * @param templatePath, Absolute path of the template used to send the Email (e.g. /etc/notification/email/acsEmailTemplate/emailtemplate.txt)
+	 * @param recipientsList, List of recipient's email id's 
+	 * @param emailParams, Email param Map to be injected in the template
+	 * @return Boolean, true- if Email is sent, false otherwise.
+	 */
 	Boolean sendEmail(String templatePath, List<InternetAddress> recipientsList, Map<String,String> emailParams);
 }
--- bundle/src/main/java/com/adobe/acs/commons/email/impl/EmailServiceImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/email/impl/EmailServiceImpl.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.email.impl;
 
 import java.io.IOException;
@@ -7,7 +26,6 @@ import javax.jcr.Session;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import org.apache.sling.api.resource.LoginException;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.commons.lang.text.StrLookup;
@@ -25,99 +43,113 @@ import com.day.cq.mailer.MessageGateway;
 import com.day.cq.mailer.MessageGatewayService;
 
 /**
- * 
- * @author Upasana Chaube, 2014
+ * Generic Email Notification Service implementation class that defines an API to send a template based Email
+ * notification to a given list of recipients.
  *
  */
-
 @Component(immediate = true, metatype = true, label = "Generic Email Service",
 			description = "A Generic Email service that sends an email to the given list of recipients using a template specified")
-@Service(EmailService.class)
+@Service
 public class EmailServiceImpl implements EmailService {
 
-	public static final Logger LOG  = LoggerFactory.getLogger(EmailServiceImpl.class);
+	public static final Logger log  = LoggerFactory.getLogger(EmailServiceImpl.class);
 	
 	/**
 	 * This is the global email configuration inside the DAY CQ MAIL service.
-	 * 
 	 */
 	@Reference
 	private MessageGatewayService messageGatewayService;
-
-	/**
-	 * This is the configuration to send out an email, which will at least
-	 * contain one recipient, and message body.
-	 * 
-	 */
-	private MessageGateway<HtmlEmail> messageGateway;
 	 
 	@Reference
 	private ResourceResolverFactory resourceResolverFactory;
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	public Boolean sendEmail(String templatePath, List<InternetAddress> recipientsList, Map<String, String> emailParams) {
 		
+		
+		ResourceResolver resourceResolver = null;
+		
 		try {
+			resourceResolver = getResourceResolver();
 			
-			Resource templateRsrc = null;	
-			ResourceResolver resourceResolver = getResourceResolver();
-			if (resourceResolver.resolve(templatePath) != null) {
-				templateRsrc = resourceResolver.resolve(templatePath) ;
+			if (resourceResolver.getResource(templatePath) == null) {
+				log.error("[IllegalArgumentException] : Missing template -> "+ templatePath);
+				return Boolean.FALSE;
 			}
-			if (templateRsrc == null) {
-				LOG.error("IllegalArgumentException: Missing template -> "+ templatePath);
-				return false;
-			}
-  		
-			final MailTemplate mailTemplate = MailTemplate.create(templateRsrc.getPath(), templateRsrc.getResourceResolver().adaptTo(Session.class));			
+			
+			final MailTemplate mailTemplate = MailTemplate.create(templatePath, resourceResolver.adaptTo(Session.class));			
 		
 			final HtmlEmail email = mailTemplate.getEmail(StrLookup.mapLookup(emailParams),HtmlEmail.class);
 			
 			email.setTo(recipientsList);
 			
-			//Set Email Sender information if thats present in the input param			
-			if(emailParams.containsKey(EmailServiceConstants.SENDER_EMAIL_ID) && 
-					emailParams.containsKey(EmailServiceConstants.SENDER_NAME))
-				email.setFrom(emailParams.get(EmailServiceConstants.SENDER_EMAIL_ID), emailParams.get(EmailServiceConstants.SENDER_NAME));
-			else if(emailParams.containsKey(EmailServiceConstants.SENDER_EMAIL_ID))
-				email.setFrom(emailParams.get(EmailServiceConstants.SENDER_EMAIL_ID));
-			
-			// Check the logs to see that messageGatewayService is not null
-			LOG.info("messageGatewayService : " + messageGatewayService);
-			
-			if (messageGatewayService == null) {
-				LOG.error("FAILED to send out the email because the message gateway service is NULL, please check the Mail Service configuration in CQ");
-			}
+			setSenderInformation(email, emailParams);
 
-			else {
-				messageGateway = messageGatewayService.getGateway(HtmlEmail.class);
-				
-				// Check the logs to see that messageGateway is not null
-				LOG.info("messageGateway : " + messageGateway);
-				
+			/**
+			 * This is the configuration to send out an email, which will at least
+			 * contain one recipient, and message body.
+			 * 
+			 */
+			MessageGateway<HtmlEmail> messageGateway = messageGatewayService.getGateway(HtmlEmail.class);	
+			
+			if(messageGateway != null)  {
 				messageGateway.send(email);
-				
-			    return true;
+				return Boolean.TRUE;
+			} 
+			else {
+				log.error("FAILED to send out the email because the message gateway service is NULL, please check the Mail Service configuration in CQ");
 			}
-
+				
 		} catch (IOException e) {		
-			LOG.error("IOException : FAILED TO SEND THE EMAIL TO ADDRESSES: ", e);		
+			log.error("[IOException] : Failed to send the Email to Addresses: ", e);		
 			
 		} catch (MessagingException e) {
-			LOG.error("MessagingException : FAILED TO SEND THE EMAIL TO ADDRESSES: ", e);			
+			log.error("[MessagingException] : Failed to send the Email to Addresses: ", e);			
 			
 		} catch (EmailException e) {
-			LOG.error("EmailException : FAILED TO SEND THE EMAIL TO ADDRESSES: ", e);	
+			log.error("[EmailException] : Failed to send the Email to Addresses: ", e);	
 			
-		}catch(Exception e){
-			LOG.error("Exception : FAILED TO SEND THE EMAIL TO ADDRESSES: ", e);	
-		}
-		 return false;
+		} catch(Exception e) {
+			log.error("[Exception] : Failed to send the Email to Addresses: ", e);	
+			
+		} finally {
+            if (resourceResolver != null) {
+                resourceResolver.close();
+            }
+        }
+		
+		return Boolean.FALSE;
 
 	}
 	
+	
+	/**
+	 * Gets the resourceResolver object from the ResourceResolverFactory Service
+	 * @return resourceResolver
+	 * @throws LoginException
+	 */
 	private ResourceResolver getResourceResolver() throws LoginException {
 		
 		return resourceResolverFactory.getAdministrativeResourceResolver(null);
 	}
 
+	
+	/**
+	 * Sets the Email Sender information if thats present in the input param
+	 * @param email
+	 * @param emailParams
+	 * @throws EmailException
+	 */
+	private void setSenderInformation(HtmlEmail email, Map<String, String> emailParams) throws EmailException {
+		
+		if(emailParams.containsKey(EmailServiceConstants.SENDER_EMAIL_ID) && 
+				emailParams.containsKey(EmailServiceConstants.SENDER_NAME)) {
+			email.setFrom(emailParams.get(EmailServiceConstants.SENDER_EMAIL_ID), emailParams.get(EmailServiceConstants.SENDER_NAME));
+		}
+		else if(emailParams.containsKey(EmailServiceConstants.SENDER_EMAIL_ID)) {
+			email.setFrom(emailParams.get(EmailServiceConstants.SENDER_EMAIL_ID));
+		}
+	}
 }
