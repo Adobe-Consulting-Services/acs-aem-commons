3c93f52c8613b2262e87463a75f5a4a31d697837
Justin Edelson <jedelson@adobe.com>
2014-04-10 14:58:05 +0100

    minor fixes. related to #174

--- content/src/main/content/jcr_root/apps/acs-commons/authoring/audio-asset-finder/audioController.js
+++ content/src/main/content/jcr_root/apps/acs-commons/authoring/audio-asset-finder/audioController.js
@@ -1,13 +1,28 @@
-;
-(function ($, ns, channel, window, undefined) {
+/*
+ * #%L
+ * ACS AEM Commons Package
+ * %%
+ * Copyright (C) 2013 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/*global Granite: false */
+(function ($, ns, channel, window) {
 
-    var self = {},
-        name = 'Audio';
-
-    // make the loadAssets fuction more flexible
-    self.searchRoot = '/content/dam';
-
-    var searchPath = self.searchRoot,
+    var self = { searchRoot : '/content/dam' },
+        name = 'Audio',
+        searchPath = self.searchRoot,
         imageServlet = '/bin/wcm/contentfinder/asset/view.html',
         itemResourceType = 'cq/gui/components/authoring/assetfinder/asset';
 
