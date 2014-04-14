2d46c5a9efb5c2f2785e8c2fd11a3cff4a8647ad
Justin Edelson <jedelson@adobe.com>
2014-04-14 13:31:25 -0400

    updating design reference provider to use design/page API

--- bundle/src/main/java/com/adobe/acs/commons/wcm/impl/DesignReferenceProvider.java
+++ bundle/src/main/java/com/adobe/acs/commons/wcm/impl/DesignReferenceProvider.java
@@ -19,7 +19,6 @@
  */
 package com.adobe.acs.commons.wcm.impl;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
@@ -28,9 +27,12 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.ConfigurationPolicy;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 
-import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.day.cq.wcm.api.designer.Design;
+import com.day.cq.wcm.api.designer.Designer;
 import com.day.cq.wcm.api.reference.Reference;
 import com.day.cq.wcm.api.reference.ReferenceProvider;
 
@@ -38,43 +40,41 @@ import com.day.cq.wcm.api.reference.ReferenceProvider;
  * 
  * ACS AEM commons Design Reference Provider.
  * Reference provider that searches for design pages for any given page resource.
- *
  */
 @Component(
         label = "ACS AEM Commons - Design Reference Provider",
         description = "Reference provider that searches for design pages for any given page resource",
-        metatype = false , policy = ConfigurationPolicy.REQUIRE)
+        policy = ConfigurationPolicy.REQUIRE)
 @Service
-public class DesignReferenceProvider implements ReferenceProvider {
+public final class DesignReferenceProvider implements ReferenceProvider {
 
     private static final String TYPE_DESIGN_PAGE = "designpage";
-    private static final String DESIGN_PATH = "cq:designPath";
 
     @Override
-    public final List<Reference> findReferences(Resource resource) {
-        String designPath = getDesignPath(resource);
-        if (null == designPath || "".equals(designPath)) {
+    public List<Reference> findReferences(Resource resource) {
+        ResourceResolver resourceResolver = resource.getResourceResolver();
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        Designer designer = resourceResolver.adaptTo(Designer.class);
+
+        Page page = pageManager.getContainingPage(resource);
+        if (page == null) {
             return Collections.emptyList();
         }
-        Resource designResource =
-                resource.getResourceResolver().getResource(designPath);
-        Page designPage = designResource.adaptTo(Page.class);
-        List<Reference> references = new ArrayList<Reference>(1);
-        references.add(new Reference(TYPE_DESIGN_PAGE,
-                designResource.getName(), designResource,
-                getLastModifiedTimeOfResource(designPage)));
-        return references;
+
+        Design design = designer.getDesign(page);
+        if (design == null) {
+            return Collections.emptyList();
+        }
+
+        return Collections.singletonList(new Reference(TYPE_DESIGN_PAGE,
+                String.format("%s (Design)", design.getId()),
+                design.getContentResource(),
+                getLastModified(design)));
     }
 
-    private long getLastModifiedTimeOfResource(Page page) {
-        final Calendar mod = page.getLastModified();
+    private long getLastModified(Design design) {
+        final Calendar mod = design.getLastModified();
         long lastModified = mod != null ? mod.getTimeInMillis() : -1;
         return lastModified;
-    }
-
-    private String getDesignPath(Resource resource) {
-        HierarchyNodeInheritanceValueMap hnvm =
-                new HierarchyNodeInheritanceValueMap(resource);
-        return hnvm.getInherited(DESIGN_PATH, "");
     }
 }
--- bundle/src/test/java/com/adobe/acs/commons/wcm/impl/DesignReferenceProviderTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/wcm/impl/DesignReferenceProviderTest.java
@@ -19,78 +19,85 @@
  */
 package com.adobe.acs.commons.wcm.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.day.cq.wcm.api.designer.Design;
+import com.day.cq.wcm.api.designer.Designer;
 import com.day.cq.wcm.api.reference.Reference;
- 
- @RunWith(MockitoJUnitRunner.class)
- public class DesignReferenceProviderTest {
-     @InjectMocks
-     private DesignReferenceProvider instance;
-     
-     @Mock
-     private Resource resource;
-     
-     
-     @Mock
-     private ResourceResolver resolver;
-     
-     @Mock
-     private Resource designResource;
-     
-     @Mock
-     private Page designPage;
-     
-     @Before
-     public void setUp() throws Exception {
-         instance = new DesignReferenceProvider();
-         Map<String, Object> map = new HashMap<String, Object>();
-         map.put("cq:designPath", "/etc/designs/geometrixx");
-         ValueMap vm =  new ValueMapDecorator(map);
-         when(resource.adaptTo(ValueMap.class)).thenReturn(vm);
-         when(resource.getResourceResolver()).thenReturn(resolver);
-         when(resolver.getResource("/etc/designs/geometrixx")).thenReturn(designResource);
-         when(designResource.adaptTo(Page.class)).thenReturn(designPage);
-         Calendar cal = GregorianCalendar.getInstance();
-         when(designPage.getLastModified()).thenReturn(cal);
-         
-     }
-     
-     @Test
-     public void testfindReferencesWithDesignPathinResource() {
+
+@RunWith(MockitoJUnitRunner.class)
+public class DesignReferenceProviderTest {
+
+    private DesignReferenceProvider instance;
+
+    @Mock
+    private Page page;
+
+    @Mock
+    private Design design;
+
+    @Mock
+    private Designer designer;
+
+    @Mock
+    private PageManager pageManager;
+
+    @Mock
+    private Resource resource;
+
+    @Mock
+    private ResourceResolver resolver;
+
+    @Mock
+    private Resource designResource;
+
+    @Before
+    public void setUp() throws Exception {
+        instance = new DesignReferenceProvider();
+        when(resource.getResourceResolver()).thenReturn(resolver);
+        when(resolver.adaptTo(PageManager.class)).thenReturn(pageManager);
+        when(resolver.adaptTo(Designer.class)).thenReturn(designer);
+
+        when(pageManager.getContainingPage(resource)).thenReturn(page);
         
+        when(design.getContentResource()).thenReturn(designResource);
+        when(design.getId()).thenReturn("test");
+        Calendar cal = Calendar.getInstance();
+        when(design.getLastModified()).thenReturn(cal);
+
+    }
+
+    @Test
+    public void testfindReferencesWithDesignPathinResource() {
+        when(designer.getDesign(page)).thenReturn(design);
+
         List<Reference> ref = instance.findReferences(resource);
+        assertNotNull(ref);
         assertEquals(1, ref.size());
-        assertEquals(false, ref==null);
-      }
-     @Test
-     public void testfindReferencesWithNoDesignPathinResource() {
-         Map<String, Object> map = new HashMap<String, Object>();
-         map.put("cq:designPath", "");
-         ValueMap vm =  new ValueMapDecorator(map);
-         when(resource.adaptTo(ValueMap.class)).thenReturn(vm);
+        assertEquals(designResource, ref.get(0).getResource());
+        assertEquals("designpage", ref.get(0).getType());
+        assertEquals("test (Design)", ref.get(0).getName());
+    }
+
+    @Test
+    public void testfindReferencesWithNoDesignPathinResource() {
         List<Reference> ref = instance.findReferences(resource);
+        assertNotNull(ref);
         assertEquals(0, ref.size());
-        assertEquals(false, ref==null);
-      }
-     
- }
+    }
+
+}
