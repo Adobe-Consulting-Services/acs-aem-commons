84770d22df1cee72bab7d41d1cd51f791a8901e7
Justin Edelson <jedelson@adobe.com>
2014-05-28 13:31:37 -0400

    formatting & null check along with unit test

--- bundle/src/main/java/com/adobe/acs/commons/util/impl/ComponentDisabler.java
+++ bundle/src/main/java/com/adobe/acs/commons/util/impl/ComponentDisabler.java
@@ -1,6 +1,26 @@
+/*
+ * #%L
+ * ACS AEM Tools Bundle
+ * %%
+ * Copyright (C) 2014 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.util.impl;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import org.apache.felix.scr.Component;
 import org.apache.felix.scr.ScrService;
@@ -10,12 +30,10 @@ import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.commons.osgi.PropertiesUtil;
-import org.osgi.service.component.ComponentContext;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 /**
  * Component disabler service
@@ -36,62 +54,54 @@ import org.slf4j.LoggerFactory;
  * 
  *
  */
+@org.apache.felix.scr.annotations.Component(immediate = true, metatype = true,
+        label = "ACS AEM Commons - OSGI Component Disabler", description = "Disables components by configuration",
+        policy = ConfigurationPolicy.REQUIRE)
+@Service()
+@Property(name = "event.topics", value = { "/org/osgi/framework/BundleEvent/STARTED",
+        "org/osgi/framework/ServiceEvent/REGISTERED" }, propertyPrivate = true)
+public class ComponentDisabler implements EventHandler {
 
-
-@org.apache.felix.scr.annotations.Component(
-	immediate = true, 
-	metatype = true, 
- label = "ACS AEM Commons - OSGI Component Disabler",
-	description = "Disables components by configuration", 
-	policy = ConfigurationPolicy.REQUIRE)
-@Service() 
-@Property(name="event.topics", value={"/org/osgi/framework/BundleEvent/STARTED","org/osgi/framework/ServiceEvent/REGISTERED"}, propertyPrivate=true) 
-public class ComponentDisabler implements EventHandler { 
-
+    private static final Logger log = LoggerFactory.getLogger(ComponentDisabler.class);
 
     @Reference
-    ScrService scrService;
+    private ScrService scrService;
 
-    private static final Logger log = LoggerFactory
-	    .getLogger(ComponentDisabler.class);
+    @Property(label = "Disabled components", description = "The names of the components/services you want to disable",
+            cardinality = Integer.MAX_VALUE)
+    private static final String DISABLED_COMPONENTS = "components";
 
-    @Property(label = "Disabled components", description = "The names of the components/services you want to disable", cardinality = Integer.MAX_VALUE)
-    private static final String DISABLED_COMPONENTS = "services";
     private String[] disabledComponents;
 
     @Activate
-    protected void activate(ComponentContext ctx) {
-	disabledComponents = PropertiesUtil.toStringArray(ctx.getProperties()
-		.get(DISABLED_COMPONENTS), new String[] {});
-	log.info("Disabling components and services {}",
-		Arrays.toString(disabledComponents));
-	handleEvent(null);
+    protected void activate(Map<String, Object> properties) {
+        disabledComponents = PropertiesUtil
+                .toStringArray(properties.get(DISABLED_COMPONENTS), new String[0]);
+        handleEvent(null);
     }
-
 
     @Override
     public void handleEvent(Event event) {
-	// We don't care about the event, we just need iterate all configured
-	// components and
-	// try to disable them
+        // We don't care about the event, we just need iterate all configured
+        // components and try to disable them
+        log.info("Disabling components and services {}", Arrays.toString(disabledComponents));
 
-	for (String component : disabledComponents) {
-	    disableComponent(component);
-	}
+        for (String component : disabledComponents) {
+            disableComponent(component);
+        }
     }
 
     private boolean disableComponent(String componentName) {
-	Component[] comps = (Component[]) scrService
-		.getComponents(componentName);
-	for (Component comp : comps) {
-	    if (comp.getState() != Component.STATE_DISABLED) {
-		comp.disable();
-		log.info(
-			"Component {} disabled by configuration (pid={}) ",
-			new Object[] { comp.getClassName(),
-				comp.getConfigurationPid() });
-	    }
-	}
-	return true;
+        Component[] comps = (Component[]) scrService.getComponents(componentName);
+        if (comps != null) {
+            for (Component comp : comps) {
+                if (comp.getState() != Component.STATE_DISABLED) {
+                    log.info("Component {} disabled by configuration (pid={}) ",
+                            new Object[] { comp.getClassName(), comp.getConfigurationPid() });
+                    comp.disable();
+                }
+            }
+        }
+        return true;
     }
 }
