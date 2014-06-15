44f7f46625fcb429d32f3dc3a5fdb196c02d72cb
Steffen Roesinger <sroesing@adobe.com>
2014-06-15 16:10:45 +0200

    added loading indicator to async component loading

--- content/src/main/content/jcr_root/apps/acs-commons/components/content/base/ajax/clientlibs/js/ajax.js
+++ content/src/main/content/jcr_root/apps/acs-commons/components/content/base/ajax/clientlibs/js/ajax.js
@@ -18,12 +18,36 @@
  * #L%
  */
 ;$(function() {
+	
+	var opts = {
+			  lines: 11, // The number of lines to draw
+			  length: 10, // The length of each line
+			  width: 4, // The line thickness
+			  radius: 12, // The radius of the inner circle
+			  corners: 1, // Corner roundness (0..1)
+			  rotate: 9, // The rotation offset
+			  direction: 1, // 1: clockwise, -1: counterclockwise
+			  color: '#000', // #rgb or #rrggbb or array of colors
+			  speed: 1, // Rounds per second
+			  trail: 60, // Afterglow percentage
+			  shadow: false, // Whether to render a shadow
+			  hwaccel: false, // Whether to use hardware acceleration
+			  className: 'spinner', // The CSS class to assign to the spinner
+			  zIndex: 2e9, // The z-index (defaults to 2000000000)
+			  top: '50%', // Top position relative to parent
+			  left: '50%' // Left position relative to parent
+			};
+	
+	$('.acs-ajax-loader').each(function() {
+		var $this = this, 
+			mySpinner = new Spinner(opts).spin($this);
+	});
+	
     $('[data-ajax-component]').each(function() {
         var $this = $(this),
             url = $this.data('url');
 
         url += "?t=" + (new Date()).getTime();
-
         $.get(url).success(function(data) {
             if(!data.match(/\sdata-ajax-component/)) {
                 $this.replaceWith(data);
