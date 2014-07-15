e6f0e1880c3cae23e0d1b4cf0df6ba7ff73a91db
bstopp <bryan.stopp@gmail.com>
2014-07-15 13:08:17 -0500

    Package naming convention doesn't match that of the source under test cases.

---
+++ bundle/src/test/java/com/adobe/acs/commons/images/transformers/impl/GreyscaleImageTransformerImplTest.java
@@ -0,0 +1,93 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package com.adobe.acs.commons.images.transformers.impl;
+
+import com.adobe.acs.commons.images.transformers.impl.GreyscaleImageTransformerImpl;
+import com.day.image.Layer;
+
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GreyscaleImageTransformerImplTest {
+
+    GreyscaleImageTransformerImpl transformer;
+
+    @Mock
+    Layer layer;
+
+    Map<String, Object> map = null;
+
+    @Before
+    public void setUp() throws Exception {
+        map = new HashMap<String, Object>();
+        transformer = new GreyscaleImageTransformerImpl();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        map = null;
+        reset(layer);
+    }
+
+    @Test
+    public void testTransform() throws Exception {
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).grayscale();
+        verifyNoMoreInteractions(layer);
+    }
+
+    @Test
+    public void testTransform_emptyParams() throws Exception {
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).grayscale();
+        verifyZeroInteractions(layer);
+    }
+
+    @Test
+    public void testTransform_nullParams() throws Exception {
+        transformer.transform(layer, null);
+
+        verify(layer, times(1)).grayscale();
+        verifyZeroInteractions(layer);
+    }
+}
