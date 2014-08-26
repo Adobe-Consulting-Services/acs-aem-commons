061297748c941223b0d75f480f80d02d13848e45
Michael Kreis <michi.kreis@gmail.com>
2014-08-26 14:46:38 +0200

    add new image transformer to always keep ratio when resizing the image. Only resizes either width or height (cherry picked from commit 7bbc902)

---
+++ bundle/src/main/java/com/adobe/acs/commons/images/transformers/impl/MaxWidthOrHeightTransformerImpl.java
@@ -0,0 +1,84 @@
+package com.adobe.acs.commons.images.transformers.impl;
+
+import com.adobe.acs.commons.images.ImageTransformer;
+import com.day.image.Layer;
+import org.apache.felix.scr.annotations.*;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+@Component(
+        label = "Image Transformer - Max width or height resizing",
+        description =
+                "ImageTransformer that resizes the layer. Accepts two Integer params: height and width. " +
+                "Only width or height will be scaled and the other dimension will be calculated to keep original ratio. " +
+                "If the original image is smaller than the configured dimensions the image won't be resized")
+@Properties({
+        @Property(
+                name = ImageTransformer.PROP_TYPE,
+                value = MaxWidthOrHeightTransformerImpl.TYPE,
+                propertyPrivate = true
+        )
+})
+@Service(value = ImageTransformer.class)
+/**
+ * @author mkreis
+ */
+public class MaxWidthOrHeightTransformerImpl implements ImageTransformer {
+    private static final Logger log = LoggerFactory.getLogger(MaxWidthOrHeightTransformerImpl.class);
+
+    @Property(label = "Allow upscaling images", boolValue = false)
+    static final String ALLOW_UPSCALING_PROPERTY = "maxwidthorheight.allowUpscaling";
+    private boolean allowUpscaling;
+
+    static final String TYPE = "maxwidthorheight";
+
+    private static final String KEY_WIDTH = "width";
+
+    private static final String KEY_WIDTH_ALIAS = "w";
+
+    private static final String KEY_HEIGHT = "height";
+
+    private static final String KEY_HEIGHT_ALIAS = "h";
+
+
+    @Override
+    public final Layer transform(final Layer layer, final ValueMap properties) {
+        if (properties == null || properties.isEmpty()) {
+            log.warn("Transform [ {} ] requires parameters.", TYPE);
+            return layer;
+        }
+
+        log.debug("Transforming with [ {} ]", TYPE);
+
+        int originalWidth = layer.getWidth();
+        int originalHeight = layer.getHeight();
+        int width = properties.get(KEY_WIDTH, properties.get(KEY_WIDTH_ALIAS, originalWidth));
+        int height = properties.get(KEY_HEIGHT, properties.get(KEY_HEIGHT_ALIAS, originalHeight));
+
+
+        if((float) width / originalWidth < (float) height / originalHeight){
+            final float aspect = (float) width / layer.getWidth();
+            height = Math.round(layer.getHeight() * aspect);
+        }
+        else {
+            final float aspect = (float) height / layer.getHeight();
+            width = Math.round(layer.getWidth() * aspect);
+        }
+
+        //only resize if image is not upscaled or upscaling is allowed explicitly
+        if(allowUpscaling || (!(width > originalWidth) && !(height > originalHeight))){
+            layer.resize(width, height);
+        }
+
+        return layer;
+    }
+
+    @Activate
+    protected final void activate(final Map<String, String> config) {
+        allowUpscaling = PropertiesUtil.toBoolean(config.get(ALLOW_UPSCALING_PROPERTY), false);
+    }
+}
---
+++ bundle/src/test/java/com/adobe/acs/commons/images/imagetransformers/impl/MaxWidthOrHeightTransformerImplTest.java
@@ -0,0 +1,188 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+package com.adobe.acs.commons.images.imagetransformers.impl;
+
+import com.adobe.acs.commons.images.transformers.impl.MaxWidthOrHeightTransformerImpl;
+import com.day.image.Layer;
+
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MaxWidthOrHeightTransformerImplTest {
+
+    MaxWidthOrHeightTransformerImpl transformer;
+
+    @Mock
+    Layer layer;
+
+    Map<String, Object> map = null;
+
+    @Before
+    public void setUp() throws Exception {
+        map = new HashMap<String, Object>();
+        transformer = new MaxWidthOrHeightTransformerImpl();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        map = null;
+        reset(layer);
+    }
+
+    @Test
+    public void testTransform_Landscape_only_width() throws Exception {
+        prepareLandscape();
+
+        map.put("width", 200);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(200, 131);
+    }
+
+    @Test
+    public void testTransform_Landscape_to_Landscape_Ratio() throws Exception {
+        prepareLandscape();
+
+        map.put("width", 200);
+        map.put("height", 10);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(15, 10);
+    }
+
+    @Test
+    public void testTransform_Landscape_to_Portrait_Ratio() throws Exception {
+        prepareLandscape();
+
+        map.put("width", 10);
+        map.put("height", 200);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(10, 7);
+    }
+
+    @Test
+    public void testTransform_Portrait_to_Landscape_Ratio() throws Exception {
+        preparePortrait();
+
+        map.put("width", 300);
+        map.put("height", 200);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(131, 200);
+    }
+
+    @Test
+    public void testTransform_Portrait_to_Portrait_Ratio() throws Exception {
+        preparePortrait();
+
+        map.put("width", 200);
+        map.put("height", 300);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(197, 300);
+    }
+
+    @Test
+    public void testTransform_Square_to_Portrait_Ratio() throws Exception {
+        prepareSquare();
+
+        map.put("width", 200);
+        map.put("height", 300);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(200, 200);
+    }
+
+    @Test
+    public void testTransform_Square_to_Landscape_Ratio() throws Exception {
+        prepareSquare();
+
+        map.put("width", 300);
+        map.put("height", 200);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(1)).resize(200, 200);
+    }
+
+    @Test
+    public void testTransform_no_resize() throws Exception {
+        prepareSquare();
+
+        map.put("width", 800);
+        map.put("height", 600);
+
+        ValueMap properties = new ValueMapDecorator(map);
+
+        transformer.transform(layer, properties);
+
+        verify(layer, times(0)).resize(anyInt(), anyInt());
+    }
+
+    private void prepareLandscape(){
+        when(layer.getWidth()).thenReturn(640);
+        when(layer.getHeight()).thenReturn(420);
+    }
+
+    private void preparePortrait(){
+        when(layer.getWidth()).thenReturn(420);
+        when(layer.getHeight()).thenReturn(640);
+    }
+
+    private void prepareSquare(){
+        when(layer.getWidth()).thenReturn(420);
+        when(layer.getHeight()).thenReturn(420);
+    }
+}
