89f1a13b3dcbaaf093f892d61244adef4e4c5b46
David Gonzalez <davidjgonzalez@gmail.com>
2014-08-30 11:08:44 -0400

    Bounded Resize Image Transformer - #checkstyle and updated osgi param names to acs conventions

--- bundle/src/main/java/com/adobe/acs/commons/images/transformers/impl/BoundedResizeTransformerImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/images/transformers/impl/BoundedResizeTransformerImpl.java
@@ -2,20 +2,22 @@ package com.adobe.acs.commons.images.transformers.impl;
 
 import com.adobe.acs.commons.images.ImageTransformer;
 import com.day.image.Layer;
-import org.apache.felix.scr.annotations.*;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
 
 @Component(
         label = "ACS AEM Commons - Image Transformer - Bounded Resize",
         description =
-                "ImageTransformer that resizes the layer. Accepts two Integer params: height and width. " +
-                "Only width or height will be scaled and the other dimension will be calculated to keep original ratio. " +
-                "If the original image is smaller than the configured dimensions the image won't be resized")
+                "ImageTransformer that resizes the layer but will not resize past maximum dimension constraints. "
+                        + "Accepts two Integer params: height and width. "
+                        + "Either width or height will scale to the parameterized limit. "
+                        + "The other dimension scale automatically to maintain the original aspect ratio"
+                        + "If the original image is smaller than the configured dimensions the image won't be resized")
 @Properties({
         @Property(
                 name = ImageTransformer.PROP_TYPE,
@@ -27,7 +29,7 @@ import java.util.Map;
 public class BoundedResizeTransformerImpl implements ImageTransformer {
     private static final Logger log = LoggerFactory.getLogger(BoundedResizeTransformerImpl.class);
 
-    static final String TYPE = "boundedResize";
+    static final String TYPE = "bounded-resize";
 
     private static final String KEY_WIDTH = "width";
 
@@ -37,7 +39,7 @@ public class BoundedResizeTransformerImpl implements ImageTransformer {
 
     private static final String KEY_HEIGHT_ALIAS = "h";
 
-    private static final String ALLOW_UPSCALING = "allowUpscaling";
+    private static final String KEY_UPSCALE = "upscale";
 
 
     @Override
@@ -53,20 +55,19 @@ public class BoundedResizeTransformerImpl implements ImageTransformer {
         int originalHeight = layer.getHeight();
         int width = properties.get(KEY_WIDTH, properties.get(KEY_WIDTH_ALIAS, originalWidth));
         int height = properties.get(KEY_HEIGHT, properties.get(KEY_HEIGHT_ALIAS, originalHeight));
-        boolean allowUpscaling = properties.get(ALLOW_UPSCALING, false);
+        boolean upscale = properties.get(KEY_UPSCALE, false);
 
 
-        if((float) width / originalWidth < (float) height / originalHeight){
+        if ((float) width / originalWidth < (float) height / originalHeight) {
             final float aspect = (float) width / layer.getWidth();
             height = Math.round(layer.getHeight() * aspect);
-        }
-        else {
+        } else {
             final float aspect = (float) height / layer.getHeight();
             width = Math.round(layer.getWidth() * aspect);
         }
 
         //only resize if image is not upscaled or upscaling is allowed explicitly
-        if(allowUpscaling || (!(width > originalWidth) && !(height > originalHeight))){
+        if (upscale || (!(width > originalWidth) && !(height > originalHeight))) {
             layer.resize(width, height);
         }
 
