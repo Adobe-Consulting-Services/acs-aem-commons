d7a37fd8ceffa1bcaf896a2cfb328e9919ac5d86
david g <davidjgonzalez@gmail.com>
2015-05-09 16:59:56 -0400

    WCM Views - #copyright and cleanup

--- bundle/src/main/java/com/adobe/acs/commons/wcm/views/impl/WCMViewsFilter.java
+++ bundle/src/main/java/com/adobe/acs/commons/wcm/views/impl/WCMViewsFilter.java
@@ -1,3 +1,23 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2015 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 package com.adobe.acs.commons.wcm.views.impl;
 
 import com.adobe.acs.commons.util.CookieUtil;
@@ -10,6 +30,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.ConfigurationPolicy;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
@@ -43,7 +64,8 @@ import java.util.regex.Pattern;
 
 @org.apache.felix.scr.annotations.Component(
         label = "ACS AEM Commons - WCM Views Filter",
-        metatype = true
+        metatype = true,
+        policy = ConfigurationPolicy.REQUIRE
 )
 @Properties({
         @Property(
