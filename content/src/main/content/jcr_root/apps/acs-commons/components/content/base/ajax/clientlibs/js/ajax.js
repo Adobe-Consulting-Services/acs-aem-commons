fc414aefd6d004c5504621c1735f4d72192ec19a
Brian Johnson <ice1138@gmail.com>
2015-06-28 11:56:46 -0400

    Enhancement to ajax component: Add option to pass query parameters to the ajax request. Particularly useful for components (search) in which query parameters are require for functionality.

--- content/src/main/content/jcr_root/apps/acs-commons/components/content/base/ajax/clientlibs/js/ajax.js
+++ content/src/main/content/jcr_root/apps/acs-commons/components/content/base/ajax/clientlibs/js/ajax.js
@@ -21,8 +21,13 @@
     $('[data-ajax-component]').each(function() {
         var $this = $(this),
             url = $this.data('url');
+		var	queryDelimter = "?";
 
-        url += "?t=" + (new Date()).getTime();
+        if (url.indexOf(queryDelimter) > 0){
+            queryDelimter = "&";
+        };
+
+        url += queryDelimter + "t=" + (new Date()).getTime();
         $.get(url).success(function(data) {
             if(!data.match(/\sdata-ajax-component/)) {
                 $this.replaceWith(data);
