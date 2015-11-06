e5c49294f403116cec22bf5b4a3277124d6241a3
Sivaramvt <Sivaramvt@gmail.com>
2015-11-06 12:39:30 +0530

    Changes in APIs of Cache Config.

--- bundle-httpcache/src/main/java/com/adobe/acs/commons/httpcache/config/HttpCacheConfig.java
+++ bundle-httpcache/src/main/java/com/adobe/acs/commons/httpcache/config/HttpCacheConfig.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 /**
  * Configuration for Http cache. Multiple configs can be supplied. Each config has an one on one mapping with request
- * pattern that has to be cached.
+ * pattern that has to be cached. Apart from request uri, authentication details, aem user group details, cache store
+ * details and invalidation JCR path details are captured through configs. <ul><li>User groups are applied only for
+ * configs that need authenticated requests. </li><li>User group list represents logical OR condition and at least one
+ * of the groups should be present in the request.</li><li>If 2 cache configs with the same url pattern and different
+ * list of groups present, one with high match score of groups with request would be considered. In case of levelled
+ * score, cache will be rejected.</li><li>Cache invalidation path is the JCR path expressed in REGEX pattern.</li></ul>
  */
 public interface HttpCacheConfig {
     /**
@@ -15,28 +20,19 @@ public interface HttpCacheConfig {
     String getRequestUri();
 
     /**
-     * Get configured response mime type to be cached.
+     * Check if this config applies for authenticated requests.
      *
-     * @return Example application/json.
+     * @return True if yes.
      */
-    String getResponseMimeType();
-
-    // TODO - Mandatory and optional list of user groups may not be required. Cached item shall be a combination of
-    // request uri and the set of configured groups.
+    boolean isRequestAuthenticationRequired();
 
     /**
-     * Get the list of configured AEM user groups which must be present in the request user's group list.
+     * Get the list of configured AEM user groups in which at least one of them must be present in the request user's
+     * group list.
      *
      * @return
      */
-    List<String> getMandatoryUserGroupNames();
-
-    /**
-     * Get the list of configured AEM user groups which could be optionally present in the request user's group list.
-     *
-     * @return
-     */
-    List<String> getOptionalUserGroupNames();
+    List<String> geUserGroupNames();
 
     /**
      * Name of the configured cache store.
@@ -46,7 +42,7 @@ public interface HttpCacheConfig {
     String getCacheStoreName();
 
     /**
-     * Get the list of configured JCR paths for which this cache will be invalidated.
+     * Get the list of configured JCR path patterns (REGEX for which this cache will be invalidated.
      *
      * @return
      */
