cf943dd0555b841b6f8024c3e0e19ed03dd0f2c6
david g <davidjgonzalez@gmail.com>
2015-12-11 22:54:41 -0500

    Http Cache - missing package-info.java files

--- bundle/src/main/java/com/adobe/acs/commons/httpcache/keys/package-info.java
+++ bundle/src/main/java/com/adobe/acs/commons/httpcache/keys/package-info.java
@@ -18,13 +18,6 @@
  * #L%
  */
 
-/**
- * Invalidation module of http cache implementation has a sling job with a defined topic, a job consumer and a set of
- * invalidation events. Invalidation events create invalidation jobs which would be consumed by the job consumer and
- * invalidates the cache. For a typical implementation, invalidation event could be custom supplied based on the cache
- * config invalidation requirements. A sample implementation based on sling eventing is provided.
- */
 @aQute.bnd.annotation.Version("1.0.0")
-package com.adobe.acs.commons.httpcache.invalidator;
-
+package com.adobe.acs.commons.httpcache.keys;
 
