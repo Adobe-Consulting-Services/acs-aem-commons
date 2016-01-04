2a14bed40b661c624af97cbb91ce91ff49f91851
David Gonzalez <davidjgonzalez@gmail.com>
2016-01-03 19:56:10 -0500

    EnsureOakIndex updates - Class/Method renaming - Servlet to MBean - Removed extra Solr classes

--- bundle/src/main/java/com/adobe/acs/commons/oak/package-info.java
+++ bundle/src/main/java/com/adobe/acs/commons/oak/package-info.java
@@ -2,14 +2,14 @@
  * #%L
  * ACS AEM Commons Bundle
  * %%
- * Copyright (C) 2014 Adobe
+ * Copyright (C) 2013 Adobe
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,24 +17,8 @@
  * limitations under the License.
  * #L%
  */
-package com.adobe.acs.commons.oak;
-
 /**
- * The OakIndexManager allows one to control the application of the indexes,
- * which are managed by the EnsureOakIndex functionality.
- * 
- * 
- *
+ * Oak utilities.
  */
-
-public interface OakIndexManager {
-
-    
-    /**
-     * runs through all available index EnsureOakIndex services and forces them to
-     * install their definitions (irrespectively if the index has been already applied).
-     * 
-     * 
-     */
-    public void installAllIndexes ();
-}
+@aQute.bnd.annotation.Version("1.0.0")
+package com.adobe.acs.commons.oak;
