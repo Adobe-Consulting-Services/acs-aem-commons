5e36ce76387eaf816a645363b9382f38af27ca53
Justin Edelson <justin@justinedelson.com>
2016-01-20 13:30:19 -0500

    include event topics in hint. fixes #632

--- bundle/src/main/java/com/adobe/acs/commons/logging/impl/JsonEventLogger.java
+++ bundle/src/main/java/com/adobe/acs/commons/logging/impl/JsonEventLogger.java
@@ -46,7 +46,7 @@ import java.util.*;
 @Properties({
     @Property(
             name = "webconsole.configurationFactory.nameHint",
-            value = "Logger: {event.logger.category} for events matching '{event.filter}'")
+            value = "Logger: {event.logger.category} for events matching '{event.filter}' on '{event.topics}'")
 })
 public class JsonEventLogger implements EventHandler {
 
