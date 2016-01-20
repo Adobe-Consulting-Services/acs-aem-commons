86f248655de126ef77a5445dfff4fe224a1bac07
Justin Edelson <justin@justinedelson.com>
2016-01-20 14:12:33 -0500

    update bundle plugin and move activator class to non-exported package

--- bundle/src/main/java/com/adobe/acs/commons/util/impl/Activator.java
+++ bundle/src/main/java/com/adobe/acs/commons/util/impl/Activator.java
@@ -17,7 +17,7 @@
  * limitations under the License.
  * #L%
  */
-package com.adobe.acs.commons;
+package com.adobe.acs.commons.util.impl;
 
 
 import org.apache.sling.settings.SlingSettingsService;
