be1264f733a52d08346c87818881f5d30c8254a9
Justin Edelson <justin@justinedelson.com>
2016-04-22 07:33:52 +0800

    formatting

--- bundle/src/main/java/com/adobe/acs/commons/images/impl/SocialRemoteImageImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/images/impl/SocialRemoteImageImpl.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2016 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.images.impl;
 
 import java.io.IOException;
@@ -12,12 +31,12 @@ import com.day.image.Layer;
 
 public class SocialRemoteImageImpl extends Image {
 
-	private final Resource res;
-	
-	public SocialRemoteImageImpl(Resource arg0, String name) {
-		super(arg0, name);
-		res = arg0;
-	}
+    private final Resource res;
+
+    public SocialRemoteImageImpl(Resource resource, String name) {
+        super(resource, name);
+        res = resource;
+    }
 
     public Layer getLayer(boolean cropped, boolean resized, boolean rotated)
             throws IOException, RepositoryException {
@@ -36,5 +55,5 @@ public class SocialRemoteImageImpl extends Image {
         }
         return layer;
     }
-    
+
 }
