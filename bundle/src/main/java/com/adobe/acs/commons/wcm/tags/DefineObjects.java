673887c416082c5871fae7dacaec29dd1a1b042d
vanassd <derek.vanassche@hs2solutions.com>
2016-06-09 16:48:47 -0500

    Read property from the component property node instead of child nodes; null check around component property home.

--- bundle/src/main/java/com/adobe/acs/commons/wcm/tags/DefineObjects.java
+++ bundle/src/main/java/com/adobe/acs/commons/wcm/tags/DefineObjects.java
@@ -1,25 +1,22 @@
 package com.adobe.acs.commons.wcm.tags;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import aQute.bnd.annotation.ProviderType;
-
-import com.adobe.granite.ui.components.Value;
 import com.day.cq.wcm.api.components.Component;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import tldgen.BodyContentType;
 import tldgen.Tag;
 
 import javax.jcr.Node;
-import javax.jcr.NodeIterator;
 import javax.jcr.Property;
+import javax.jcr.PropertyIterator;
 import javax.jcr.RepositoryException;
+import javax.jcr.Value;
 import javax.servlet.ServletRequest;
 import javax.servlet.jsp.tagext.BodyTagSupport;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by derek on 6/9/16.
@@ -67,19 +64,45 @@ public class DefineObjects extends BodyTagSupport {
 
     protected void setProperties(Node componentPropertyHome) {
         try {
-            ServletRequest req = this.pageContext.getRequest();
-            NodeIterator it = componentPropertyHome.getNodes();
-            Map<String,  String> propertyMap = new HashMap<String,  String>();
-            while (it.hasNext()) {
-                Node n = it.nextNode();
-                Property nameProp = n.getProperty("name");
-                javax.jcr.Value valueProp = n.getProperty("value").getValue();
-                propertyMap.put(nameProp.getString(), valueProp.getString());
-                log.info("Setting " + nameProp.getString() + " to " + valueProp.getString());
+            if (componentPropertyHome != null) {
+                ServletRequest req = this.pageContext.getRequest();
+                PropertyIterator it = componentPropertyHome.getProperties();
+                Map<String,  String> propertyMap = new HashMap<String,  String>();
+                while (it.hasNext()) {
+                    Property p = it.nextProperty();
+                    String propName = p.getName();
+                    if (!"jcr:primaryType".equalsIgnoreCase(propName)) {
+                        Value v = p.getValue();
+                        if (v != null) {
+                            propertyMap.put(p.getName(), v.toString());
+                        } else {
+                            propertyMap.put(p.getName(), null);
+                        }
+                    }
+                }
+
                 pageContext.setAttribute("sitewideProperties", propertyMap);
             }
         } catch (RepositoryException e) {
             log.error("Could node set properties on node.", e);
         }
+    }
+
+    protected Map<String, String> mergeProperties(Map<String, String> instanceProps, Map<String, String> sitewideProps) {
+        Map<String, String> mergedProperties = new HashMap<String, String>();
+
+        Set<String> instanceKeys = instanceProps.keySet();
+        for (String instanceKey : instanceKeys) {
+            mergedProperties.put(instanceKey, instanceProps.get(instanceKey));
+        }
+
+        Set<String> sitewideKeys = sitewideProps.keySet();
+        for (String sitewideKey : sitewideKeys) {
+            if (!mergedProperties.containsKey(sitewideKey)) {
+                mergedProperties.put(sitewideKey, sitewideProps.get(sitewideKey));
+            }
+        }
+
+        return mergedProperties;
     }
 }
