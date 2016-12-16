c97ab20ba081d78cde72608d3a00820313954a67
Dominik Foerderreuther <df@adobe.com>
2016-12-16 08:45:23 +0100

    One2One-Compare, Unit-Test

--- bundle/src/main/java/com/adobe/acs/commons/one2one/model/One2OneCompareModel.java
+++ bundle/src/main/java/com/adobe/acs/commons/one2one/model/One2OneCompareModel.java
@@ -2,6 +2,7 @@ package com.adobe.acs.commons.one2one.model;
 
 import com.adobe.acs.commons.version.Evolution;
 import com.adobe.acs.commons.version.EvolutionAnalyser;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.base.Predicate;
@@ -26,31 +27,36 @@ public class One2OneCompareModel {
 
     private static final Logger log = LoggerFactory.getLogger(One2OneCompareModel.class);
 
+    @VisibleForTesting
     @Inject
-    private ResourceResolver resolver;
+    ResourceResolver resolver;
 
+    @VisibleForTesting
     @Inject
-    private EvolutionAnalyser analyser;
+    EvolutionAnalyser analyser;
 
     private final String pathA;
-    private final Optional<String> versionA;
-    private final Optional<String> pathB;
-    private final Optional<String> versionB;
+    private final String versionA;
+    private final String pathB;
+    private final String versionB;
 
-    private FluentIterable<Evolution> evolutionsA;
-    private Optional<FluentIterable<Evolution>> evolutionsB;
+    @VisibleForTesting
+    FluentIterable<Evolution> evolutionsA;
+
+    @VisibleForTesting
+    FluentIterable<Evolution> evolutionsB;
 
     public One2OneCompareModel(SlingHttpServletRequest request) {
         this.pathA = request.getParameter("path");
-        this.pathB = Optional.fromNullable(request.getParameter("pathB"));
-        this.versionA = Optional.fromNullable(request.getParameter("a"));
-        this.versionB = Optional.fromNullable(request.getParameter("b"));
+        this.pathB = request.getParameter("pathB");
+        this.versionA = request.getParameter("a");
+        this.versionB = request.getParameter("b");
     }
 
     @PostConstruct
     protected void activate() {
         this.evolutionsA = FluentIterable.from(evolutions(pathA));
-        this.evolutionsB = pathB.isPresent() ? Optional.of(FluentIterable.from(evolutions(pathB.get()))) : Optional.<FluentIterable<Evolution>>absent();
+        this.evolutionsB = pathB != null ? FluentIterable.from(evolutions(pathB)) : this.evolutionsA;
     }
 
     public String getResourcePathA() {
@@ -58,7 +64,7 @@ public class One2OneCompareModel {
     }
 
     public String getResourcePathB() {
-        return pathB.or("");
+        return Optional.fromNullable(pathB).or("");
     }
 
     public List<VersionSelection> getVersionSelectionsA() {
@@ -66,7 +72,7 @@ public class One2OneCompareModel {
     }
 
     public List<VersionSelection> getVersionSelectionsB() {
-        return evolutionsB.or(evolutionsA).transform(TO_VERSION_SELECTION).toList();
+        return evolutionsB.transform(TO_VERSION_SELECTION).toList();
     }
 
     public Evolution getEvolutionA() {
@@ -74,15 +80,23 @@ public class One2OneCompareModel {
     }
 
     public Evolution getEvolutionB() {
-        return version(evolutionsB.or(evolutionsA), getVersionB()).orNull();
+        return version(evolutionsB, getVersionB()).orNull();
     }
 
     public String getVersionA() {
-        return versionA.or(evolutionsA.transform(TO_NAME).first().or(""));
+        if (versionA == null) {
+            return evolutionsA.transform(TO_NAME).first()
+                    .or("");
+        }
+        return versionA;
     }
 
     public String getVersionB() {
-        return versionB.or(evolutionsB.or(evolutionsA).transform(TO_NAME).last().or(""));
+        if (versionB == null) {
+            return evolutionsB.transform(TO_NAME).last()
+                    .or("");
+        }
+        return versionB;
     }
 
 
