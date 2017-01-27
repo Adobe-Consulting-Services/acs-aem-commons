02676716871630cb7b1e7dd85aa7a4d46b3a97a7
david g <davidjgonzalez@gmail.com>
2017-01-27 12:06:38 -0500

    Ensure Service User - Support blank glob

--- bundle/src/test/java/com/adobe/acs/commons/users/impl/ServiceUserTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/users/impl/ServiceUserTest.java
@@ -33,18 +33,62 @@ public class ServiceUserTest {
             assertEquals("jcr:read", ace.getPrivilegeNames().get(0));
             assertEquals("rep:write", ace.getPrivilegeNames().get(1));
             assertEquals("/content/dam", ace.getContentPath());
+
+            assertTrue(ace.hasRepGlob());
             assertEquals("/jcr:content/*", ace.getRepGlob());
 
+            assertTrue(ace.hasRepNtNames());
             assertEquals("cq:Page", ace.getRepNtNames().get(0));
             assertEquals("dam:Asset", ace.getRepNtNames().get(1));
 
+            assertTrue(ace.hasRepItemNames());
             assertEquals("jcr:title", ace.getRepItemNames().get(0));
             assertEquals("jcr:description", ace.getRepItemNames().get(1));
 
+            assertTrue(ace.hasRepPrefixes());
             assertEquals("cq", ace.getRepPrefixes().get(0));
             assertEquals("dam", ace.getRepPrefixes().get(1));
         }
 
         assertTrue(serviceUser.hasAceAt("/content/dam"));
+    }
+
+
+    public void testServiceUser_blankGlob() throws EnsureServiceUserException {
+        String[] aces = new String[1];
+        aces[0] = "type=allow;privileges=jcr:read;path=/content/dam;rep:glob=;";
+
+        Map<String, Object> config = new HashMap<String, Object>();
+        config.put(EnsureServiceUser.PROP_PRINCIPAL_NAME, "test-service-user");
+        config.put(EnsureServiceUser.PROP_ACES, aces);
+
+        ServiceUser serviceUser = new ServiceUser(config);
+
+        Assert.assertEquals("test-service-user", serviceUser.getPrincipalName());
+        Assert.assertEquals("/home/users/system", serviceUser.getIntermediatePath());
+
+        for (Ace ace : serviceUser.getAces()) {
+            assertTrue(ace.hasRepGlob());
+            assertEquals("", ace.getRepGlob());
+        }
+    }
+
+    public void testServiceUser_NoGlob() throws EnsureServiceUserException {
+        String[] aces = new String[1];
+        aces[0] = "type=allow;privileges=jcr:read;path=/content/dam;";
+
+        Map<String, Object> config = new HashMap<String, Object>();
+        config.put(EnsureServiceUser.PROP_PRINCIPAL_NAME, "test-service-user");
+        config.put(EnsureServiceUser.PROP_ACES, aces);
+
+        ServiceUser serviceUser = new ServiceUser(config);
+
+        Assert.assertEquals("test-service-user", serviceUser.getPrincipalName());
+        Assert.assertEquals("/home/users/system", serviceUser.getIntermediatePath());
+
+        for (Ace ace : serviceUser.getAces()) {
+            assertTrue(!ace.hasRepGlob());
+            assertEquals(null, ace.getRepGlob());
+        }
     }
 }
