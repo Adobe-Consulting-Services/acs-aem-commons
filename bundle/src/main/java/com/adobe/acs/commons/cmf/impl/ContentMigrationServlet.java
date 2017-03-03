81667fc070bd428ebe4f8d059e01e3809b7721c6
Jörg Hoh <joerg@joerghoh.de>
2017-03-03 23:48:08 +0100

    improved implementation based on first sample content migration step

--- bundle/src/main/java/com/adobe/acs/commons/cmf/impl/ContentMigrationServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/cmf/impl/ContentMigrationServlet.java
@@ -10,9 +10,12 @@ import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.sling.SlingServlet;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 
 import com.adobe.acs.commons.cmf.ContentMigrationProcessor;
+import com.adobe.acs.commons.cmf.IdentifiedResources;
+import com.adobe.acs.commons.cmf.NoSuchContentMigrationStepException;
 
 @SlingServlet(
         methods = { "GET" },
@@ -22,6 +25,12 @@ import com.adobe.acs.commons.cmf.ContentMigrationProcessor;
 )
 public class ContentMigrationServlet extends SlingAllMethodsServlet {
 	
+	private static final String PATH = "path";
+
+
+	private static final String MIGRATION_STEP = "migrationStep";
+
+
 	@Reference
 	ContentMigrationProcessor cmp;
 	
@@ -39,9 +48,36 @@ public class ContentMigrationServlet extends SlingAllMethodsServlet {
 		out.print("List of registered processors (" + registeredProcessors.size() + ")");
 		Iterator<String> iter = registeredProcessors.iterator();
 		
+		out.print("<ul>");
 		while (iter.hasNext()) {
 			String s = iter.next();
-			out.println(s + ",");
+			out.println("<li>" + s + "</li>");
+		}
+		out.print("</ul>");
+		
+		String usedMigrationStep = request.getParameter(MIGRATION_STEP);
+		String path = request.getParameter(PATH);
+		
+		out.print(String.format("<p>Starting %s on path %s</p>", new Object[]{usedMigrationStep,path}));
+		
+		try {
+			IdentifiedResources id = cmp.identifyAffectedResources(usedMigrationStep, path, request.getResourceResolver());
+			Iterator<String> resources = id.getResources().iterator();
+			out.print("Found resources:<ul>");
+			while (resources.hasNext()) {
+				out.print (String.format("<li>%s</li>", resources.next()));
+			}
+			out.print("</ul>");
+			
+			out.print("<p>Start migration</p>");
+			
+			cmp.migrateResources(id, request.getResourceResolver());
+			
+			out.print("<p>Migration completed</p>");
+			
+			
+		} catch (NoSuchContentMigrationStepException e) {
+			out.print ("<p>Did not find migration step called " + usedMigrationStep + "</p>");
 		}
 		
 		
