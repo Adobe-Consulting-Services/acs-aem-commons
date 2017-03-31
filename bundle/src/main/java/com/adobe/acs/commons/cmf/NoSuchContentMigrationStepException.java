86f961613b7cdd51bdaa36ead4eb4c9650feacb0
Jörg Hoh <joerg@joerghoh.de>
2017-03-31 22:58:41 +0200

    refactoring went wrong, old files survived

--- bundle/src/main/java/com/adobe/acs/commons/cmf/NoSuchContentMigrationStepException.java
+++
@@ -1,9 +0,0 @@
-package com.adobe.acs.commons.cmf;
-
-public class NoSuchContentMigrationStepException extends Exception {
-
-	public NoSuchContentMigrationStepException (String msg) {
-		super (msg);
-	}
-	
-}
