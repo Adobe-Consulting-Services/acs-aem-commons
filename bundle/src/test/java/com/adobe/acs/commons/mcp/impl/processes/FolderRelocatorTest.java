502954a34ae3e2f747e0c399ccdc2de50defd306
Brendan Robert <brobert@adobe.com>
2017-07-24 21:45:52 -0500

    Added more mocking and now the MCP framework is mostly covered through the process of defining and starting a process.

--- bundle/src/test/java/com/adobe/acs/commons/mcp/impl/processes/FolderRelocatorTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/mcp/impl/processes/FolderRelocatorTest.java
@@ -22,12 +22,18 @@ import com.adobe.acs.commons.mcp.ControlledProcessManager;
 import com.adobe.acs.commons.mcp.ProcessInstance;
 import com.adobe.acs.commons.mcp.impl.ProcessInstanceImpl;
 import static com.adobe.acs.commons.fam.impl.ActionManagerTest.*;
+import com.adobe.acs.commons.mcp.impl.AbstractResourceImpl;
 import com.adobe.acs.commons.mcp.util.DeserializeException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.security.AccessControlManager;
+import javax.jcr.security.Privilege;
 import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.ResourceMetadata;
+import org.apache.sling.api.resource.ResourceResolver;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
@@ -41,6 +47,22 @@ public class FolderRelocatorTest {
     public void testInit() throws LoginException, DeserializeException, RepositoryException {
         FolderRelocator tool = new FolderRelocator();
         ProcessInstance instance = new ProcessInstanceImpl(getControlledProcessManager(), tool, "relocator test");
+        ResourceResolver rr = getMockResolver();
+        AbstractResourceImpl mockFolderA = new AbstractResourceImpl("/content/folderA", "", "", new ResourceMetadata());
+        when(rr.getResource("/content/folderA")).thenReturn(mockFolderA);
+        AbstractResourceImpl mockFolderB = new AbstractResourceImpl("/content/folderB", "", "", new ResourceMetadata());
+        when(rr.getResource("/content/folderB")).thenReturn(mockFolderB);
+        AbstractResourceImpl mockFolder = new AbstractResourceImpl("/content", "", "", new ResourceMetadata());
+        when(rr.getResource("/content")).thenReturn(mockFolder);
+        mockFolder.addChild(mockFolderA);
+        mockFolder.addChild(mockFolderB);
+        
+        Session ses = mock(Session.class);
+        when(rr.adaptTo(Session.class)).thenReturn(ses);
+        AccessControlManager acm = mock(AccessControlManager.class);
+        when(ses.getAccessControlManager()).thenReturn(acm);
+        when(acm.privilegeFromName(any())).thenReturn(mock(Privilege.class));
+        
         assertEquals("Folder relocator: relocator test", instance.getName());
         try {
             instance.init(getMockResolver(), Collections.EMPTY_MAP);
@@ -51,7 +73,9 @@ public class FolderRelocatorTest {
         Map<String, Object> values = new HashMap<>();
         values.put("sourcePaths", "/content/folderA");
         values.put("destinationPath", "/content/folderB");
-        instance.init(getMockResolver(), values);
+        values.put("mode", FolderRelocator.Mode.MOVE.toString());
+        instance.init(rr, values);
+        instance.run(rr);
     }
 
     private ControlledProcessManager getControlledProcessManager() throws LoginException {
