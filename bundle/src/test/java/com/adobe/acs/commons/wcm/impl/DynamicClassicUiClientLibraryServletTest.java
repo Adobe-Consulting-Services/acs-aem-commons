da7d805a286c382e7b3690551bf986ce046b3b1f
Steve Maris <steve_maris@hotmail.com>
2017-11-28 14:06:37 +0100

    Update unit-test #1187

--- bundle/src/test/java/com/adobe/acs/commons/wcm/impl/DynamicClassicUiClientLibraryServletTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/wcm/impl/DynamicClassicUiClientLibraryServletTest.java
@@ -22,6 +22,7 @@ package com.adobe.acs.commons.wcm.impl;
 import com.adobe.granite.ui.clientlibs.ClientLibrary;
 import com.adobe.granite.ui.clientlibs.HtmlLibraryManager;
 import com.adobe.granite.ui.clientlibs.LibraryType;
+import com.day.cq.commons.Externalizer;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -52,6 +53,7 @@ public class DynamicClassicUiClientLibraryServletTest {
     private static final Category LIMIT = new Category("acs-commons.cq-widgets.add-ons.classicui-limit-parsys", "/etc/clientlibs/limit");
     private static final Category PLACEHOLDER = new Category("acs-commons.cq-widgets.add-ons.classicui-parsys-placeholder", "/etc/clientlibs/placeholder");
     private static final Category CUSTOM = new Category("custom", "/etc/clientlibs/custom");
+    private static final Category CUSTOM_CONTEXT = new Category("custom-context", "/etc/clientlibs/custom");
 
     @Mock
     private SlingHttpServletRequest request;
@@ -65,6 +67,9 @@ public class DynamicClassicUiClientLibraryServletTest {
     @Mock
     private HtmlLibraryManager htmlLibraryManager;
 
+    @Mock
+    private Externalizer externalizer;
+
     private StringWriter writer;
 
     @Before
@@ -73,6 +78,8 @@ public class DynamicClassicUiClientLibraryServletTest {
         when(response.getWriter()).thenReturn(new PrintWriter(writer));
         when(request.getResourceResolver()).thenReturn(resourceResolver);
         when(resourceResolver.map(anyString())).then(i -> i.getArgumentAt(0, String.class));
+
+        when(externalizer.relativeLink(any(SlingHttpServletRequest.class), anyString())).thenAnswer(i -> i.getArgumentAt(1, String.class));
 
         when(htmlLibraryManager.getLibraries(any(String[].class), any(LibraryType.class), eq(true), eq(true))).thenAnswer(i -> {
             Set<ClientLibrary> result = new HashSet<>();
