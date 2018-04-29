60f57a05e71f36a7a62e7eb43762bb26671fc9ea
Brendan Robert <brobert@adobe.com>
2018-04-29 10:09:59 -0500

    More dev progress, almost 3/4 done now

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/RefreshFolderTumbnails.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/RefreshFolderTumbnails.java
@@ -16,64 +16,87 @@
 package com.adobe.acs.commons.mcp.impl.processes;
 
 import com.adobe.acs.commons.fam.ActionManager;
+import com.adobe.acs.commons.fam.actions.ActionBatch;
 import com.adobe.acs.commons.functions.CheckedFunction;
 import com.adobe.acs.commons.mcp.ProcessDefinition;
 import com.adobe.acs.commons.mcp.ProcessInstance;
+import com.adobe.acs.commons.mcp.form.FormField;
 import com.adobe.acs.commons.util.visitors.TreeFilteringResourceVisitor;
-import com.adobe.acs.commons.workflow.synthetic.impl.SyntheticWorkflowProcess;
 import com.day.cq.commons.jcr.JcrConstants;
+import com.day.cq.contentsync.handler.util.RequestResponseFactory;
 import com.day.cq.dam.api.DamConstants;
-import com.day.cq.dam.core.process.ThumbnailProcess;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.io.output.NullOutputStream;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.engine.SlingRequestProcessor;
 
 /**
- * Replace folder thumbnails under a user-definable set of circumstances
- * As a business user, I would like an easy way to scan and repair missing thumbnails, or just regenerate all thumbnails under a given tree of the DAM.
+ * Replace folder thumbnails under a user-definable set of circumstances As a business user, I would like an easy way to
+ * scan and repair missing thumbnails, or just regenerate all thumbnails under a given tree of the DAM.
  */
-
 public class RefreshFolderTumbnails extends ProcessDefinition {
+
     private static enum ThumbnailScanLogic {
         MISSING(RefreshFolderTumbnails::isThumbnailMissing),
         PLACEHOLDERS(RefreshFolderTumbnails::isThumbnailMissing,
                 RefreshFolderTumbnails::isPlaceholderThumbnail),
-        OUTDATED(RefreshFolderTumbnails::isThumbnailMissing, 
+        OUTDATED(RefreshFolderTumbnails::isThumbnailMissing,
                 RefreshFolderTumbnails::isPlaceholderThumbnail,
-                RefreshFolderTumbnails::isThumbnailContentsOutdated
-        ),
-        ALL(r->true);
-        
+                RefreshFolderTumbnails::isThumbnailContentsOutdated),
+        ALL_AUTOMATIC_OR_MISSING(RefreshFolderTumbnails::isThumbnailMissing,
+                RefreshFolderTumbnails::isThumbnailAutomatic),
+        ALL(r -> true);
+
         CheckedFunction<Resource, Boolean> test;
+
         ThumbnailScanLogic(CheckedFunction<Resource, Boolean>... tests) {
             this.test = CheckedFunction.or(tests);
         }
-        
+
         public boolean shouldReplace(Resource r) throws Exception {
             return this.test.apply(r);
         }
     }
-    
-    private final ThumbnailProcess thumbnailProcess;
-    private SyntheticWorkflowProcess syntheticProcess;
+
+    public static final String FOLDER_THUMBNAIL = "jcr:content/folderThumbnail";
+
+    private static final Map<String, Object> THUMBNAIL_PARAMS = new HashMap<String, Object>(){{
+        put("width", "200");
+        put("height", "120");
+    }};
     private static final int PLACEHOLDER_SIZE = 1024;
-    private String startingPath;
-    private ThumbnailScanLogic scanLogic = ThumbnailScanLogic.PLACEHOLDERS;
     
+
+    private RequestResponseFactory requestFactory;
+    private SlingRequestProcessor slingProcessor;
+    
+    @FormField(name="Starting Path")
+    private String startingPath = "/content/dam";
+    
+    @FormField(name="Mode")
+    private ThumbnailScanLogic scanMode = ThumbnailScanLogic.PLACEHOLDERS;
+
     private transient List<String> foldersToReplace = Collections.synchronizedList(new ArrayList<>());
-    
-    public RefreshFolderTumbnails(ThumbnailProcess process) {
-        this.thumbnailProcess = process;
+
+    public RefreshFolderTumbnails(RequestResponseFactory reqRspFactory, SlingRequestProcessor slingProcessor) {
+        this.requestFactory = reqRspFactory;
+        this.slingProcessor = this.slingProcessor;
     }
-    
+
     @Override
     public void init() throws RepositoryException {
-        SyntheticWorkflowProcess syntheticWorkflowProcess = new SyntheticWorkflowProcess(thumbnailProcess);
     }
 
     @Override
@@ -87,31 +110,40 @@ public class RefreshFolderTumbnails extends ProcessDefinition {
     public void storeReport(ProcessInstance instance, ResourceResolver rr) throws RepositoryException, PersistenceException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     private void scanFolders(ActionManager manager) {
         TreeFilteringResourceVisitor visitor = new TreeFilteringResourceVisitor();
         visitor.setBreadthFirstMode();
         visitor.setResourceVisitor((folder, level) -> {
             String path = folder.getPath();
             manager.deferredWithResolver(rrr -> {
-                if (scanLogic.shouldReplace(rrr.getResource(path))) {
+                if (scanMode.shouldReplace(rrr.getResource(path))) {
                     foldersToReplace.add(path);
-                }                
+                }
             });
         });
     }
-    
+
     private void removeOldThumbnails(ActionManager manager) {
-        // Remove folder/jcr:content/folderThumbnail
-        
+        ActionBatch batch = new ActionBatch(manager, 20);
+        foldersToReplace.forEach(path -> { 
+            batch.add(rr->rr.delete(rr.getResource(path + FOLDER_THUMBNAIL)));
+        });
+        batch.commitBatch();
     }
-    
+
     private void rebuildThumbnails(ActionManager manager) {
-        //Haven't figured out a clean way to regenerate the thumbnail but the .folderthumbnail.jpg selector/suffix does it from the browser.
-        // If the folder thumbnail generation is in a "public" API then it would make sense to call it directly, otherwise an internal Sling Request will have to suffice to trigger that servlet.
-        FolderPreviewUpdater 
+        foldersToReplace.forEach(path -> { 
+            manager.deferredWithResolver(rr -> rebuildThumbnail(rr, path));
+        });
     }
     
+    private void rebuildThumbnail(ResourceResolver rr, String folderPath) throws ServletException, IOException { 
+        HttpServletRequest req = requestFactory.createRequest("GET", folderPath + FOLDER_THUMBNAIL + ".png", THUMBNAIL_PARAMS);
+        HttpServletResponse res = requestFactory.createResponse(new NullOutputStream());
+        slingProcessor.processRequest(req, res, rr);
+    }
+
     private static boolean isThumbnailMissing(Resource damFolder) {
         Resource jcrContent = damFolder.getChild(JcrConstants.JCR_CONTENT);
         if (jcrContent == null) {
@@ -120,19 +152,23 @@ public class RefreshFolderTumbnails extends ProcessDefinition {
         Resource thumbnail = jcrContent.getChild(DamConstants.THUMBNAIL_NODE);
         return thumbnail == null;
     }
-    
+
     private static boolean isPlaceholderThumbnail(Resource damFolder) {
         Resource jcrContent = damFolder.getChild(JcrConstants.JCR_CONTENT);
         Resource thumbnail = jcrContent.getChild(DamConstants.THUMBNAIL_NODE);
         byte[] thumbnailData = thumbnail.getValueMap().get(JcrConstants.JCR_DATA, byte[].class);
-        return thumbnailData == null || thumbnailData.length  <= PLACEHOLDER_SIZE;
+        return thumbnailData == null || thumbnailData.length <= PLACEHOLDER_SIZE;
     }
-    
+
     private static boolean isThumbnailContentsOutdated(Resource damFolder) {
         //Look at /jcr:content/folderThumbnail/jcr:content/@dam:folderThumbnailPaths that list 3 images in that folder.
         //jcr:lastModified property in the jcr:content might be a useful determining factor if a thumbnail is older than the content of the folder
         //Confirm if all assets exist and if any modified dates are later than thumbnail
-        PreviewGenerator.validateExistingPreview
+//        PreviewGenerator.validateExistingPreview
+        return false;
+    }
+    
+    private static boolean isThumbnailAutomatic(Resource damFolder) {
         return false;
     }
 }
