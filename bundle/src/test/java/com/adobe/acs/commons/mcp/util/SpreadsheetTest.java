8d74b2ff8d10dc9b1780ec3f12828fb5e31c9813
Brendan Robert <brobert@adobe.com>
2018-05-02 19:10:58 -0500

    Corrected license headers

--- bundle/src/test/java/com/adobe/acs/commons/mcp/util/SpreadsheetTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/mcp/util/SpreadsheetTest.java
@@ -4,7 +4,6 @@
  * %%
  * Copyright (C) 2018 Adobe
  * %%
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,6 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
 package com.adobe.acs.commons.mcp.util;
 
