d923d58146049bf9d86452754c13835f33c6661a
Brendan Robert <brendan.robert@gmail.com>
2018-05-07 16:53:29 -0500

    Feature/sheet data handling improvements (#1352)
    * Improved Spreadsheet with new Variant and CompositeVariant implementations
    
    * Moved new data utilities into their own package and left a stub deprecated Spreadsheet class behind to be removed in the future.

--- bundle/src/test/java/com/adobe/acs/commons/data/SpreadsheetTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/data/SpreadsheetTest.java
@@ -17,14 +17,18 @@
  * limitations under the License.
  * #L%
  */
-package com.adobe.acs.commons.mcp.util;
+package com.adobe.acs.commons.data;
 
+import com.adobe.acs.commons.data.CompositeVariant;
+import com.adobe.acs.commons.data.Spreadsheet;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -37,14 +41,16 @@ import static org.junit.Assert.*;
  * Various basic tests of the spreadsheet utility class
  */
 public class SpreadsheetTest {
-    
+
     public SpreadsheetTest() {
     }
 
     static XSSFWorkbook testWorkbook;
-    static String[] header = new String[]{"path", "title", "someOtherCol"};
+    static String[] header = new String[]{"path", "title", "someOtherCol", "int-val@integer", "string-list1@string[]", "string-list2@string[;]",
+        "double-val@double", "array", "array", "array", "date-val@date"};
     static ByteArrayOutputStream workbookData = new ByteArrayOutputStream();
-    
+    static Date testDate = new Date();
+
     @BeforeClass
     public static void setUp() throws IOException {
         testWorkbook = new XSSFWorkbook();
@@ -54,6 +60,10 @@ public class SpreadsheetTest {
         createRow(sheet, "/test/a2", "A-2", "val");
         createRow(sheet, "/test/a1/a1a", "A-1-A", "val");
         createRow(sheet, "/test/a3/a3a", "A-3-A", "val");
+        XSSFRow valuesRow = createRow(sheet, "/some/types", "Types", "...", "12345", "one,two,three", "four;five;six", 
+                "12.345", "One Value", null, "Another Value");
+        XSSFCell dateCell = valuesRow.createCell(10);
+        dateCell.setCellValue(testDate);
         testWorkbook.write(workbookData);
         workbookData.close();
     }
@@ -75,7 +85,7 @@ public class SpreadsheetTest {
     @Test
     public void testGetRowCount() throws IOException {
         Spreadsheet instance = new Spreadsheet(new ByteArrayInputStream(workbookData.toByteArray()));
-        int expResult = 4;
+        int expResult = 5;
         int result = instance.getRowCount();
         assertEquals(expResult, result);
     }
@@ -97,9 +107,9 @@ public class SpreadsheetTest {
     @Test
     public void testGetDataRows() throws IOException {
         Spreadsheet instance = new Spreadsheet(new ByteArrayInputStream(workbookData.toByteArray()));
-        List<Map<String, String>> result = instance.getDataRows();
-        assertEquals(result.get(0).get("path"), "/test/a1");
-        assertEquals(result.get(3).get("path"), "/test/a3/a3a");
+        List<Map<String, CompositeVariant>> result = instance.getDataRowsAsCompositeVariants();
+        assertEquals("/test/a1", result.get(0).get("path").toPropertyValue());
+        assertEquals("/test/a3/a3a", result.get(3).get("path").toString());
     }
 
     /**
@@ -110,8 +120,8 @@ public class SpreadsheetTest {
         Spreadsheet instance = new Spreadsheet(false, new ByteArrayInputStream(workbookData.toByteArray()), "someOtherCol");
         List<String> required = instance.getRequiredColumns();
         assertEquals("someOtherCol", required.get(0));
-        List<Map<String,String>> result = instance.getDataRows();
-        assertEquals("/test/a2", result.get(0).get("path"));
+        List<Map<String, CompositeVariant>> result = instance.getDataRowsAsCompositeVariants();
+        assertEquals("/test/a2", result.get(0).get("path").toString());
     }
 
     /**
@@ -122,17 +132,32 @@ public class SpreadsheetTest {
         Spreadsheet instance = new Spreadsheet(true, new ByteArrayInputStream(workbookData.toByteArray()), "someOtherCol");
         List<String> required = instance.getRequiredColumns();
         assertEquals("someothercol", required.get(0));
-        List<Map<String,String>> result = instance.getDataRows();
-        assertEquals("/test/a2", result.get(0).get("path"));
+        List<Map<String, CompositeVariant>> result = instance.getDataRowsAsCompositeVariants();
+        assertEquals("/test/a2", result.get(0).get("path").toString());
     }
-    
+
+    @Test
+    public void testVariantTypes() throws IOException {
+        Spreadsheet instance = new Spreadsheet(true, new ByteArrayInputStream(workbookData.toByteArray()));
+        Map<String, CompositeVariant> values = instance.getDataRowsAsCompositeVariants().get(4);
+        assertEquals((Integer) 12345, values.get("int-val").toPropertyValue());
+        assertArrayEquals(new String[]{"one", "two", "three"}, (Object[]) values.get("string-list1").toPropertyValue());
+        assertArrayEquals(new String[]{"four", "five", "six"}, (Object[]) values.get("string-list2").toPropertyValue());
+        assertArrayEquals(new String[]{"One Value", "Another Value"}, (Object[]) values.get("array").toPropertyValue());
+        assertEquals(12.345, (Double) values.get("double-val").toPropertyValue(), 0.000001);
+        assertEquals(testDate, values.get("date-val").toPropertyValue());
+    }
+
     private static XSSFRow createRow(XSSFSheet sheet, String... values) {
         int rowNum = sheet.getPhysicalNumberOfRows();
         XSSFRow row = sheet.createRow(rowNum);
-        for (int i=0; i < values.length; i++) {
-            row.createCell(i).setCellValue(values[i]);
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == null) {
+                row.createCell(i);
+            } else {
+                row.createCell(i).setCellValue(values[i]);
+            }
         }
         return row;
     }
-    
 }
