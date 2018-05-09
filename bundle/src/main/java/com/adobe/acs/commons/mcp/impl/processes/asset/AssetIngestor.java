a312274ded2ee3e25344115c8a49a6c20f9a0a12
Kiran Murugulla <kiran.murugulla@hotmail.com>
2018-05-09 17:49:19 -0600

    Feature/asset ingestor foldername with hyphen (#1357)
    * adds preserve filename option, which selects between as-is naming or jcr-friendly asset naming
    
    * replacing folder node name default character with hyphen

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/asset/AssetIngestor.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/asset/AssetIngestor.java
@@ -54,9 +54,9 @@ public abstract class AssetIngestor extends ProcessDefinition {
 
     public static final String ALL_ASSETS = "All Assets";
     static final String[] AUTHORIZED_GROUPS = new String[]{
-        "administrators",
-        "asset-ingest",
-        "dam-administrators"
+            "administrators",
+            "asset-ingest",
+            "dam-administrators"
     };
 
     protected final transient MimeTypeService mimetypeService;
@@ -92,6 +92,14 @@ public abstract class AssetIngestor extends ProcessDefinition {
             options = "checked"
     )
     boolean inhibitWorkflow = true;
+
+    @FormField(
+            name = "Preserve Filename",
+            description = "If checked, file name is preserved as asset name.  If unchecked, asset name is converted to a JCR-friendly name.",
+            component = CheckboxComponent.class,
+            options = "checked"
+    )
+    boolean preserveFileName = true;
 
     @FormField(
             name = "Target JCR Folder",
@@ -282,6 +290,7 @@ public abstract class AssetIngestor extends ProcessDefinition {
         }
     }
 
+    @SuppressWarnings("squid:S3776")
     protected boolean createFolderNode(HierarchialElement el, ResourceResolver r) throws RepositoryException, PersistenceException {
         if (el == null || !el.isFolder()) {
             return false;
@@ -294,10 +303,14 @@ public abstract class AssetIngestor extends ProcessDefinition {
         Session s = r.adaptTo(Session.class);
         if (s.nodeExists(folderPath)) {
             Node folderNode = s.getNode(folderPath);
-            if (folderPath.equals(jcrBasePath) || (folderNode.hasProperty(JcrConstants.JCR_TITLE) && folderNode.getProperty(JcrConstants.JCR_TITLE).getString().equals(name))) {
+            Node folderContentNode = folderNode.hasNode(JcrConstants.JCR_CONTENT) ? folderNode.getNode(JcrConstants.JCR_CONTENT) : null;
+            if (folderNode.getPath().equals(jcrBasePath) || (null != folderContentNode
+                    && folderContentNode.hasProperty(JcrConstants.JCR_TITLE)
+                    && folderContentNode.getProperty(JcrConstants.JCR_TITLE).getString().equals(name))) {
                 return false;
             } else {
-                folderNode.setProperty(JcrConstants.JCR_TITLE, name);
+                folderContentNode = folderNode.addNode(JcrConstants.JCR_CONTENT,JcrConstants.NT_UNSTRUCTURED);
+                folderContentNode.setProperty(JcrConstants.JCR_TITLE, name);
                 r.commit();
                 r.refresh();
                 return true;
@@ -315,13 +328,21 @@ public abstract class AssetIngestor extends ProcessDefinition {
         }
         Node child = s.getNode(parentPath).addNode(el.getNodeName(), DEFAULT_FOLDER_TYPE);
         trackDetailedActivity(el.getNodePath(), "Create Folder", "Create folder", 0L);
+        setFolderTitle(child,folderPath,name);
         incrementCount(createdFolders, 1L);
-        if (!folderPath.equals(jcrBasePath)) {
-            child.setProperty(JcrConstants.JCR_TITLE, name);
-        }
         r.commit();
         r.refresh();
         return true;
+    }
+
+    private void setFolderTitle(Node child,String folderPath,String title) throws RepositoryException{
+        if (!folderPath.equals(jcrBasePath)) {
+            if(child.hasNode(JcrConstants.JCR_CONTENT)){
+                child.getNode(JcrConstants.JCR_CONTENT).setProperty(JcrConstants.JCR_TITLE, title);
+            }else{
+                child.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_UNSTRUCTURED).setProperty(JcrConstants.JCR_TITLE, title);
+            }
+        }
     }
 
     @SuppressWarnings("squid:S00112")
@@ -331,9 +352,17 @@ public abstract class AssetIngestor extends ProcessDefinition {
 
     protected CheckedConsumer<ResourceResolver> importAsset(final Source source, ActionManager actionManager) {
         return (ResourceResolver r) -> {
-            String path = source.getElement().getNodePath();
             createFolderNode(source.getElement().getParent(), r);
             actionManager.setCurrentItem(source.getElement().getItemName());
+            HierarchialElement el = source.getElement();
+            String path = source.getElement().getNodePath();
+            if(null != el && el.isFile() && el.getName().contains(".") && !preserveFileName){
+                String baseName = StringUtils.substringBeforeLast(el.getName(), ".");
+                String extension = StringUtils.substringAfterLast(el.getName(), ".");
+                path = (el.getParent() == null ? el.getJcrBasePath() : el.getParent().getNodePath()) + "/"
+                        + JcrUtil.createValidName(baseName,JcrUtil.HYPHEN_LABEL_CHAR_MAPPING,"-")
+                        + "." + JcrUtil.createValidName(extension,JcrUtil.HYPHEN_LABEL_CHAR_MAPPING,"-");
+            }
             handleExistingAsset(source, path, r);
         };
     }
@@ -434,11 +463,9 @@ public abstract class AssetIngestor extends ProcessDefinition {
         default String getNodeName() {
             String name = getName();
             if (isFile() && name.contains(".")) {
-                String baseName = StringUtils.substringBeforeLast(name, ".");
-                String extension = StringUtils.substringAfterLast(name, ".");
-                return JcrUtil.createValidName(baseName) + "." + JcrUtil.createValidName(extension);
+                return name;
             } else {
-                return JcrUtil.createValidName(name);
+                return JcrUtil.createValidName(name,JcrUtil.HYPHEN_LABEL_CHAR_MAPPING,"-");
             }
         }
     }
