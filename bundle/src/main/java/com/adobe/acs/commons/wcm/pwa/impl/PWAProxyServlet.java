1945639b962c3c58ca95dbe4d2434c3d8626e1c9
Avijit Tripathy <avtripat@adobe.com>
2018-07-10 17:17:34 +0530

    [feature/pwa-aem] Work done to handle what paths have to be cached and denied

--- bundle/src/main/java/com/adobe/acs/commons/wcm/pwa/impl/PWAProxyServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/wcm/pwa/impl/PWAProxyServlet.java
@@ -40,18 +40,16 @@ import java.util.Map;
                 "sling.servlet.selectors=pwa",
                 "sling.servlet.selectors=manifest",
                 "sling.servlet.selectors=service-worker",
-                "sling.servlet.methods="+ HttpConstants.METHOD_GET,
+                "sling.servlet.methods=" + HttpConstants.METHOD_GET,
                 "sling.servlet.resourceTypes=cq:Page"
         }
 )
 public class PWAProxyServlet extends SlingSafeMethodsServlet implements OptingServlet {
     private static final Logger log = LoggerFactory.getLogger(PWAProxyServlet.class);
     @Reference
-    private ResourceResolverFactory resolverFactory;
-
-    @Reference
     ConfMgr confMgr;
-
+    @Reference
+    private ResourceResolverFactory resolverFactory;
 
     @Override
     protected final void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws
@@ -59,24 +57,26 @@ public class PWAProxyServlet extends SlingSafeMethodsServlet implements OptingSe
 
         RequestPathInfo requestPathInfo = request.getRequestPathInfo();
 
-        boolean swRequest =false;
+        boolean swRequest = false;
 
-        String[] selectors =requestPathInfo.getSelectors();
-        for(String selector : selectors){
-            if("service-worker".equals(selector)){
+        String[] selectors = requestPathInfo.getSelectors();
+        for (String selector : selectors) {
+            if ("service-worker".equals(selector)) {
                 swRequest = true;
             }
 
         }
-        if("/".equalsIgnoreCase(requestPathInfo.getSuffix()) && "load".equalsIgnoreCase(requestPathInfo.getExtension())){
-            request.getRequestDispatcher(requestPathInfo.getResourcePath()+".html").forward(request, response);
+        if ("/".equalsIgnoreCase(requestPathInfo.getSuffix()) && "load".equalsIgnoreCase(requestPathInfo.getExtension())) {
+            request.getRequestDispatcher(requestPathInfo.getResourcePath() + ".html").forward(request, response);
         }
         Resource proxyResource = null;
         if ("/manifest.json".equalsIgnoreCase(requestPathInfo.getSuffix())) {
             response.getWriter().write(handleManifest(request, response));
 
-        } else if (swRequest){
-            proxyResource= request.getResourceResolver().getResource(handleServiceWorker(response));
+        } else if ("/cacheDetails.json".equalsIgnoreCase(requestPathInfo.getSuffix())) {
+            response.getWriter().write(handleCacheDetails(request, response));
+        } else if (swRequest) {
+            proxyResource = request.getResourceResolver().getResource(handleServiceWorker(response));
 
             if (proxyResource != null) {
                 final InputStream in = proxyResource.adaptTo(InputStream.class);
@@ -94,25 +94,61 @@ public class PWAProxyServlet extends SlingSafeMethodsServlet implements OptingSe
 
     }
 
+    private String handleCacheDetails(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        JSONObject jsonObject = new JSONObject();
+
+        ValueMap manifestSettings = getConfigProperties(request);
+        if (manifestSettings != null) {
+            try {
+                jsonObject.put("fallback", manifestSettings.get("fallbackUrl", "/"));
+                jsonObject.put("deniedHTMLCache", getCachePaths(manifestSettings, "deniedHTMLCache"));
+                jsonObject.put("deniedAssetsCache", getCachePaths(manifestSettings, "deniedAssetsCache"));
+                jsonObject.put("staticHTMLCache", getCachePaths(manifestSettings, "staticHTMLCache"));
+                jsonObject.put("staticAssetsCache", getCachePaths(manifestSettings, "staticAssetsCache"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return jsonObject.toString();
+    }
+
+    private JSONArray getCachePaths(ValueMap manifestSettings, String cachePaths) {
+        String[] paths = manifestSettings.get(cachePaths, new String[0]);
+        JSONArray jsonArray = new JSONArray();
+        for (String path : paths) {
+            if ("deniedHTMLCache".equals(cachePaths) || "staticHTMLCache".equals(cachePaths)) {
+                path += ".html";
+            }
+            jsonArray.put(path);
+        }
+        return jsonArray;
+    }
+
+
     private String handleServiceWorker(SlingHttpServletResponse response) {
         response.setContentType("application/javascript");
         response.setHeader("Content-Disposition", "attachment");
         response.setCharacterEncoding("UTF-8");
-        return  "/etc/clientlibs/acs-commons/clientlib-sw/service-worker.js";
+        return "/etc/clientlibs/acs-commons/clientlib-sw/service-worker.js";
     }
 
 
     /**
      * Example: HTTP GET http://localhost:4502/content/we-retail/us/en.pwa.load/manifest.json
-     *
+     * <p>
      * End-point for loading the PWA manifest.json in the context providing scope.
-     *
+     * <p>
      * This would load a JSON file available internally at some path specified at:
-     *
-     *  [cq:Page]/jcr:content/pwa/manifestPath = /apps/some/path/in/jcr/manifest.json
-     *
+     * <p>
+     * [cq:Page]/jcr:content/pwa/manifestPath = /apps/some/path/in/jcr/manifest.json
+     * <p>
      * This could let different PWA's on an AEM instance have different manifest implementations.
-     *
+     * <p>
      * TODO: Ideally this JSON could be built from edittable page properties instead of a file.
      *
      * @param request
@@ -125,7 +161,7 @@ public class PWAProxyServlet extends SlingSafeMethodsServlet implements OptingSe
         JSONObject jsonObject = new JSONObject();
 
         ValueMap manifestSettings = getConfigProperties(request);
-        if(manifestSettings !=null){
+        if (manifestSettings != null) {
             try {
                 jsonObject.put("name", manifestSettings.get("applicationName", "PWAName"));
                 jsonObject.put("short_name", manifestSettings.get("shortName", "PWA ShortName"));
@@ -148,33 +184,33 @@ public class PWAProxyServlet extends SlingSafeMethodsServlet implements OptingSe
 
         String[] iconImages = manifestSettings.get("src", new String[]{});
         String[] iconSizes = manifestSettings.get("size", new String[]{});
-        JSONArray jsonArray =new JSONArray();
-        if(iconImages.length == iconSizes.length){
-            for(int i=0; i< iconImages.length; i++){
+        JSONArray jsonArray = new JSONArray();
+        if (iconImages.length == iconSizes.length) {
+            for (int i = 0; i < iconImages.length; i++) {
                 JSONObject jsonObject = new JSONObject();
-                try{
+                try {
                     jsonObject.put("src", iconImages[i]);
                     jsonObject.put("type", "image/png");
                     jsonObject.put("sizes", iconSizes[i]);
-                }catch (JSONException ex){
+                } catch (JSONException ex) {
                     ex.printStackTrace();
                 }
                 jsonArray.put(jsonObject);
             }
         }
 
-        return  jsonArray;
+        return jsonArray;
     }
 
     /**
      * End-point for loading the PWA service-worker.js in the context providing scope.
-     *
+     * <p>
      * Example: HTTP GET http://localhost:4502/content/we-retail/us/en.pwa.load/service-worker.js
-     *
+     * <p>
      * This would load a JS file available internally at some path specified at:
-     *
-     *  [cq:Page]/jcr:content/pwa/serviceWorkerJSPath = /apps/some/path/in/jcr/service-worker.js
-     *
+     * <p>
+     * [cq:Page]/jcr:content/pwa/serviceWorkerJSPath = /apps/some/path/in/jcr/service-worker.js
+     * <p>
      * This could let different PWA's on an AEM instance have different service worker implementations.
      *
      * @param request
@@ -186,38 +222,39 @@ public class PWAProxyServlet extends SlingSafeMethodsServlet implements OptingSe
         response.setCharacterEncoding("UTF-8");
         String navRootPath = StringUtils.EMPTY;
         ValueMap configProps = getConfigProperties(request);
-        if(configProps != null && configProps.containsKey("rootPath")){
+        if (configProps != null && configProps.containsKey("rootPath")) {
             navRootPath = configProps.get("rootPath").toString();
         }
         JSONObject jsonObject = new JSONObject();
-        try{
-            jsonObject.put("path", navRootPath) ;
-        }catch (JSONException ex){
+        try {
+            jsonObject.put("path", navRootPath);
+        } catch (JSONException ex) {
             ex.printStackTrace();
         }
-        return  jsonObject.toString();
+        return jsonObject.toString();
     }
 
     private ValueMap getConfigProperties(SlingHttpServletRequest request) {
-        PageManager pageManager = getServiceResolver().adaptTo(PageManager.class);
-       // PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
+        ResourceResolver serviceResolver = getServiceResolver();
+        PageManager pageManager = serviceResolver.adaptTo(PageManager.class);
+        // PageManager pageManager = request.getResourceResolver().adaptTo(PageManager.class);
         Page page = pageManager.getContainingPage(request.getResource());
-        Conf conf = confMgr.getConf(page.adaptTo(Resource.class), getServiceResolver());
+        Conf conf = confMgr.getConf(page.adaptTo(Resource.class), serviceResolver);
         return conf.getItem("cloudconfigs/pwa/pwa-configuration");
 
 
     }
+
     private ResourceResolver getServiceResolver() {
         final Map<String, Object> authInfo =
                 Collections.singletonMap(ResourceResolverFactory.SUBSERVICE,
                         "pwa-service-handler");
         ResourceResolver resourceResolver = null;
-        try{
-        resourceResolver = resolverFactory.getServiceResourceResolver(authInfo);}
-        catch (Exception e){
+        try {
+            resourceResolver = resolverFactory.getServiceResourceResolver(authInfo);
+        } catch (Exception e) {
             log.debug(e.getMessage());
         }
-
 
 
         return resourceResolver;
