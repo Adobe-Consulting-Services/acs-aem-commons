1d2e0a3cf0a2b153ac66f6c59c7eecc101630381
david g <davidjgonzalez@gmail.com>
2018-08-01 08:18:16 -0400

    #1435 - Adjusted CPU limit to 75% on Throttled Task Runner

--- bundle/src/main/java/com/adobe/acs/commons/fam/impl/ThrottledTaskRunnerImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/fam/impl/ThrottledTaskRunnerImpl.java
@@ -61,7 +61,7 @@ import java.util.concurrent.TimeUnit;
 @Properties({
     @Property(name = "jmx.objectname", value = "com.adobe.acs.commons.fam:type=Throttled Task Runner", propertyPrivate = true),
     @Property(name = "max.threads", label = "Max threads", description = "Default is 4, recommended not to exceed the number of CPU cores",value = "4"),
-    @Property(name = "max.cpu", label = "Max cpu %", description = "Range is 0..1; -1 means disable this check", value = "0.50"),
+    @Property(name = "max.cpu", label = "Max cpu %", description = "Range is 0..1; -1 means disable this check", value = "0.75"),
     @Property(name = "max.heap", label = "Max heap %", description = "Range is 0..1; -1 means disable this check", value = "0.85"),
     @Property(name = "cooldown.wait.time", label = "Cooldown time", description="Time to wait for cpu/mem cooldown between checks", value = "100"),
     @Property(name = "task.timeout", label = "Watchdog time", description="Maximum time allowed (in ms) per action before it is interrupted forcefully. Defaults to 1 hour.", value = "3600000"),})
@@ -293,7 +293,7 @@ public class ThrottledTaskRunnerImpl extends AnnotatedStandardMBean implements T
         Dictionary<?, ?> properties = componentContext.getProperties();
         int defaultThreadCount = Math.max(1, Runtime.getRuntime().availableProcessors()/2);
 
-        maxCpu = PropertiesUtil.toDouble(properties.get("max.cpu"), 0.5);
+        maxCpu = PropertiesUtil.toDouble(properties.get("max.cpu"), 0.75);
         maxHeap = PropertiesUtil.toDouble(properties.get("max.heap"), 0.85);
         maxThreads = PropertiesUtil.toInteger(properties.get("max.threads"), defaultThreadCount);
         cooldownWaitTime = PropertiesUtil.toInteger(properties.get("cooldown.wait.time"), 100);
