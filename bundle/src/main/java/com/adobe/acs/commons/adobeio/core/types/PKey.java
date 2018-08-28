a763596e475fc36416a641836d3467e3dba5b41d
Feike Visser <fvisser@adobe.com>
2018-08-28 16:08:41 +0200

    Space correction

--- bundle/src/main/java/com/adobe/acs/commons/adobeio/core/types/PKey.java
+++ bundle/src/main/java/com/adobe/acs/commons/adobeio/core/types/PKey.java
@@ -23,5 +23,5 @@ package com.adobe.acs.commons.adobeio.core.types;
  * This interface is used to specify the pkey in the operation
  */
 public interface PKey {
-    String getValue();
+   String getValue();
 }
