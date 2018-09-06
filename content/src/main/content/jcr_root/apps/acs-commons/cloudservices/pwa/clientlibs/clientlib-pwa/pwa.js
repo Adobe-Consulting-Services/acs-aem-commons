4556cfd14175154503aaa597013bae9f13a813cc
Avijit Tripathy <avtripat@adobe.com>
2018-09-07 00:57:45 +0530

    [feature/pwa-aemv2] Ask for the latest Root SW and cache busting implemented to check for new version without indexedB

--- content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-pwa/pwa.js
+++ content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-pwa/pwa.js
@@ -1,8 +1,80 @@
 if ('serviceWorker' in navigator) {
     var pwaRoot = document.querySelector('meta[name="pwa.root"]').getAttribute('value'),
-        configPath = encodeURIComponent(pwaRoot + '.pwa.service-worker.json');
+        configPath = encodeURI(pwaRoot + '.pwa.service-worker.json');
 
+    var rootSW = ''; //fetchRootPath(configPath);
+    fetch(configPath).then(function(response) {
+        return response.json();
+    }).then(function(res) {
+        // read the root SW path; currently the fallback[0] is the rootPath for testing purpose
+        rootSW = res.fallback[0].pattern;
+        //register SW only if the cached version has changed
+        if (rootSW) {
+            clearCacheCheck(res, rootSW, configPath);
+            /*.then(function() {
+                 
+                    loadSW(rootSW, configPath);
+                 
+            });*/
+
+        }
+    });
+}
+
+function clearCacheCheck(resp, rootSW, configPath) {
+    caches.has(resp.cache_name)
+        .then(function(hasCacheName) {
+            if (navigator.serviceWorker.controller === null) { // no SW exists; load one 
+                loadSW(rootSW, configPath);
+            } else {
+                if (!hasCacheName) {
+                    updateServiceWorker().then(function() {
+
+                        clearOldCache()
+                            .then(function() {
+                                navigator.serviceWorker.controller = null;
+                                console.log('SW Cleared');
+                                loadSW(rootSW, configPath);
+                            });
+                    });
+
+                }
+            }
+
+        });
+}
+
+function clearOldCache() {
+
+    return caches.keys()
+        .then(function(keyList) {
+
+            return Promise.all(keyList.map(function(key) {
+                //delete cache keys
+                return caches.delete(key);
+            }));
+        });
+}
+
+function loadSW(rootSW, configPath) {
+    var time = new Date().getTime();
     navigator.serviceWorker
-        .register(pwaRoot + '.pwa.service-worker.js?config=' + configPath)
-        .then(function() { console.debug('PWA Service Worker Registered'); });
+        .register(rootSW + '.pwa.service-worker.' + time + '.js?config=' + configPath)
+        .then(function() {
+            console.log('PWA Service Worker Registered');
+        })
+        .catch(function(error) {
+            console.error('Failed to load SW')
+        });
+}
+
+function updateServiceWorker(rootSW, configPath) {
+    return navigator.serviceWorker.getRegistration().then(
+
+        function(registration) {
+
+            return registration.unregister();
+
+        })
+
 }
--- content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-serviceworker/service-worker.js
+++ content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-serviceworker/service-worker.js
@@ -16,8 +16,12 @@ function isCacheable(request) {
     if (request.method !== "GET") {
         return false;
     }
-
-    (config.no_cache || []).forEach(function (pattern) {
+    if (!config.no_cache || !Array.isArray(config.no_cache)) {
+        config.no_cache = [];
+    }
+    config.no_cache.push('service-worker');
+    config.no_cache.push('.pwa.');
+    (config.no_cache || []).forEach(function(pattern) {
         if (cacheable && request.url.match(pattern)) {
             cacheable = false;
             //console.log("Unable to cache [ " + request.url + " ] due to pattern [ " + pattern + " ]");
@@ -31,7 +35,7 @@ function isCacheable(request) {
 function getFallback(request) {
     var fallback = null;
 
-    (config.fallback || []).forEach(function (entry) {
+    (config.fallback || []).forEach(function(entry) {
         if (!fallback && request.url.match(entry.pattern)) {
             fallback = entry.path;
         }
@@ -47,12 +51,12 @@ function getFallback(request) {
 function getServiceWorkerConfig() {
     var configJson = new URL(location).searchParams.get('config');
 
-    return fetch(configJson).then(function (response) {
-        return response.json().then(function (json) {
+    return fetch(configJson).then(function(response) {
+        return response.json().then(function(json) {
             config = json;
- 
+            console.log("In SW ", config);
             return json;
- 
+
         });
     });
 }
@@ -66,11 +70,12 @@ function getServiceWorkerConfig() {
  * - Request the Fallback URLs
  * - Request the Pre-cache URLs
  */
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
+    console.log('Registering SW');
     e.waitUntil(
-        init().then(function () {
-            caches.open(config.cache_name).then(function (cache) {
-                var urlsToCache = config.fallback.map(function (entry) {
+        init().then(function() {
+            caches.open(config.cache_name).then(function(cache) {
+                var urlsToCache = config.fallback.map(function(entry) {
                     return entry.path;
                 }).concat(config.pre_cache);
 
@@ -79,14 +84,62 @@ self.addEventListener('install', function (e) {
         })
     );
 });
+self.addEventListener("activate", function(event) {
 
-self.addEventListener('fetch', function (event) {
+    console.log("Activated SW");
+              event.waitUntil(function() {
+                        caches.keys().then(function(keyList) {
+                            return Promise.all(keyList.map(function(key) {
+                                if (key !== config.cache_name) {
+                                    console.log(key);
+                                    return caches.delete(key);
+                                }
+                            }));
+
+                        });
+                    });
+
+    return self.clients.claim();
+
+});
+
+self.addEventListener('fetch', function(event) {
     if (!isCacheable(event.request)) {
         return;
     }
-
+// network then cache fallback
     event.respondWith(
-        caches.open(config.cache_name).then(function(cache) {
+        fetch(event.request)
+        .then(function(response) {
+            return caches.open(config.cache_name)
+                .then(function(cache) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+        }).catch(function(err) {
+            return cachedResponse(event);
+        })
+    );
+
+
+});
+
+function cachedResponse(event) {
+
+    return caches.match(event.request).then(function(resp) {
+        if (resp) {
+            console.log('Resolved respone', resp);
+            return resp;
+        } else {
+            console.log('chek for fallback for ', event.request);
+            return caches.match(getFallback(event.request));
+        }
+
+    });
+}
+
+/*
+caches.open(config.cache_name).then(function(cache) {
             return cache.match(event.request).then(function (cachedResponse) {
                 return cachedResponse || fetch(event.request).then(function(response) {
                     cache.put(event.request, response.clone());
@@ -98,5 +151,4 @@ self.addEventListener('fetch', function (event) {
                 });
             });
         })
-    );
-});
+*/
