714b608a5d2086cea5b2f39ac9bedf8a1c90db79
Brendan Robert <brobert@adobe.com>
2018-10-09 01:02:05 -0500

    Restore expected behavior for radio component in MCP (don't set fieldDescription)

--- bundle/src/main/java/com/adobe/acs/commons/mcp/form/RadioComponent.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/form/RadioComponent.java
@@ -67,6 +67,7 @@ public abstract class RadioComponent extends FieldComponent {
         getComponentMetadata().put("vertical", hasOption("vertical"));
         getComponentMetadata().put("text", getFieldDefinition().name());
         getComponentMetadata().remove("fieldLabel");
+        getComponentMetadata().remove("fieldDescription");
     }
 
     @Override
