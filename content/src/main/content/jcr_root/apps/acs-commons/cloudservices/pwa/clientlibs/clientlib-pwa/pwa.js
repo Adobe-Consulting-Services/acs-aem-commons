6d679e22a54770a16c7c3faf85651e041b9a9529
Avijit Tripathy <avtripat@adobe.com>
2018-10-20 00:58:07 +0530

    [feature/pwa-aemdemo] Refactored for fallback implementation

--- content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-pwa/pwa.js
+++ content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-pwa/pwa.js
@@ -1,129 +1,79 @@
-if ('serviceWorker' in navigator) {
-    var pwaRoot = document.querySelector('meta[name="pwa.root"]').getAttribute('value'),
-        configPath = encodeURI(pwaRoot + '.pwa.service-worker.json');
+(function () {
+    if ('serviceWorker' in navigator) {
+        var pwaRoot = document.querySelector('meta[name="pwa.root"]').getAttribute('value'),
+            configPath = encodeURI(pwaRoot + '.pwa.service-worker.json');
 
-    var rootSW = ''; //fetchRootPath(configPath);
-    fetch(configPath).then(function(response) {
-        return response.json();
-    }).then(function(res) {
-        // read the root SW path; currently the fallback[0] is the rootPath for testing purpose
-         
-        //register SW only if the cached version has changed
-        if (res && res.scope) {
-            registerSW(res, configPath);
-        }
-    });
-}
-
-function registerSW(resp, configPath) {
-    checkIDB(resp.version).then(function() {
-                        loadSW(resp.scope, configPath, resp.version);
-                    });/*
-    caches.has(resp.cache_name)
-        .then(function(hasCacheName) {
-            if (navigator.serviceWorker.controller === null) { // no SW exists; load one 
-                loadSW(resp.scope, configPath, resp.version);
-            } else {
-                if (!hasCacheName && navigator.onLine) {
-                     updateServiceWorker().then(function() {
-
-                        clearOldCache()
-                            .then(function() {
-                                navigator.serviceWorker.controller = null;
-                                loadSW(resp.scope, configPath, resp.version);
-                            });
-                    }); 
-                    
-
-                }
+        fetch(configPath).then(function (response) {
+            return response.json();
+        }).then(function (res) {
+            if (res && res.scope) {
+                registerSW(res.scope, res.version);
             }
-
-        });*/
-}
-
-function clearOldCache() {
-
-    return caches.keys()
-        .then(function(keyList) {
-
-            return Promise.all(keyList.map(function(key) {
-                //delete cache keys
-                return caches.delete(key);
-            }));
         });
-}
-
-function loadSW(rootSW, configPath, version) {
-    navigator.serviceWorker
-        .register(rootSW + '.pwa.service-worker.V' + version + '.js')
-        .then(function() {
-            console.log('PWA Service Worker Registered');
-        })
-        .catch(function(error) {
-            console.error('Failed to load SW');
-        });
-}
-
-function updateServiceWorker(rootSW, configPath) {
-    return navigator.serviceWorker.getRegistration().then(
-
-        function(registration) {
-
-            return registration.unregister();
-
-        });
-
-}
-
-
-function checkIDB(versionNum) {
-    if (!('indexedDB' in window)) {
-        console.log('This browser doesn\'t support IndexedDB');
-        return;
     }
 
-    var dbPromise = idb.open('pwa-aem-db', 1, function(db) {
-        console.log('database created ');
-        if (!db.objectStoreNames.contains('aem-pwa-store')) {
-            db.createObjectStore('aem-pwa-store', {
-                keyPath: 'pwaStore'
-            });
-        }
-    });
+    function registerSW(scope, cacheVersion) {
+        checkIDB(cacheVersion).then(function () {
+            loadSW(scope, cacheVersion);
+        });
+    }
 
-    return dbPromise.then(function(db) {
-        var version = versionNum;
-        var tx = db.transaction('aem-pwa-store', 'readwrite');
-        var store = tx.objectStore('aem-pwa-store');
-        var item = {
-            pwaStore: 'pwa-version-store', 
-            version: version, 
-            configPath: configPath
-        };
-        return store.get('pwa-version-store').then(function(data) {
-            if (!data || data.version !== version) {
-                store.clear();
-                store.put(item);
-                updateCacheAndSW();
-				return tx.complete;
+    function deregisterSW() {
+        navigator.serviceWorker.getRegistrations().then(function (registrations) {
+            for (let registration of registrations) {
+                registration.unregister()
+            }
+        });
+    }
+
+    /* load a service worker under a given path with the version number  */
+    function loadSW(swRootPath, version) {
+        navigator.serviceWorker
+            .register(swRootPath + '.pwa.service-worker.V' + version + '.js')
+            .then(function () {
+                console.log('PWA Service Worker Registered');
+            })
+            .catch(function () {
+                console.error('Failed to load SW');
+            });
+    }
+
+    /* Check the versionNum matches in IndexedDb else unregister the Service worker */
+    function checkIDB(versionNum) {
+        if (!('indexedDB' in window)) {
+            console.log('This browser doesn\'t support IndexedDB');
+            return;
+        }
+        var dbPromise = idb.open('pwa-aem-db', 1, function (db) {
+            console.log('database created ');
+            if (!db.objectStoreNames.contains('aem-pwa-store')) {
+                db.createObjectStore('aem-pwa-store', {
+                    keyPath: 'pwaStore'
+                });
             }
         });
 
+        return dbPromise.then(function (db) {
+            var version = versionNum;
+            var tx = db.transaction('aem-pwa-store', 'readwrite');
+            var store = tx.objectStore('aem-pwa-store');
+            var item = {
+                pwaStore: 'pwa-version-store',
+                version: version,
+                configPath: configPath
+            };
+            return store.get('pwa-version-store').then(function (data) {
+                if (!data || data.version !== version) {
+                    store.clear();
+                    store.put(item);
+                    deregisterSW();
+                    return tx.complete;
+                }
+            });
 
-    });
+
+        });
+    }
 
 
-
-
-}
-
-function updateCacheAndSW(){
-	caches.delete('STATIC_CACHE');
-    caches.delete('DYNAMIC_CACHE');
-    navigator.serviceWorker.getRegistrations().then(function(registrations) {
- 		for(let registration of registrations) {
-  			registration.unregister()
-		} 
-    });
-
-}
+})();
--- content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-serviceworker/service-worker.js
+++ content/src/main/content/jcr_root/apps/acs-commons/cloudservices/pwa/clientlibs/clientlib-serviceworker/service-worker.js
@@ -1,209 +1,199 @@
-/* Global state */
- 
-var config = {};
+(function () {
 
-/* Helpers */
+    /* Global state */
+    var config = {};
 
-function init() {
-    return clearCaches()
-    .then(function(){
-        return getServiceWorkerConfig();    
-    });
-    
-}
+    /* Helpers */
 
-function isCacheable(request) {
-    var cacheable = true;
-
-    if (request.method !== "GET") {
-        return false;
-    }
-    if (!config.no_cache || !Array.isArray(config.no_cache)) {
-        config.no_cache = [];
-    }
-    config.no_cache.push('service-worker');
-    config.no_cache.push('.pwa.');
-    config.no_cache.push('manifest');
-    (config.no_cache || []).forEach(function(pattern) {
-        if (cacheable && request.url.match(pattern)) {
-            cacheable = false;
-            //console.log("Unable to cache [ " + request.url + " ] due to pattern [ " + pattern + " ]");
-        }
-    });
-
-    return cacheable;
-}
-
-
-function getFallback(request) {
-    var fallback = null;
-
-    (config.fallback || []).forEach(function(entry) {
-        if (!fallback && request.url.match(entry.pattern)) {
-            fallback = entry.path;
-        }
-    });
-
-    if (!fallback) {
-        fallback = config.fallback_default;
-    }
-
-    return fallback;
-}
-
-function getServiceWorkerConfig() { 
-    return getStoreData()
-        .then(function(storeData){
-            console.log('storeData: ', storeData);
-            return fetch(storeData[0].configPath)
-                .then(function(response) {
-                    return response.json()
-                        .then(function(json) {
-                            config = json;
-                            console.log("In SW ", config);
-                            return json;
-                    });
-                });         
-    });
-    
-    
-}
-
-function getStoreData(){
-    var dbPromise = idb.open('pwa-aem-db', 1, function(db){});
-    return dbPromise.then(function(db) {
-        console.log('db', db);
-        var tx = db.transaction('aem-pwa-store', 'readonly');
-        var store = tx.objectStore('aem-pwa-store');
-        console.log('store', store);
-        return store.getAll();
-    });
-
-    
-}
-
-/* Events */
-
-/**
- * Install should initialize the cache..
- * - Get the SW Config
- * - Set the cacheName
- * - Request the Fallback URLs
- * - Request the Pre-cache URLs
- */
-self.addEventListener('install', function(e) {
-    console.log('Registering SW');
-    e.waitUntil(
-        init().then(function(configObject) {
-            console.log('configObject', configObject);
-            caches.open(configObject.cache_name)
-                .then(function(cache) {
-                var urlsToCache = configObject.fallback.map(function(entry) {
-                    return entry.path+'.html';
-                }).concat(configObject.pre_cache);
-                    
-                return cache.addAll(urlsToCache);
+    function init() {
+        return clearCaches()
+            .then(function () {
+                return getServiceWorkerConfig();
             });
-        })
-    );
-});
-self.addEventListener("activate", function(event) {
 
-    
-              event.waitUntil(function() {
-                        return clearCaches();
-                    });
-   return self.clients.claim();
-
-});
-
-function clearCaches(){
-    return caches.keys().then(function(keyList) {
-                            return Promise.all(keyList.map(function(key) {
-                                if (key !== config.cache_name) {
-                                    console.log(key);
-                                    return caches.delete(key);
-                                }
-                            }));
-
-                        });
-}
-
-self.addEventListener('fetch', function(event) {
-    if (!isCacheable(event.request)) {
-        return;
     }
-    if(!config.cache_name){
-        getServiceWorkerConfig()
-            .then(function(){
-            useSWCacheStrategy(event);
+
+    /* check if request is cacheable */
+    function isCacheable(request) {
+        var cacheable = true;
+
+        if (request.method !== "GET") {
+            return false;
+        }
+        if (!config.no_cache || !Array.isArray(config.no_cache)) {
+            config.no_cache = [];
+        }
+        config.no_cache.push('service-worker');
+        config.no_cache.push('.pwa.');
+        config.no_cache.push('manifest');
+        (config.no_cache || []).forEach(function (pattern) {
+            if (cacheable && request.url.match(pattern)) {
+                cacheable = false;
+            }
         });
-    }else{
-        useSWCacheStrategy(event);
+
+        return cacheable;
     }
-});
-function useSWCacheStrategy(event){
-    // cache then network fallback
-    event.respondWith(
-        caches.open(config.cache_name).then(function(cache) {
-            return cache.match(event.request).then(function (cachedResponse) {
-                return cachedResponse || fetch(event.request)
-                    .then(function(response) {
-                       cache.put(event.request, response.clone());
-                            return response;
-                }).catch(function() {
-                    return cache.match(getFallback(event.request))
-                        .then(function(cachedResponse) {
-                            return cachedResponse;
-                    });
-                });
-            });
-        })
-   
-    );
 
-    // network then cache fallback
-    /*
+    /* Get the fallback url given the request pattern */
+    function getFallback(request) {
+        var fallback = null;
+        (config.fallback || []).forEach(function (entry) {
+            if (!fallback && request.url.match(entry.pattern)) {
+                fallback = entry.path;
+            }
+        });
 
-         fetch(event.request)
-        .then(function(response) {
-            return caches.open(config.cache_name)
-                .then(function(cache) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-        }).catch(function(err) {
-            return cachedResponse(event);
-        })
-*/
-
-}
-
-function cachedResponse(event) {
-
-    return caches.match(event.request).then(function(resp) {
-        if (resp) {
-            console.log('Resolved response', resp);
-            return resp;
-        } else {
-            console.log('check for fallback for ', event.request);
-            return caches.match(getFallback(event.request));
+        if (!fallback && config.fallback.length > 0) {
+            fallback = config.fallback[0].path;
         }
 
-    });
-}
+        return request.headers.get('accept').includes('text/html') ? fallback + '.html' : fallback;
+    }
 
-/*
-caches.open(config.cache_name).then(function(cache) {
-            return cache.match(event.request).then(function (cachedResponse) {
-                return cachedResponse || fetch(event.request).then(function(response) {
-                    cache.put(event.request, response.clone());
-                    return response;
-                }).catch(function() {
-                    return cache.match(getFallback(event.request)).then(function(cachedResponse) {
-                        return cachedResponse;
+    /* The config object as authored in the server */
+    function getServiceWorkerConfig() {
+        return getStoreData()
+            .then(function (storeData) {
+                console.log('storeData: ', storeData);
+                return fetch(storeData[0].configPath)
+                    .then(function (response) {
+                        return response.json()
+                            .then(function (json) {
+                                config = json;
+                                return json;
+                            });
                     });
-                });
             });
-        })
-*/
+
+
+    }
+
+    /* Get all the data available in IndexedDb */
+    function getStoreData() {
+        var dbPromise = idb.open('pwa-aem-db', 1, function (db) { });
+        return dbPromise.then(function (db) {
+            var tx = db.transaction('aem-pwa-store', 'readonly');
+            var store = tx.objectStore('aem-pwa-store');
+            return store.getAll();
+        });
+
+
+    }
+
+    /* Clear the Application cache */
+    function clearCaches() {
+        return caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== config.cache_name) {
+                    console.log(key);
+                    return caches.delete(key);
+                }
+            }));
+
+        });
+    }
+
+    /* Get the cached response or the fallback
+        used in case of Cache then Network stategy */
+    function cachedResponse(event) {
+        return caches.match(event.request).then(function (resp) {
+            if (resp) {
+                return resp;
+            } else {
+                return caches.match(getFallback(event.request));
+            }
+
+        });
+    }
+
+    /* Strategy used by the SW to get the response */
+    function useSWCacheStrategy(event) {
+        // cache then network fallback
+        event.respondWith(
+            caches.open(config.cache_name).then(function (cache) {
+                return cache.match(event.request).then(function (cachedResponse) {
+                    return cachedResponse || fetch(event.request)
+                        .then(function (response) {
+                            cache.put(event.request, response.clone());
+                            return response;
+                        }).catch(function () {
+                            return cache.match(getFallback(event.request))
+                                .then(function (cachedResponse) {
+                                    return cachedResponse;
+                                });
+                        });
+                });
+            })
+
+        );
+
+        // network then cache fallback
+        /*
+    
+             fetch(event.request)
+            .then(function(response) {
+                return caches.open(config.cache_name)
+                    .then(function(cache) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+            }).catch(function(err) {
+                return cachedResponse(event);
+            })
+    */
+
+    }
+
+    /* Events */
+
+    /**
+     * Install should initialize the cache..
+     * - Get the SW Config
+     * - Set the cacheName
+     * - Request the Fallback URLs
+     * - Request the Pre-cache URLs
+     */
+
+    self.addEventListener('install', function (e) {
+        console.log('Registering SW');
+        e.waitUntil(
+            init().then(function (configObject) {
+                console.log('configObject', configObject);
+                caches.open(configObject.cache_name)
+                    .then(function (cache) {
+                        var urlsToCache = configObject.fallback.map(function (entry) {
+                            return entry.path + '.html';
+                        }).concat(configObject.pre_cache);
+
+                        return cache.addAll(urlsToCache);
+                    });
+            })
+        );
+    });
+    self.addEventListener("activate", function (event) {
+        event.waitUntil(function () {
+            return clearCaches();
+        });
+        return self.clients.claim();
+    });
+
+
+    self.addEventListener('fetch', function (event) {
+        if (!isCacheable(event.request)) {
+            return;
+        }
+        if (!config.cache_name) {
+            getServiceWorkerConfig()
+                .then(function () {
+                    useSWCacheStrategy(event);
+                });
+        } else {
+            useSWCacheStrategy(event);
+        }
+    });
+
+})();
+
+
+
