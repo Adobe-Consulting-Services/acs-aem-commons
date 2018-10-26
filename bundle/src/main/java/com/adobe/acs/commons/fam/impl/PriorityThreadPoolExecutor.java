fb39e0149975e192e683c7c745447a38ee0db13d
Sean Steimer <ssteimer@adobe.com>
2018-10-26 09:43:50 -0700

    added java docs

--- bundle/src/main/java/com/adobe/acs/commons/fam/impl/PriorityThreadPoolExecutor.java
+++ bundle/src/main/java/com/adobe/acs/commons/fam/impl/PriorityThreadPoolExecutor.java
@@ -7,23 +7,39 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * A custom extension of the thread pool executor which uses our {@link TimedRunnableFuture} implementation.
+ * This ensures that deferred tasks still respect the priority and don't cause errors when those are cast to {@link Comparable}.
+ */
 public class PriorityThreadPoolExecutor extends ThreadPoolExecutor {
 
+    /**
+     * {@inheritDoc}
+     */
     public PriorityThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
             BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public PriorityThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
             BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public PriorityThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
             BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public PriorityThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit,
             BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory, RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
--- bundle/src/main/java/com/adobe/acs/commons/fam/impl/TimedRunnableFuture.java
+++ bundle/src/main/java/com/adobe/acs/commons/fam/impl/TimedRunnableFuture.java
@@ -4,14 +4,23 @@ package com.adobe.acs.commons.fam.impl;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
+/**
+ * A runnable future for {@link TimedRunnable} which implements comparable for the purpsoe of priority execution.
+ */
 public class TimedRunnableFuture extends FutureTask implements Comparable {
 
     private TimedRunnable timedRunnable;
 
+    /**
+     * {@inheritDoc}
+     */
     public TimedRunnableFuture(Callable callable) {
         super(callable);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public TimedRunnableFuture(Runnable runnable, Object result) {
         super(runnable, result);
         if(runnable instanceof TimedRunnable) {
