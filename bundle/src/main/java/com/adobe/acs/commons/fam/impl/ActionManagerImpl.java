a2d3813ca08c38a724d7d71418a258203b246758
Brendan Robert <brobert@adobe.com>
2018-10-30 13:24:42 -0500

    Added missing import

--- bundle/src/main/java/com/adobe/acs/commons/fam/impl/ActionManagerImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/fam/impl/ActionManagerImpl.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
