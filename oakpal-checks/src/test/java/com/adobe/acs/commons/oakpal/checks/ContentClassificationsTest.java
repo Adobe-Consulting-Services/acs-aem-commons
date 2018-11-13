d14de4b91ce53fd5a9bd3f74fa2c7c412858bb4f
Mark Adamcin <adamcin@gmail.com>
2018-11-13 11:05:25 -0800

    Initial introduction of oakpal-maven-plugin and oakpal-checks.

---
+++ oakpal-checks/src/test/java/com/adobe/acs/commons/oakpal/checks/ContentClassificationsTest.java
@@ -0,0 +1,12 @@
+package com.adobe.acs.commons.oakpal.checks;
+
+import org.junit.Test;
+
+public class ContentClassificationsTest {
+
+    @Test
+    public void testImportedPath() {
+        ContentClassifications check = new ContentClassifications();
+
+    }
+}
