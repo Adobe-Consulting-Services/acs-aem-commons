35c27233e2c6451d60649de04f580a25574e7506
Joerg Hoh <joerg@joerghoh.de>
2018-11-18 13:33:01 +0100

    fix codeclimate issues

--- bundle/src/main/java/com/adobe/acs/commons/dam/impl/CustomComponentActivatorListServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/dam/impl/CustomComponentActivatorListServlet.java
@@ -52,9 +52,7 @@ public class CustomComponentActivatorListServlet extends SlingSafeMethodsServlet
     private static final String COLORANTS = "xmpTPg:Colorants=/apps/acs-commons/dam/content/admin/color-swatches";
     private static final String LOCATION = "location=/apps/acs-commons/dam/content/admin/asset-location-map";
 
-
     static final String[] DEFAULT_COMPONENTS = { HISTORY, FONTS, COLORANTS, LOCATION };
-
 
        
     @ObjectClassDefinition(name="ACS AEM Commons - Custom Component Activator List Servlet")
