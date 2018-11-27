3049e1d6f0a200e2f59fd7ea02851b4046f00f71
Feike Visser <fvisser@adobe.com>
2018-11-27 09:47:02 +0100

    osgi r6

--- bundle/src/main/java/com/adobe/acs/commons/genericlists/impl/GenericListJsonResourceProvider.java
+++ bundle/src/main/java/com/adobe/acs/commons/genericlists/impl/GenericListJsonResourceProvider.java
@@ -22,18 +22,19 @@ package com.adobe.acs.commons.genericlists.impl;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Iterator;
-import java.util.Map;
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Service;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceMetadata;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.SyntheticResource;
-import org.apache.sling.commons.osgi.PropertiesUtil;
+
+import org.apache.sling.commons.json.io.JSONWriter;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,6 +46,8 @@ import com.google.gson.Gson;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.sling.spi.resource.provider.ResolveContext;
 import org.apache.sling.spi.resource.provider.ResourceContext;
 import org.apache.sling.spi.resource.provider.ResourceProvider;
@@ -53,12 +56,13 @@ import org.apache.sling.spi.resource.provider.ResourceProvider;
  * Resource provider which makes Generic Lists available as JSON String resources
  * for use with the Touch UI Metadata Asset Editor.
  */
-@Component(metatype = true, label = "ACS AEM Commons - Generic List JSON Resource Provider",
-    description = "Resource Provider which makes Generic Lists available as JSON structures suitable for use in the Touch UI Asset Metadata Editor")
-//TODO: Confirm this works considering that spi Resource Provider is not an interface!
-@Service(ResourceProvider.class)
-@Properties({ @Property(name = ResourceProvider.PROPERTY_ROOT, value = GenericListJsonResourceProvider.ROOT) })
+
+@Component(service=ResourceProvider.class, property= {
+		ResourceProvider.PROPERTY_ROOT + "=" + GenericListJsonResourceProvider.ROOT
+})
+@Designate(ocd=GenericListJsonResourceProvider.Config.class)
 public final class GenericListJsonResourceProvider extends ResourceProvider {
+
 
     private static final Logger log = LoggerFactory.getLogger(GenericListJsonResourceProvider.class);
 
@@ -69,15 +73,19 @@ public final class GenericListJsonResourceProvider extends ResourceProvider {
     private static final String EXTENSION = ".json";
 
     private static final int EXTENSION_LENGTH = EXTENSION.length();
-
-    @Property(label = "Generic List Root", description = "Root path under which generic lists can be found", value = DEFAULT_LIST_ROOT)
-    private static final String PROP_LIST_ROOT = "list.root";
+    
+    @ObjectClassDefinition(name = "ACS AEM Commons - Generic List JSON Resource Provider",
+    	    description = "Resource Provider which makes Generic Lists available as JSON structures suitable for use in the Touch UI Asset Metadata Editor")
+    public @interface Config {
+       @AttributeDefinition(name = "Generic List Root", description = "Root path under which generic lists can be found", defaultValue = DEFAULT_LIST_ROOT)
+       String list_root();
+    }
 
     private String listRoot;
 
     @Activate
-    protected void activate(final Map<String, String> props) {
-        this.listRoot = PropertiesUtil.toString(props.get(PROP_LIST_ROOT), DEFAULT_LIST_ROOT);
+    protected void activate(GenericListJsonResourceProvider.Config config) {
+        this.listRoot = StringUtils.defaultIfEmpty(config.list_root(), DEFAULT_LIST_ROOT);
     }
 
     @Override
--- bundle/src/main/java/com/adobe/acs/commons/http/headers/impl/DispatcherMaxAgeHeaderFilter.java
+++ bundle/src/main/java/com/adobe/acs/commons/http/headers/impl/DispatcherMaxAgeHeaderFilter.java
@@ -19,46 +19,38 @@
  */
 package com.adobe.acs.commons.http.headers.impl;
 
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.ConfigurationPolicy;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.Service;
-import org.apache.sling.commons.osgi.PropertiesUtil;
 import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.component.ComponentContext;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Dictionary;
-import java.util.Enumeration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 //@formatter:off
 @Component(
-      label = "ACS AEM Commons - Dispacher Cache Control Header - Max Age",
-      description = "Adds a Cache-Control max-age header to content to enable Dispatcher TTL support.",
-      metatype = true,
-      configurationFactory = true,
-      policy = ConfigurationPolicy.REQUIRE)
-@Properties({
-    @Property(label = "Filter Patterns",
-        description = "Patterns on which to apply this Max Age cache-control rule.",
-        cardinality = Integer.MAX_VALUE,
-        name = AbstractDispatcherCacheHeaderFilter.PROP_FILTER_PATTERN,
-        propertyPrivate = false,
-        value = { }),
-    @Property(
-        name = "webconsole.configurationFactory.nameHint",
-        value = "Max Age: {max.age} for Patterns: [{filter.pattern}]",
-        propertyPrivate = true)
-})
+      factory = "DispatcherMaxAgeHeaderFilter",
+      configurationPolicy = ConfigurationPolicy.REQUIRE, property= {
+            "webconsole.configurationFactory.nameHint" + "=" + "Max Age: {max.age} for Patterns: [{filter.pattern}]"
+      })
+@Designate(ocd=DispatcherMaxAgeHeaderFilter.Config.class)
 //@formatter:on
 public class DispatcherMaxAgeHeaderFilter extends AbstractDispatcherCacheHeaderFilter {
 
     protected static final String CACHE_CONTROL_NAME = "Cache-Control";
-
-    @Property(label = "Cache-Control Max Age",
-            description = "Max age value (in seconds) to put in Cache Control header.")
-    public static final String PROP_MAX_AGE = "max.age";
+    
+    @ObjectClassDefinition( name = "ACS AEM Commons - Dispacher Cache Control Header - Max Age",
+      description = "Adds a Cache-Control max-age header to content to enable Dispatcher TTL support.")
+    public @interface Config {
+        @AttributeDefinition(name = "Cache-Control Max Age",
+                description = "Max age value (in seconds) to put in Cache Control header.")
+           int max_age();
+        @AttributeDefinition(name = "Filter Patterns",
+                description = "Patterns on which to apply this Max Age cache-control rule.",
+                cardinality = Integer.MAX_VALUE)
+        String[] filter_pattern();
+    }
 
     private static final String HEADER_PREFIX = "max-age=";
 
@@ -73,13 +65,16 @@ public class DispatcherMaxAgeHeaderFilter extends AbstractDispatcherCacheHeaderF
     protected String getHeaderValue() {
         return HEADER_PREFIX + maxage;
     }
-
+    
     @Override
-    protected void doActivate(ComponentContext context) throws Exception {
-        Dictionary<?, ?> properties = context.getProperties();
-        maxage = PropertiesUtil.toLong(properties.get(PROP_MAX_AGE), -1);
+   protected void doActivate(ComponentContext context) throws Exception {
+   }
+
+   @Activate
+    protected void activate(DispatcherMaxAgeHeaderFilter.Config config) throws Exception {
+         maxage = config.max_age();
         if (maxage < 0) {
-            throw new ConfigurationException(PROP_MAX_AGE, "Max Age must be specified and greater than 0.");
+            throw new ConfigurationException("max.age", "Max Age must be specified and greater than 0.");
         }
     }
 
