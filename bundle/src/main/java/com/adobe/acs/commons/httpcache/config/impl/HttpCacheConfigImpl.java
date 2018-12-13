17f92637c809e232ab3ef05bdaf32b2d3a4f1069
Feike Visser <fvisser@adobe.com>
2018-12-13 14:42:53 +0100

    osgi r6

--- bundle/src/main/java/com/adobe/acs/commons/httpcache/config/impl/HttpCacheConfigImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/httpcache/config/impl/HttpCacheConfigImpl.java
@@ -30,20 +30,15 @@ import com.adobe.acs.commons.httpcache.store.HttpCacheStore;
 import com.adobe.acs.commons.httpcache.util.UserUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.ConfigurationPolicy;
-import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Properties;
-import org.apache.felix.scr.annotations.Property;
-import org.apache.felix.scr.annotations.PropertyOption;
-import org.apache.felix.scr.annotations.PropertyUnbounded;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.ReferenceCardinality;
-import org.apache.felix.scr.annotations.ReferencePolicy;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.osgi.service.component.annotations.*;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,154 +53,161 @@ import java.util.regex.Pattern;
 /**
  * Concrete implementation of cache config for http cache. Modelled as OSGi config factory.
  */
-@Component(label = "ACS AEM Commons - HTTP Cache - Cache config",
-           description = "Config for request URI patterns that have to be cached.",
-           configurationFactory = true,
-           metatype = true,
-           policy = ConfigurationPolicy.REQUIRE
-)
-@Properties({
-        @Property(name = "webconsole.configurationFactory.nameHint",
-                value = "Order: {httpcache.config.order}, "
-                        + "Request URIs: {httpcache.config.requesturi.patterns}, "
-                        + "Request URIs blacklist: {httpcache.config.requesturi.patterns.blacklisted}, "
-                        + "Authentication: {httpcache.config.request.authentication}, "
-                        + "Invalidation paths: {httpcache.config.invalidation.oak.paths}, "
-                        + "Cache type: {httpcache.config.cachestore}",
-                propertyPrivate = true)
-})
-@Service
+@Component(service=HttpCacheConfig.class,
+configurationPolicy=ConfigurationPolicy.REQUIRE,
+property= {
+      "webconsole.configurationFactory.nameHint" + "=" +
+             "Order: {httpcache.config.order}, "
+                 + "Request URIs: {httpcache.config.requesturi.patterns}, "
+                 + "Request URIs blacklist: {httpcache.config.requesturi.patterns.blacklisted}, "
+                 + "Authentication: {httpcache.config.request.authentication}, "
+                 + "Invalidation paths: {httpcache.config.invalidation.oak.paths}, "
+                 + "Cache type: {httpcache.config.cachestore}"})
+@Designate(ocd=HttpCacheConfigImpl.Config.class, factory=true)
 public class HttpCacheConfigImpl implements HttpCacheConfig {
     private static final Logger log = LoggerFactory.getLogger(HttpCacheConfigImpl.class);
     private static final String FILTER_SCOPE_REQUEST = "REQUEST";
     private static final String FILTER_SCOPE_INCLUDE = "INCLUDE";
+    
+    @ObjectClassDefinition(name= "ACS AEM Commons - HTTP Cache - Cache config",
+           description = "Config for request URI patterns that have to be cached.")
+    public @interface Config {
+        @AttributeDefinition(name = "Priority order",
+                description = "Order in which the HttpCacheEngine should evaluate the HttpCacheConfigs against the "
+                        + "request. Evaluates smallest to largest (Integer.MIN_VALUE -> Integer.MAX_VALUE). Defaults to "
+                        + "1000 ",
+                defaultValue = ""+DEFAULT_ORDER)
+        int httpcache_config_order();
+        
+        @AttributeDefinition(name = "Request URI patterns",
+                description = "Request URI patterns (REGEX) to be cached. Example - /content/mysite(.*).product-data"
+                        + ".json. Mandatory parameter.",
+                cardinality = Integer.MAX_VALUE)
+      String[] httpcache_config_requesturi_patterns();
+        
+        @AttributeDefinition(name = "Blacklisted request URI patterns",
+                description = "Blacklisted request URI patterns (REGEX). Evaluated post applying the above request uri "
+                        + "patterns (httpcache.config.requesturi.patterns). Optional parameter.",
+                cardinality = Integer.MAX_VALUE)
+      String[] httpcache_config_requesturi_patterns_blacklisted();
+
+        @AttributeDefinition(name = "Authentication",
+                description = "Authentication requirement.",
+                options = {
+                        @Option(value = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST,
+                                           label = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST),
+                        @Option(value = AuthenticationStatusConfigConstants.AUTHENTICATED_REQUEST,
+                              label = AuthenticationStatusConfigConstants.AUTHENTICATED_REQUEST),
+                        @Option(value = AuthenticationStatusConfigConstants.BOTH_ANONYMOUS_AUTHENTICATED_REQUESTS,
+                              label = AuthenticationStatusConfigConstants.BOTH_ANONYMOUS_AUTHENTICATED_REQUESTS)
+                },
+                defaultValue = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST)
+      String httpcache_config_request_authentication();
+        
+        @AttributeDefinition(name = "JCR path pattern (REGEX) for cache invalidation ",
+                description = "Optional set of paths in JCR (Oak) repository for which this cache has to be invalidated"
+                        + ". This accepts " + "REGEX. Example - /etc/my-products(.*)",
+                cardinality = Integer.MAX_VALUE)
+       String[] httpcache_config_invalidation_oak_paths()
+       ;
+        @AttributeDefinition(name = "Cache store",
+                description = "Cache store for caching the response for this request URI. Example - MEM. This should "
+                        + "be one of the cache stores active in this installation. Mandatory parameter.",
+                options = {
+                        @Option(value = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE,
+                                           label = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE),
+                        @Option(value = HttpCacheStore.VALUE_JCR_CACHE_STORE_TYPE,
+                              label = HttpCacheStore.VALUE_JCR_CACHE_STORE_TYPE)
+                },
+              defaultValue = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE)
+      String httpcache_config_cachestore();
+        
+        @AttributeDefinition(name = "Filter scope",
+                description = "Specify the scope of this HttpCacheConfig in the scope of the Sling Servlet Filter processing chain.",
+                options = {
+                      @Option(value = FILTER_SCOPE_REQUEST,
+                                label = FILTER_SCOPE_REQUEST),
+                      @Option(value = FILTER_SCOPE_INCLUDE,
+                            label = FILTER_SCOPE_INCLUDE)
+                },
+                defaultValue = DEFAULT_FILTER_SCOPE)
+        String httpcache_config_filter_scope();
+
+        @AttributeDefinition(
+                name = "HttpCacheConfigExtension service pid",
+                description = "Service pid of target implementation of HttpCacheConfigExtension to be used. Example - "
+                        + "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)."
+                        + " Optional parameter.",
+                defaultValue = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)")
+   String cacheConfigExtension_target();
+        
+        @AttributeDefinition( name = "CacheKeyFactory service pid",
+              description = "Service pid of target implementation of CacheKeyFactory to be used. Example - "
+                        + "(service.pid=com.adobe.acs.commons.httpcac`he.config.impl.GroupHttpCacheConfigExtension)."
+                        + " Mandatory parameter.",
+                defaultValue = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)")
+        String cacheKeyFactory();
+        
+        @AttributeDefinition(name = "Config-specific HttpCacheHandlingRules",
+                description = "List of Service pid of HttpCacheHandlingRule applicable for this cache config. Optional "
+                        + "parameter")
+        String[] httpcache_config_cache_handling_rules_pid();
+
+    }
 
     // Order
     public static final int DEFAULT_ORDER = 1000;
     private int order = DEFAULT_ORDER;
-    @Property(label = "Priority order",
-            description = "Order in which the HttpCacheEngine should evaluate the HttpCacheConfigs against the "
-                    + "request. Evaluates smallest to largest (Integer.MIN_VALUE -> Integer.MAX_VALUE). Defaults to "
-                    + "1000 ",
-            intValue = DEFAULT_ORDER)
     public static final String PROP_ORDER = "httpcache.config.order";
 
     // Request URIs - Whitelisted.
-    @Property(label = "Request URI patterns",
-              description = "Request URI patterns (REGEX) to be cached. Example - /content/mysite(.*).product-data"
-                      + ".json. Mandatory parameter.",
-              cardinality = Integer.MAX_VALUE)
     private static final String PROP_REQUEST_URI_PATTERNS = "httpcache.config.requesturi.patterns";
     private List<String> requestUriPatterns;
     private List<Pattern> requestUriPatternsAsRegEx;
 
     // Request URIs - Blacklisted.
-    @Property(label = "Blacklisted request URI patterns",
-              description = "Blacklisted request URI patterns (REGEX). Evaluated post applying the above request uri "
-                      + "patterns (httpcache.config.requesturi.patterns). Optional parameter.",
-              cardinality = Integer.MAX_VALUE)
+
     private static final String PROP_BLACKLISTED_REQUEST_URI_PATTERNS =
             "httpcache.config.requesturi.patterns.blacklisted";
     private List<String> blacklistedRequestUriPatterns;
     private List<Pattern> blacklistedRequestUriPatternsAsRegEx;
 
     // Authentication requirement
-    // @formatter:off
-    @Property(label = "Authentication",
-              description = "Authentication requirement.",
-              options = {
-                      @PropertyOption(name = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST,
-                                         value = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST),
-                      @PropertyOption(name = AuthenticationStatusConfigConstants.AUTHENTICATED_REQUEST,
-                                      value = AuthenticationStatusConfigConstants.AUTHENTICATED_REQUEST),
-                      @PropertyOption(name = AuthenticationStatusConfigConstants.BOTH_ANONYMOUS_AUTHENTICATED_REQUESTS,
-                                      value = AuthenticationStatusConfigConstants.BOTH_ANONYMOUS_AUTHENTICATED_REQUESTS)
-              },
-              value = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST)
-    // @formatter:on
     private static final String PROP_AUTHENTICATION_REQUIREMENT = "httpcache.config.request.authentication";
     private static final String DEFAULT_AUTHENTICATION_REQUIREMENT = AuthenticationStatusConfigConstants
             .ANONYMOUS_REQUEST;
     private String authenticationRequirement;
 
     // Invalidation paths
-    @Property(label = "JCR path pattern (REGEX) for cache invalidation ",
-              description = "Optional set of paths in JCR (Oak) repository for which this cache has to be invalidated"
-                      + ". This accepts " + "REGEX. Example - /etc/my-products(.*)",
-              cardinality = Integer.MAX_VALUE)
     private static final String PROP_CACHE_INVALIDATION_PATH_PATTERNS = "httpcache.config.invalidation.oak.paths";
     private List<String> cacheInvalidationPathPatterns;
     private List<Pattern> cacheInvalidationPathPatternsAsRegEx;
 
     // Cache store
-    // @formatter:off
-    @Property(label = "Cache store",
-              description = "Cache store for caching the response for this request URI. Example - MEM. This should "
-                      + "be one of the cache stores active in this installation. Mandatory parameter.",
-              propertyPrivate = false,
-              options = {
-                      @PropertyOption(name = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE,
-                                         value = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE),
-                      // Only MEM and JCR implementations are available now.
-                      //@PropertyOption(name = HttpCacheStore.VALUE_DISK_CACHE_STORE_TYPE,
-                      //                value = HttpCacheStore.VALUE_DISK_CACHE_STORE_TYPE),
-                      @PropertyOption(name = HttpCacheStore.VALUE_JCR_CACHE_STORE_TYPE,
-                                      value = HttpCacheStore.VALUE_JCR_CACHE_STORE_TYPE)
-              },
-            value = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE)
-    // @formatter:on
     private static final String PROP_CACHE_STORE = "httpcache.config.cachestore";
     private static final String DEFAULT_CACHE_STORE = "MEM"; // Defaults to memory cache store
     private String cacheStore;
 
 
     // Cache store
-    // @formatter:off
     private static final String DEFAULT_FILTER_SCOPE = FILTER_SCOPE_REQUEST; // Defaults to REQUEST scope
-    @Property(label = "Filter scope",
-            description = "Specify the scope of this HttpCacheConfig in the scope of the Sling Servlet Filter processing chain.",
-            options = {
-                    @PropertyOption(name = FILTER_SCOPE_REQUEST,
-                            value = FILTER_SCOPE_REQUEST),
-                    @PropertyOption(name = FILTER_SCOPE_INCLUDE,
-                            value = FILTER_SCOPE_INCLUDE)
-            },
-            value = DEFAULT_FILTER_SCOPE)
-    // @formatter:on
-    private static final String PROP_FILTER_SCOPE = "httpcache.config.filter-scope";
+    private static final String PROP_FILTER_SCOPE = "httpcache.config.filter.scope";
     private FilterScope filterScope;
 
 
     // Making the cache config extension configurable.
-    @Property(name = "cacheConfigExtension.target",
-              label = "HttpCacheConfigExtension service pid",
-              description = "Service pid of target implementation of HttpCacheConfigExtension to be used. Example - "
-                      + "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)."
-                      + " Optional parameter.",
-              value = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)")
-    @Reference(cardinality = ReferenceCardinality.OPTIONAL_UNARY,
+    @Reference(cardinality = ReferenceCardinality.OPTIONAL,
                policy = ReferencePolicy.DYNAMIC,
                name = "cacheConfigExtension")
     private volatile HttpCacheConfigExtension cacheConfigExtension;
 
     // Making the cache key factory configurable.
-    @Property(name = "cacheKeyFactory.target",
-              label = "CacheKeyFactory service pid",
-              description = "Service pid of target implementation of CacheKeyFactory to be used. Example - "
-                      + "(service.pid=com.adobe.acs.commons.httpcac`he.config.impl.GroupHttpCacheConfigExtension)."
-                      + " Mandatory parameter.",
-              value = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)")
 
-    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY,
+    @Reference(cardinality = ReferenceCardinality.MANDATORY,
                policy = ReferencePolicy.DYNAMIC,
                name = "cacheKeyFactory")
     private volatile CacheKeyFactory cacheKeyFactory;
 
-
-    @Property(label = "Config-specific HttpCacheHandlingRules",
-              description = "List of Service pid of HttpCacheHandlingRule applicable for this cache config. Optional "
-                      + "parameter",
-              unbounded = PropertyUnbounded.ARRAY)
     private static final String PROP_CACHE_HANDLING_RULES_PID = "httpcache.config.cache-handling-rules.pid";
     private List<String> cacheHandlingRulesPid;
 
