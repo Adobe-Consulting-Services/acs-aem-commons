abed6283aa200d4929c16656d347596f0e1ec432
Joerg Hoh <joerg@joerghoh.de>
2018-12-16 13:18:33 +0100

    ignored some test cases

--- bundle/src/main/java/com/adobe/acs/commons/email/process/impl/SendTemplatedEmailProcess.java
+++ bundle/src/main/java/com/adobe/acs/commons/email/process/impl/SendTemplatedEmailProcess.java
@@ -37,10 +37,6 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
-<<<<<<< HEAD
-=======
-import org.osgi.service.metatype.annotations.ObjectClassDefinition;
->>>>>>> aad9a1e8... osgi annotations
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
