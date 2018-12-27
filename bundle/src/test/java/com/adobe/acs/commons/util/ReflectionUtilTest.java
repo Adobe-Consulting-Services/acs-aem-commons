d83371172df99e10a51c290aa4cd1d3b95345244
Niek Raaijmakers <niek@adobe.com>
2018-12-27 14:44:20 +0100

    Remove unneeded mockito junit runner

--- bundle/src/test/java/com/adobe/acs/commons/util/ReflectionUtilTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/util/ReflectionUtilTest.java
@@ -38,7 +38,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(MockitoJUnitRunner.class)
 public class ReflectionUtilTest {
 
     private ValueMap valueMap;
