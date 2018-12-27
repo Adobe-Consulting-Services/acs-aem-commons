0abdf7577a70d64667af90db86b23d7e0d29fff8
Niek Raaijmakers <niek@adobe.com>
2018-12-27 11:02:42 +0100

    Utility class for injectors Reflection utility class

---
+++ bundle/src/main/java/com/adobe/acs/commons/util/ReflectionUtil.java
@@ -0,0 +1,149 @@
+package com.adobe.acs.commons.util;
+
+import com.adobe.acs.commons.util.impl.ValueMapTypeConverter;
+import com.day.cq.commons.inherit.InheritanceValueMap;
+import org.apache.sling.api.resource.ValueMap;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Contains reflection utility methods
+ */
+public class ReflectionUtil {
+    private ReflectionUtil() {
+        // static methods only
+    }
+
+    public static Object convertValueMapValue(ValueMap valueMap, String name, Type declaredType) {
+        return new ValueMapTypeConverter(valueMap, name, declaredType).getConvertedValue();
+    }
+
+    public static Object convertValueMapValue(InheritanceValueMap valueMap, String name, Type declaredType) {
+        return new ValueMapTypeConverter(valueMap, name, declaredType).getConvertedValue();
+    }
+
+    public static <T> T[] toArray(Collection<T> c, T[] a) {
+        return c.size() > a.length ?
+                c.toArray((T[]) Array.newInstance(a.getClass().getComponentType(), c.size())) :
+                c.toArray(a);
+    }
+
+    /**
+     * The collection CAN be empty
+     */
+    public static <T> T[] toArray(Collection<T> c, Class klass) {
+        return toArray(c, (T[]) Array.newInstance(klass, c.size()));
+    }
+
+    /**
+     * The collection CANNOT be empty!
+     */
+    public static <T> T[] toArray(Collection<T> c) {
+        return toArray(c, c.iterator().next().getClass());
+    }
+
+    public static boolean isArray(Type declaredType) {
+        if (declaredType instanceof Class<?>) {
+            Class<?> clazz = (Class<?>) declaredType;
+            return isArray(clazz);
+        }
+        return false;
+    }
+
+    public static boolean isArray(Class<?> clazz) {
+        return clazz.isArray();
+    }
+
+    public static boolean isCollectionType(Type declaredType) {
+        if (declaredType instanceof Class<?>) {
+            Class<?> clazz = (Class<?>) declaredType;
+            return isCollectionType(clazz);
+        } else {
+            ParameterizedType parameterizedType = (ParameterizedType) declaredType;
+            return isCollectionType(parameterizedType.getRawType());
+        }
+    }
+
+    public static boolean isSetType(Type declaredType) {
+        if (declaredType instanceof Class<?>) {
+            Class<?> clazz = (Class<?>) declaredType;
+            return isSetType(clazz);
+        } else {
+            ParameterizedType parameterizedType = (ParameterizedType) declaredType;
+            return isSetType(parameterizedType.getRawType());
+        }
+    }
+
+    public static boolean isListType(Type declaredType) {
+        if (declaredType instanceof Class<?>) {
+            Class<?> clazz = (Class<?>) declaredType;
+            return isListType(clazz);
+        } else {
+            ParameterizedType parameterizedType = (ParameterizedType) declaredType;
+            return isListType(parameterizedType.getRawType());
+        }
+    }
+
+    public static Class<?> getClassOrGenericParam(Type type) {
+        if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            return getGenericParameter(parameterizedType, 0);
+        } else {
+            Class<?> clazz = (Class<?>) type;
+            return clazz;
+        }
+    }
+
+    public static boolean isListType(Class<?> collectionType) {
+        return collectionType.equals(List.class);
+    }
+
+    public static boolean isSetType(Class<?> collectionType) {
+        return collectionType.equals(Set.class);
+    }
+
+    public static boolean isCollectionType(Class<?> collectionType) {
+        return collectionType.equals(Collection.class);
+    }
+
+    public static boolean isAssignableFrom(Type type, Class<?> isAssignableFrom) {
+
+        if (type == null || isAssignableFrom == null) {
+            return false;
+        }
+        if (type instanceof Class<?>) {
+            Class<?> clazz = (Class<?>) type;
+            return clazz.isAssignableFrom(isAssignableFrom);
+        } else {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            return parameterizedType.getRawType().getClass().isAssignableFrom(isAssignableFrom);
+        }
+
+    }
+
+    public static boolean hasGenericParameter(Type type) {
+        return ParameterizedType.class.isInstance(type);
+    }
+
+    public static Class<?> getGenericParameter(Type type) {
+
+        return getGenericParameter(type, 0);
+    }
+
+    public static Class<?> getGenericParameter(Type type, int index) {
+
+        if (isArray(type)) {
+            return ((Class<?>) type).getComponentType();
+        }
+        if (hasGenericParameter(type)) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            return (Class<?>) parameterizedType.getActualTypeArguments()[index];
+        }
+        return null;
+    }
+}
---
+++ bundle/src/test/java/com/adobe/acs/commons/util/ReflectionUtilTest.java
@@ -0,0 +1,88 @@
+package com.adobe.acs.commons.util;
+
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.inherit.InheritanceValueMap;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.wrappers.ValueMapDecorator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReflectionUtilTest {
+
+    private ValueMap valueMap;
+    public Number numberField;
+    public List<Integer> integerList;
+
+    @Before
+    public void setUp() {
+        HashMap<String, Object> map = new HashMap();
+        map.put("myIntegerField", 22);
+        map.put("myIntegerArray", new Integer[]{22, 33});
+        map.put("myDoubleArray", new Double[]{11.11, 22.22});
+
+        valueMap = new ValueMapDecorator(map);
+    }
+
+    @Test
+    public void convertValueMapValue() {
+        Integer myIntegerField = (Integer) ReflectionUtil.convertValueMapValue(valueMap, "myIntegerField", Integer.class);
+        assertEquals(22, myIntegerField.intValue());
+
+        Integer[] myIntegerArray = (Integer[]) ReflectionUtil.convertValueMapValue(valueMap, "myIntegerArray", Integer[].class);
+        assertEquals(2, myIntegerArray.length);
+        assertEquals(33, myIntegerArray[1].intValue());
+
+        Double[] myDoubleArray = (Double[]) ReflectionUtil.convertValueMapValue(valueMap, "myDoubleArray", Double[].class);
+        assertEquals(2, myDoubleArray.length);
+        assertEquals(22.22, myDoubleArray[1], 0);
+
+    }
+
+    @Test
+    public void test_inheritanceValueMap() {
+        InheritanceValueMap inheritanceValueMap = new HierarchyNodeInheritanceValueMap(valueMap);
+        Integer myInheritedIntegerField = (Integer) ReflectionUtil.convertValueMapValue(inheritanceValueMap, "myIntegerField", Integer.class);
+        assertEquals(22, myInheritedIntegerField.intValue());
+    }
+
+    @Test
+    public void toArray() {
+
+        List<Integer> integerList = Arrays.asList(1, 2);
+        Integer[] integers = ReflectionUtil.toArray(integerList);
+        assertEquals(2, integers.length);
+        assertEquals(2, integers[1].intValue());
+
+    }
+
+    @Test
+    public void isArray() {
+        List<Integer> integerList = Arrays.asList(1, 2);
+        Integer[] integers = ReflectionUtil.toArray(integerList);
+        assertFalse(ReflectionUtil.isArray(integerList.getClass()));
+        assertTrue(ReflectionUtil.isArray(integers.getClass()));
+    }
+
+    @Test
+    public void isAssignableFrom() {
+
+        Field numberField = FieldUtils.getDeclaredField(ReflectionUtilTest.class, "numberField");
+        boolean isAssignableFrom = ReflectionUtil.isAssignableFrom(numberField.getGenericType(), Integer.class);
+        assertTrue(isAssignableFrom);
+    }
+
+
+}
