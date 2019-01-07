330cbb4be8e5841426b53a4f969dfa864db6eda8
Niek Raaijmakers <niek@adobe.com>
2019-01-07 10:52:24 +0100

    Http cache config: switched activate to Config activation instead of classic property map + added unit test

--- bundle/src/main/java/com/adobe/acs/commons/httpcache/config/impl/Config.java
+++ bundle/src/main/java/com/adobe/acs/commons/httpcache/config/impl/Config.java
@@ -1,24 +1,4 @@
-/*
- * #%L
- * ACS AEM Commons Bundle
- * %%
- * Copyright (C) 2018 Adobe
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 package com.adobe.acs.commons.httpcache.config.impl;
-
 
 import com.adobe.acs.commons.httpcache.config.AuthenticationStatusConfigConstants;
 import com.adobe.acs.commons.httpcache.store.HttpCacheStore;
@@ -26,6 +6,15 @@ import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import org.osgi.service.metatype.annotations.Option;
 
+/**
+ * WHAT IS IT ???
+ * <p>
+ * WHAT PURPOSE THAT IT HAS ???
+ * </p>
+ *
+ * @author niek.raaijkmakers@external.cybercon.de
+ * @since 2019-01-07
+ */
 @ObjectClassDefinition(name = "ACS AEM Commons - HTTP Cache - Cache config",
         description = "Config for request URI patterns that have to be cached.")
 public @interface Config {
@@ -57,38 +46,42 @@ public @interface Config {
 
     String FILTER_SCOPE_INCLUDE = "INCLUDE";
 
+    String DEFAULT_FILTER_SCOPE = FILTER_SCOPE_REQUEST; // Defaults to REQUEST scope
+
+    String DEFAULT_KEY_FACTORY_TARGET = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)";
+
+    String DEFAULT_EXTENSION_TARGET = DEFAULT_KEY_FACTORY_TARGET;
+
+    String DEFAULT_AUTHENTICATION_REQUIREMENT = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST;
+
+    String DEFAULT_CACHE_STORE = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE;
+
     long DEFAULT_EXPIRY_ON_CREATE = 0L;
 
     long DEFAULT_EXPIRY_ON_ACCESS = 0L;
 
     long DEFAULT_EXPIRY_ON_UPDATE = 0L;
 
-    String DEFAULT_AUTHENTICATION_REQUIREMENT = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST;
-
-    String DEFAULT_CACHE_STORE = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE;
-
     int DEFAULT_ORDER = 1000;
-
-    String DEFAULT_FILTER_SCOPE = FILTER_SCOPE_REQUEST; // Defaults to REQUEST scope
 
     @AttributeDefinition(name = "Priority order",
             description = "Order in which the HttpCacheEngine should evaluate the HttpCacheConfigs against the "
                     + "request. Evaluates smallest to largest (Integer.MIN_VALUE -> Integer.MAX_VALUE). Defaults to "
                     + "1000 ",
             defaultValue = "" + DEFAULT_ORDER)
-    int httpcache_config_order();
+    int httpcache_config_order() default DEFAULT_ORDER;
 
     @AttributeDefinition(name = "Request URI patterns",
             description = "Request URI patterns (REGEX) to be cached. Example - /content/mysite(.*).product-data"
                     + ".json. Mandatory parameter.",
             cardinality = Integer.MAX_VALUE)
-    String[] httpcache_config_requesturi_patterns();
+    String[] httpcache_config_requesturi_patterns() default {};
 
     @AttributeDefinition(name = "Blacklisted request URI patterns",
             description = "Blacklisted request URI patterns (REGEX). Evaluated post applying the above request uri "
                     + "patterns (httpcache.config.requesturi.patterns). Optional parameter.",
             cardinality = Integer.MAX_VALUE)
-    String[] httpcache_config_requesturi_patterns_blacklisted();
+    String[] httpcache_config_requesturi_patterns_blacklisted() default {};
 
     @AttributeDefinition(name = "Authentication",
             description = "Authentication requirement.",
@@ -101,13 +94,13 @@ public @interface Config {
                             label = AuthenticationStatusConfigConstants.BOTH_ANONYMOUS_AUTHENTICATED_REQUESTS)
             },
             defaultValue = AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST)
-    String httpcache_config_request_authentication();
+    String httpcache_config_request_authentication() default AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST;
 
     @AttributeDefinition(name = "JCR path pattern (REGEX) for cache invalidation ",
             description = "Optional set of paths in JCR (Oak) repository for which this cache has to be invalidated"
                     + ". This accepts " + "REGEX. Example - /etc/my-products(.*)",
             cardinality = Integer.MAX_VALUE)
-    String[] httpcache_config_invalidation_oak_paths();
+    String[] httpcache_config_invalidation_oak_paths() default {};
 
     @AttributeDefinition(name = "Cache store",
             description = "Cache store for caching the response for this request URI. Example - MEM. This should "
@@ -119,7 +112,7 @@ public @interface Config {
                             label = HttpCacheStore.VALUE_JCR_CACHE_STORE_TYPE)
             },
             defaultValue = HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE)
-    String httpcache_config_cachestore();
+    String httpcache_config_cachestore() default HttpCacheStore.VALUE_MEM_CACHE_STORE_TYPE;
 
     @AttributeDefinition(name = "Filter scope",
             description = "Specify the scope of this HttpCacheConfig in the scope of the Sling Servlet Filter processing chain.",
@@ -130,26 +123,37 @@ public @interface Config {
                             label = FILTER_SCOPE_INCLUDE)
             },
             defaultValue = DEFAULT_FILTER_SCOPE)
-    String httpcache_config_filter_scope();
+    String httpcache_config_filter_scope() default DEFAULT_FILTER_SCOPE;
 
     @AttributeDefinition(
             name = "HttpCacheConfigExtension service pid",
             description = "Service pid of target implementation of HttpCacheConfigExtension to be used. Example - "
-                    + "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)."
+                    + "(service.pid=" + DEFAULT_EXTENSION_TARGET + ")."
                     + " Optional parameter.",
-            defaultValue = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)")
-    String cacheConfigExtension_target();
+            defaultValue = DEFAULT_EXTENSION_TARGET)
+    String cacheConfigExtension_target() default DEFAULT_EXTENSION_TARGET;
 
     @AttributeDefinition(name = "CacheKeyFactory service pid",
             description = "Service pid of target implementation of CacheKeyFactory to be used. Example - "
-                    + "(service.pid=com.adobe.acs.commons.httpcac`he.config.impl.GroupHttpCacheConfigExtension)."
+                    + "(service.pid=" + DEFAULT_KEY_FACTORY_TARGET + ")."
                     + " Mandatory parameter.",
-            defaultValue = "(service.pid=com.adobe.acs.commons.httpcache.config.impl.GroupHttpCacheConfigExtension)")
-    String cacheKeyFactory();
+            defaultValue = DEFAULT_KEY_FACTORY_TARGET)
+    String cacheKeyFactory() default DEFAULT_KEY_FACTORY_TARGET;
 
     @AttributeDefinition(name = "Config-specific HttpCacheHandlingRules",
             description = "List of Service pid of HttpCacheHandlingRule applicable for this cache config. Optional "
                     + "parameter")
     String[] httpcache_config_cache_handling_rules_pid();
 
+    @AttributeDefinition(name = "Expiry on create",
+            description = "Specifies a custom expiry on create. Overrules the global expiry, unless the value is 0.")
+    long httpcache_config_expiry_on_create();
+
+    @AttributeDefinition(name = "Expiry on access",
+            description = "Specifies a custom expiry on access. This refreshes the expiry of the entry if it's used. Lower then 0 means no expiry on access. ")
+    long httpcache_config_expiry_on_access();
+
+    @AttributeDefinition(name = "Expiry on update",
+            description = "Specifies a custom expiry on update. This refreshes the expiry of the entry if it's updated. Lower then 0 means no expiry on update.")
+    long httpcache_config_expiry_on_update();
 }
---
+++ bundle/src/test/java/com/adobe/acs/commons/httpcache/config/impl/HttpCacheConfigImplTest.java
@@ -0,0 +1,248 @@
+package com.adobe.acs.commons.httpcache.config.impl;
+
+import com.adobe.acs.commons.httpcache.config.HttpCacheConfig;
+import com.adobe.acs.commons.httpcache.exception.HttpCacheKeyCreationException;
+import com.adobe.acs.commons.httpcache.exception.HttpCacheRepositoryAccessException;
+import com.adobe.acs.commons.httpcache.keys.CacheKey;
+import com.adobe.acs.commons.httpcache.keys.CacheKeyFactory;
+import io.wcm.testing.mock.aem.junit.AemContext;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.adobe.acs.commons.httpcache.config.AuthenticationStatusConfigConstants.ANONYMOUS_REQUEST;
+import static com.adobe.acs.commons.httpcache.config.AuthenticationStatusConfigConstants.AUTHENTICATED_REQUEST;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_AUTHENTICATION_REQUIREMENT;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_CACHE_STORE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_EXPIRY_ON_ACCESS;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_EXPIRY_ON_CREATE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_EXPIRY_ON_UPDATE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_FILTER_SCOPE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.DEFAULT_ORDER;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.FILTER_SCOPE_INCLUDE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_AUTHENTICATION_REQUIREMENT;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_BLACKLISTED_REQUEST_URI_PATTERNS;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_CACHE_HANDLING_RULES_PID;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_CACHE_INVALIDATION_PATH_PATTERNS;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_CACHE_STORE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_EXPIRY_ON_ACCESS;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_EXPIRY_ON_CREATE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_EXPIRY_ON_UPDATE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_FILTER_SCOPE;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_ORDER;
+import static com.adobe.acs.commons.httpcache.config.impl.Config.PROP_REQUEST_URI_PATTERNS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class HttpCacheConfigImplTest {
+
+    @Rule
+    public AemContext context = new AemContext();
+    private final HttpCacheConfigImpl systemUnderTest = new HttpCacheConfigImpl();
+    private Map<String, Object> properties;
+
+    @Mock
+    private GroupHttpCacheConfigExtension extension;
+
+    @Mock
+    private SlingHttpServletRequest request;
+
+    @Mock
+    private ResourceResolver resourceResolver;
+
+    @Before
+    public void init() throws HttpCacheRepositoryAccessException {
+
+        properties = new HashMap<>();
+
+        context.registerService(CacheKeyFactory.class, extension);
+
+        when(request.getResourceResolver()).thenReturn(resourceResolver);
+        when(resourceResolver.getUserID()).thenReturn("anonymous");
+        when(extension.accepts(request, systemUnderTest)).thenReturn(true);
+    }
+
+    private void activateWithDefaultValues(Map<String,Object> specifiedProps){
+        properties.put(PROP_ORDER, DEFAULT_ORDER );
+        properties.put(PROP_REQUEST_URI_PATTERNS, new String[]{});
+        properties.put(PROP_BLACKLISTED_REQUEST_URI_PATTERNS, new String[]{});
+        properties.put(PROP_AUTHENTICATION_REQUIREMENT,DEFAULT_AUTHENTICATION_REQUIREMENT);
+        properties.put(PROP_CACHE_INVALIDATION_PATH_PATTERNS,new String[]{});
+        properties.put(PROP_CACHE_STORE,DEFAULT_CACHE_STORE);
+        properties.put(PROP_FILTER_SCOPE,DEFAULT_FILTER_SCOPE);
+        properties.put(PROP_EXPIRY_ON_CREATE,DEFAULT_EXPIRY_ON_CREATE);
+        properties.put(PROP_EXPIRY_ON_ACCESS,DEFAULT_EXPIRY_ON_ACCESS);
+        properties.put(PROP_EXPIRY_ON_UPDATE,DEFAULT_EXPIRY_ON_UPDATE);
+        properties.put(PROP_CACHE_HANDLING_RULES_PID,new String[]{});
+
+        properties.putAll(specifiedProps);
+        context.registerInjectActivateService(systemUnderTest, properties);
+    }
+
+    @Test
+    public void test_default_config_values(){
+        activateWithDefaultValues(Collections.emptyMap());
+
+        assertEquals(DEFAULT_ORDER, systemUnderTest.getOrder());
+        assertTrue( systemUnderTest.getRequestUriPatterns().isEmpty());
+        assertTrue(systemUnderTest.getBlacklistedRequestUriPatterns().isEmpty());
+        assertEquals(DEFAULT_AUTHENTICATION_REQUIREMENT, systemUnderTest.getAuthenticationRequirement());
+        assertTrue(systemUnderTest.getJCRInvalidationPathPatterns().isEmpty());
+        assertEquals(DEFAULT_CACHE_STORE, systemUnderTest.getCacheStoreName());
+        assertEquals(HttpCacheConfig.FilterScope.REQUEST, systemUnderTest.getFilterScope());
+        assertEquals(DEFAULT_EXPIRY_ON_CREATE, systemUnderTest.getExpiryOnCreate());
+        assertEquals(DEFAULT_EXPIRY_ON_ACCESS, systemUnderTest.getExpiryForAccess());
+        assertEquals(DEFAULT_EXPIRY_ON_UPDATE, systemUnderTest.getExpiryForUpdate());
+        assertFalse( systemUnderTest.acceptsRule("nonexisting"));
+
+
+    }
+
+    @Test
+    public void test_specified_values(){
+
+        properties.put(PROP_ORDER, 22 );
+        properties.put(PROP_REQUEST_URI_PATTERNS, new String[]{"/content"});
+        properties.put(PROP_BLACKLISTED_REQUEST_URI_PATTERNS, new String[]{"/content/blacklisted"});
+        properties.put(PROP_AUTHENTICATION_REQUIREMENT,AUTHENTICATED_REQUEST);
+        properties.put(PROP_CACHE_INVALIDATION_PATH_PATTERNS,new String[]{"/content"});
+        properties.put(PROP_CACHE_STORE,"JCR");
+        properties.put(PROP_FILTER_SCOPE,FILTER_SCOPE_INCLUDE);
+        properties.put(PROP_EXPIRY_ON_CREATE,5L);
+        properties.put(PROP_EXPIRY_ON_ACCESS,10L);
+        properties.put(PROP_EXPIRY_ON_UPDATE,15L);
+        properties.put(PROP_CACHE_HANDLING_RULES_PID,new String[]{"handling-rule"});
+        context.registerInjectActivateService(systemUnderTest, properties);
+
+        assertEquals(22, systemUnderTest.getOrder());
+        assertEquals("/content", systemUnderTest.getRequestUriPatterns().get(0).pattern());
+        assertEquals("/content/blacklisted",systemUnderTest.getBlacklistedRequestUriPatterns().get(0).pattern());
+        assertEquals(AUTHENTICATED_REQUEST, systemUnderTest.getAuthenticationRequirement());
+        assertEquals("/content", systemUnderTest.getJCRInvalidationPathPatterns().get(0).pattern());
+        assertEquals("JCR", systemUnderTest.getCacheStoreName());
+        assertEquals(HttpCacheConfig.FilterScope.INCLUDE, systemUnderTest.getFilterScope());
+        assertEquals(5L, systemUnderTest.getExpiryOnCreate());
+        assertEquals(10L, systemUnderTest.getExpiryForAccess());
+        assertEquals(15L, systemUnderTest.getExpiryForUpdate());
+        assertFalse( systemUnderTest.acceptsRule("nonexisting"));
+        assertTrue( systemUnderTest.acceptsRule("handling-rule"));
+    }
+
+    @Test
+    public void test_accepts_sling_request() throws HttpCacheRepositoryAccessException {
+
+        Map<String,Object> properties = new HashMap<>();
+
+        properties.put(PROP_REQUEST_URI_PATTERNS, new String[]{"/content/[a-z]{4}.html"});
+        activateWithDefaultValues(properties);
+
+        when(request.getRequestURI()).thenReturn("/content/page.html");
+
+        assertTrue(systemUnderTest.accepts(request));
+
+    }
+
+    @Test
+    public void test_denies_sling_request_authentication() throws HttpCacheRepositoryAccessException {
+
+        Map<String,Object> properties = new HashMap<>();
+
+        properties.put(PROP_AUTHENTICATION_REQUIREMENT, AUTHENTICATED_REQUEST);
+        activateWithDefaultValues(properties);
+
+        when(resourceResolver.getUserID()).thenReturn("anonymous");
+
+        assertFalse(systemUnderTest.accepts(request));
+
+    }
+
+    @Test
+    public void test_denies_sling_request_authentication_anon() throws HttpCacheRepositoryAccessException {
+
+        Map<String,Object> properties = new HashMap<>();
+
+        properties.put(PROP_AUTHENTICATION_REQUIREMENT, ANONYMOUS_REQUEST);
+        activateWithDefaultValues(properties);
+
+        when(resourceResolver.getUserID()).thenReturn("admin");
+
+        assertFalse(systemUnderTest.accepts(request));
+
+    }
+
+    @Test
+    public void test_denies_sling_request_uri_whitelist() throws HttpCacheRepositoryAccessException {
+
+        Map<String,Object> properties = new HashMap<>();
+
+        properties.put(PROP_REQUEST_URI_PATTERNS, new String[]{"/content/(.*)"});
+
+        activateWithDefaultValues(properties);
+
+        when(request.getRequestURI()).thenReturn("/invalid/url");
+
+        assertFalse(systemUnderTest.accepts(request));
+
+    }
+
+
+    @Test
+    public void test_denies_sling_request_uri_blacklisted() throws HttpCacheRepositoryAccessException {
+
+        Map<String,Object> properties = new HashMap<>();
+
+        properties.put(PROP_REQUEST_URI_PATTERNS, new String[]{"/content/(.*)"});
+        properties.put(PROP_BLACKLISTED_REQUEST_URI_PATTERNS, new String[]{"/content/[a-z]{4}/blacklisted.html"});
+
+        activateWithDefaultValues(properties);
+
+        when(request.getRequestURI()).thenReturn("/content/page/blacklisted.html");
+
+        assertFalse(systemUnderTest.accepts(request));
+
+    }
+
+    @Test
+    public void test_build_cachekey() throws HttpCacheKeyCreationException {
+
+        activateWithDefaultValues(Collections.emptyMap());
+        CacheKey cacheKey = mock(CacheKey.class);
+
+        final String resourcePath = "/content/test.html";
+        when(extension.build(resourcePath, systemUnderTest)).thenReturn(cacheKey);
+        when(extension.build(request, systemUnderTest)).thenReturn(cacheKey);
+        when(extension.doesKeyMatchConfig(cacheKey, systemUnderTest)).thenReturn(true);
+
+        CacheKey producedFromResourcePath = systemUnderTest.buildCacheKey(resourcePath);
+        assertSame(cacheKey, producedFromResourcePath);
+        verify(extension,times(1)).build(resourcePath, systemUnderTest);
+
+
+        CacheKey producedFromRequest = systemUnderTest.buildCacheKey(request);
+        assertSame(cacheKey, producedFromRequest);
+        verify(extension,times(1)).build(request, systemUnderTest);
+
+        boolean knows = systemUnderTest.knows(cacheKey);
+        assertTrue(knows);
+        verify(extension, times(1)).doesKeyMatchConfig(cacheKey, systemUnderTest);
+    }
+
+
+
+}
