1e629abe9ff70110e6a55ca4cd18e2fc800a2914
Joerg Hoh <joerg@joerghoh.de>
2019-01-14 20:18:06 +0100

    fix codeclimate issues

--- bundle/src/test/java/com/adobe/acs/commons/oak/impl/EnsureOakIndexManagerImplTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/oak/impl/EnsureOakIndexManagerImplTest.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2014 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.oak.impl;
 
 import static org.junit.Assert.assertEquals;
@@ -52,7 +71,7 @@ public class EnsureOakIndexManagerImplTest {
         
         ScheduleOptions options = mock(ScheduleOptions.class);
         when(scheduler.NOW()).thenReturn(options);
-        when(scheduler.schedule(anyObject(), anyObject())).thenAnswer ((InvocationOnMock invocation) -> {
+        when(scheduler.schedule(anyObject(), anyObject())).thenAnswer((InvocationOnMock invocation) -> {
             EnsureOakIndexJobHandler handler = invocation.getArgumentAt(0, EnsureOakIndexJobHandler.class);
             handler.run();
             return true;
