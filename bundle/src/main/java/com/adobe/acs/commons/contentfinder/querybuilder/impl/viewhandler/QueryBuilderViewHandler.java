30ff971a9c381de8c8c92d615599aa6a24258e24
Mark Adamcin <adamcin@gmail.com>
2019-01-15 14:20:48 -0800

    Fix code climate issue.

--- bundle/src/main/java/com/adobe/acs/commons/contentfinder/querybuilder/impl/viewhandler/QueryBuilderViewHandler.java
+++ bundle/src/main/java/com/adobe/acs/commons/contentfinder/querybuilder/impl/viewhandler/QueryBuilderViewHandler.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,7 +28,6 @@ import com.day.cq.wcm.core.contentfinder.ViewQuery;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
@@ -58,7 +57,6 @@ public final class QueryBuilderViewHandler extends ViewHandler {
     @Override
     protected ViewQuery createQuery(SlingHttpServletRequest slingRequest, Session session,
             String queryString) throws Exception {
-        final ResourceResolver resolver = slingRequest.getResourceResolver();
         Map<String, String> map;
 
         if (GQLToQueryBuilderConverter.convertToQueryBuilder(slingRequest)) {
