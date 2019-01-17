9233c5676aecde47027e391128fcf98da130c8c1
Brendan Robert <brendan.robert@gmail.com>
2019-01-17 13:54:22 -0600

    Fix for #1699 (#1702)
    * Fix for #1699
    
    * Added test coverage and also improved handling of multi-value parameters as arrays of strings

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/ControlledProcessManagerServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/ControlledProcessManagerServlet.java
@@ -46,6 +46,8 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.request.RequestParameter;
 import org.apache.sling.api.request.RequestParameterMap;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -63,6 +65,17 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
 
     private static final Logger LOG = LoggerFactory.getLogger(ControlledProcessManagerServlet.class);
 
+    private static final Collection<Class> IGNORED_CLASSES = Arrays.asList(
+            Logger.class,
+            Resource.class,
+            ResourceResolver.class,
+            byte[].class
+    );
+    private static final Collection<String> IGNORED_PACKAGES = Arrays.asList(
+            "java.io"
+    );
+    private static final List<String> IGNORED_SERVLET_INPUTS = Arrays.asList("definition", "description", "action");
+
     @Reference
     ControlledProcessManager manager;
 
@@ -73,9 +86,12 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
-        String action = request.getRequestPathInfo().getSelectorString();
         Object result = null;
         try {
+            String action = request.getRequestPathInfo().getSelectorString();
+            if (action == null) {
+                throw new ServletException("action not provided in url selector");
+            }
             switch (action) {
                 case "start":
                     result = doStartProcess(request);
@@ -92,10 +108,10 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
                 case "haltAll":
                 case "halt.all":
                 case "halt-all":
-                    result = doHaltAllProcesses(request);
+                    result = doHaltAllProcesses();
                     break;
                 case "purge":
-                    result = doPurgeCompleted(request);
+                    result = doPurgeCompleted();
                     break;
                 default:
                     throw new IllegalArgumentException("Action not understood.");
@@ -104,6 +120,10 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
             result = "Exception occurred " + ex.getMessage();
             LOG.error(ex.getMessage() + " -- End of line.", ex);
         }
+        getGson().toJson(result, response.getWriter());
+    }
+
+    Gson getGson() {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.addSerializationExclusionStrategy(new ExclusionStrategy() {
             @Override
@@ -113,16 +133,14 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
 
             @Override
             public boolean shouldSkipClass(Class<?> type) {
-                if (type.getPackage() != null) {
-                    return type.getPackage().getName().startsWith("java.io");
-                } else {
-                    return type == byte[].class;
-                }
+                return IGNORED_CLASSES.contains(type)
+                        || (type.getPackage() != null
+                        && IGNORED_PACKAGES.contains(type.getPackage().getName()));
             }
         });
         gsonBuilder.disableInnerClassSerialization();
         Gson gson = gsonBuilder.create();
-        gson.toJson(result, response.getWriter());
+        return gson;
     }
 
     private ProcessInstance doStartProcess(SlingHttpServletRequest request) throws RepositoryException, ReflectiveOperationException, DeserializeException {
@@ -153,13 +171,13 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
     }
 
     @SuppressWarnings("squid:S1172")
-    private boolean doHaltAllProcesses(SlingHttpServletRequest request) {
+    private boolean doHaltAllProcesses() {
         manager.haltActiveProcesses();
         return true;
     }
 
     @SuppressWarnings("squid:S1172")
-    private boolean doPurgeCompleted(SlingHttpServletRequest request) {
+    private boolean doPurgeCompleted() {
         manager.purgeCompletedProcesses();
         return true;
     }
@@ -186,11 +204,9 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
         }
     }
 
-    List<String> ignoredInputs = Arrays.asList("definition", "description", "action");
-
-    private Map<String, Object> convertRequestMap(RequestParameterMap requestParameterMap) {
+    Map<String, Object> convertRequestMap(RequestParameterMap requestParameterMap) {
         return requestParameterMap.entrySet().stream()
-                .filter(entry -> !ignoredInputs.contains(entry.getKey()))
+                .filter(entry -> !IGNORED_SERVLET_INPUTS.contains(entry.getKey()))
                 .collect(Collectors.toMap(
                         entry -> entry.getKey(),
                         entry -> {
@@ -203,7 +219,7 @@ public class ControlledProcessManagerServlet extends SlingAllMethodsServlet {
                                     return values[0].getString();
                                 }
                             } else {
-                                return Arrays.stream(values).collect(Collectors.toList());
+                                return Arrays.stream(values).map(RequestParameter::getString).collect(Collectors.toList());
                             }
                         }
                 ));
