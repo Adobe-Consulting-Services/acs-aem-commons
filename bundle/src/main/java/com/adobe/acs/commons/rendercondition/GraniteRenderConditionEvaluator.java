6edea6ef844d7f48d53aa65baf5b59b8f880ce38
Ahmed Musallam <atyaf66@gmail.com>
2019-02-20 16:05:33 -0600

    more code climate fixes

--- bundle/src/main/java/com/adobe/acs/commons/rendercondition/GraniteRenderConditionEvaluator.java
+++ bundle/src/main/java/com/adobe/acs/commons/rendercondition/GraniteRenderConditionEvaluator.java
@@ -1,3 +1,41 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.adobe.acs.commons.rendercondition;
 
 import com.adobe.granite.ui.components.ComponentHelper;
@@ -39,15 +77,15 @@ public class GraniteRenderConditionEvaluator {
     Config cfg = cmp.getConfig();
 
     // get properties on the render condition
-    String servicePID = cfg.get(GraniteRenderConditionConstants.CONDITION_NAME, null);
+    String servicePid = cfg.get(GraniteRenderConditionConstants.CONDITION_NAME, null);
     boolean defaultValue = cfg.get("default", false);
 
-    if (servicePID != null) {
-      GraniteRenderCondition condition = getRenderConditionService(slingScriptHelper, servicePID);
+    if (servicePid != null) {
+      GraniteRenderCondition condition = getRenderConditionService(slingScriptHelper, servicePid);
       if (condition != null) {
         return condition.evaluate(slingHttpServletRequest, httpServletRequest, cfg, defaultValue);
       } else {
-        log.error("Could not find service with PID: {}", servicePID);
+        log.error("Could not find service with PID: {}", servicePid);
       }
     } else {
       log.error("service.pid cannot be empty for an ACS Commons Service Render Condition. Returning default value: {}", defaultValue);
@@ -61,10 +99,10 @@ public class GraniteRenderConditionEvaluator {
    */
   private static GraniteRenderCondition getRenderConditionService(
       SlingScriptHelper slingScriptHelper,
-      @Nonnull String servicePID) {
+      @Nonnull String servicePid) {
 
     return Optional.ofNullable(slingScriptHelper)
-        .map(helper -> helper.getServices(GraniteRenderCondition.class, getConditionServiceFilter(servicePID)))
+        .map(helper -> helper.getServices(GraniteRenderCondition.class, getConditionServiceFilter(servicePid)))
         .filter(ArrayUtils::isNotEmpty)
         .map(services -> services[0])
         .orElse(null);
