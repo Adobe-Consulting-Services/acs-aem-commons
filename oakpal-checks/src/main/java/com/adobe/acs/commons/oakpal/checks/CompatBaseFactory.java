ba9b70540235c53178db09aff225659b6f20b31c
Mark Adamcin <adamcin@gmail.com>
2019-03-06 10:14:41 -0800

    Added more deprecation comments to the CompatBaseFactory base class.

--- oakpal-checks/src/main/java/com/adobe/acs/commons/oakpal/checks/CompatBaseFactory.java
+++ oakpal-checks/src/main/java/com/adobe/acs/commons/oakpal/checks/CompatBaseFactory.java
@@ -11,10 +11,24 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Temporary Shim to facilitate deprecation of usages of org.json in favor of javax.json.
+ *
+ * @deprecated 4.0.0 this base class will be removed after oakpal-core drops support for org.json
  */
+@Deprecated
 abstract class CompatBaseFactory implements ProgressCheckFactory {
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
+    /**
+     * Create a new instance of a ProgressCheck using the provided config object.
+     * <p>
+     * Legacy method implementation which indicates the deprecation in the logger and delegates to
+     * {@link #newInstance(JsonObject)}.
+     *
+     * @param config the org.json.JSONObject config
+     * @return the new ProgressCheck
+     * @deprecated 4.0.0 migrate usages of {@link #newInstance(JSONObject)} to {@link #newInstance(JsonObject)}
+     */
+    @Deprecated
     @Override
     public final ProgressCheck newInstance(final JSONObject config) {
         logger.info("ProgressCheckFactory.newInstance(org.json.JSONObject config) is deprecated as of 1.2.0.");
@@ -22,6 +36,15 @@ abstract class CompatBaseFactory implements ProgressCheckFactory {
         return newInstance(JavaxJson.wrap(config.toMap()).asJsonObject());
     }
 
+    /**
+     * Create a new instance of a ProgressCheck using the provided config object.
+     * <p>
+     * Overrides default interface method, which currently exists solely to facilitate deprecation of org.json, to force
+     * implementation in our own subclasses.
+     *
+     * @param config the config object
+     * @return the new ProgressCheck
+     */
     @Override
     public abstract ProgressCheck newInstance(final JsonObject config);
 
