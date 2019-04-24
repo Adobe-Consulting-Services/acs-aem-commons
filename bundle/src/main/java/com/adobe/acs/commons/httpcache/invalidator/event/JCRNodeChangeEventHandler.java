f80beb7f907f3320d5dead441e61f43cced6ce89
Csaba Varga <csaboka@gmail.com>
2019-04-24 08:43:22 +0200

    Suppressed SonarQube warning about using a synchronized class.

--- bundle/src/main/java/com/adobe/acs/commons/httpcache/invalidator/event/JCRNodeChangeEventHandler.java
+++ bundle/src/main/java/com/adobe/acs/commons/httpcache/invalidator/event/JCRNodeChangeEventHandler.java
@@ -89,6 +89,7 @@ public class JCRNodeChangeEventHandler implements EventHandler, ResourceChangeLi
     private ServiceRegistration<?> registration;
     
     @Activate
+    @SuppressWarnings("squid:S1149")
     protected void activate(BundleContext context, Map<String, Object> config) {
         String pathFilter = PropertiesUtil.toString(config.get(EventConstants.EVENT_FILTER), "");
         if (!pathFilter.isEmpty()) {
