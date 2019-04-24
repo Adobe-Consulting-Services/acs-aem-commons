56e7cbc029d6995c70cd656c6ad23080ee10f66d
Csaba Varga <csaboka@gmail.com>
2019-04-24 08:58:57 +0200

    Updated integration test to ignore changed component properties.

--- bundle/src/test/java/com/adobe/acs/commons/it/build/ScrMetadataIT.java
+++ bundle/src/test/java/com/adobe/acs/commons/it/build/ScrMetadataIT.java
@@ -84,6 +84,11 @@ public class ScrMetadataIT {
         // the following two values changed due to https://github.com/Adobe-Consulting-Services/acs-aem-commons/issues/1773
         COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.rewriter.impl.PlainXMLSerializerFactory:pipeline.type");
         COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.rewriter.impl.XMLParserGeneratorFactory:pipeline.type");
+        // the following four values changed due to https://github.com/Adobe-Consulting-Services/acs-aem-commons/pull/1852
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.httpcache.invalidator.event.JCRNodeChangeEventHandler:event.topics");
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.httpcache.invalidator.event.JCRNodeChangeEventHandler:event.filter");
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.replication.packages.automatic.impl.ConfigurationUpdateListener:event.topics");
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.replication.packages.automatic.impl.ConfigurationUpdateListener:event.filter");
 
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE = new HashSet<>();
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE.add("com.adobe.acs.commons.fam.impl.ThrottledTaskRunnerImpl:max.cpu");
