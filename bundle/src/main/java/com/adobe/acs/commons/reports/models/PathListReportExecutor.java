229d374869688f9d30702fe046a05af053fc78dc
Yaraslau Dzenisiuk <cylinder.y@gmail.com>
2019-05-29 16:14:50 +0300

    make codeclimate happy

--- bundle/src/main/java/com/adobe/acs/commons/reports/models/PathListReportExecutor.java
+++ bundle/src/main/java/com/adobe/acs/commons/reports/models/PathListReportExecutor.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2017 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.reports.models;
 
 import com.adobe.acs.commons.reports.api.ReportException;
@@ -12,8 +31,6 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -23,8 +40,6 @@ import java.util.stream.Collectors;
 
 @Model(adaptables = SlingHttpServletRequest.class)
 public class PathListReportExecutor implements ReportExecutor {
-
-    private static final Logger log = LoggerFactory.getLogger(PathListReportExecutor.class);
 
     private int currentPage;
 
@@ -100,4 +115,3 @@ public class PathListReportExecutor implements ReportExecutor {
         }
     }
 }
-
