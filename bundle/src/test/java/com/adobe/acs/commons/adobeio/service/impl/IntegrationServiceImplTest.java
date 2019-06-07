91662f5397d1d08e43a3464d61e3afdc1c1c15eb
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-06-07 14:27:40 +0200

    Fix codeclimate issues

--- bundle/src/test/java/com/adobe/acs/commons/adobeio/service/impl/IntegrationServiceImplTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/adobeio/service/impl/IntegrationServiceImplTest.java
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,7 +32,7 @@ import junitx.util.PrivateAccessor;
 
 public final class IntegrationServiceImplTest {
 
-    private final static String validkey;
+    private static final String validkey;
 
     static {
         String key = "";
@@ -84,7 +84,7 @@ public final class IntegrationServiceImplTest {
 
     @Test
     public void testConifg() throws NoSuchFieldException {
-    	impl.activate(config);;
+        impl.activate(config);;
         assertEquals(config, PrivateAccessor.getField(impl, "jwtServiceConfig"));
     }
 
