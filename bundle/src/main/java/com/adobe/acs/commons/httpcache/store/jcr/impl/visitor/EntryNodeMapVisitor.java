51b78dada5747472e466f633507ebbaedfa2372b
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-06-10 19:18:58 +0200

    Remove final modifier

--- bundle/src/main/java/com/adobe/acs/commons/httpcache/store/jcr/impl/visitor/EntryNodeMapVisitor.java
+++ bundle/src/main/java/com/adobe/acs/commons/httpcache/store/jcr/impl/visitor/EntryNodeMapVisitor.java
@@ -70,12 +70,12 @@ public class EntryNodeMapVisitor extends AbstractNodeVisitor
         }
     }
 
-    protected final CacheContent getCacheContent(final Node node) throws RepositoryException
+    protected CacheContent getCacheContent(final Node node) throws RepositoryException
     {
         return new EntryNodeToCacheContentHandler(node).get();
     }
 
-    protected final CacheKey getCacheKey(final Node node) throws RepositoryException, IOException, ClassNotFoundException {
+    protected CacheKey getCacheKey(final Node node) throws RepositoryException, IOException, ClassNotFoundException {
         return new EntryNodeToCacheKeyHandler(node, dclm).get();
     }
 }
