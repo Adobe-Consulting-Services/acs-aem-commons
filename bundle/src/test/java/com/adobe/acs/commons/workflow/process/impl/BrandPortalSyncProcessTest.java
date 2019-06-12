e656089da4b134d054f7f46ce4d303928b99d354
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-06-12 11:33:50 +0200

    Clean up

--- bundle/src/test/java/com/adobe/acs/commons/workflow/process/impl/BrandPortalSyncProcessTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/workflow/process/impl/BrandPortalSyncProcessTest.java
@@ -23,6 +23,7 @@ package com.adobe.acs.commons.workflow.process.impl;
 import com.adobe.acs.commons.util.WorkflowHelper;
 import com.adobe.acs.commons.workflow.WorkflowPackageManager;
 import com.adobe.cq.dam.mac.sync.api.DAMSyncService;
+import com.adobe.granite.workflow.WorkflowException;
 import com.adobe.granite.workflow.WorkflowSession;
 import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowData;
@@ -31,6 +32,8 @@ import com.adobe.granite.workflow.metadata.SimpleMetaDataMap;
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.commons.util.DamUtil;
 import com.day.cq.replication.ReplicationActionType;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.testing.mock.osgi.junit.OsgiContext;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
@@ -48,6 +51,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jcr.RepositoryException;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
@@ -55,7 +60,19 @@ import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(DamUtil.class)
-public class BrandPortalSyncProcessTest {
+public final class BrandPortalSyncProcessTest {
+
+    private static final String ASSET_PATH = "/content/dam/foo.png";
+
+    private static final String PROCESS_ARGS = "PROCESS_ARGS";
+
+    private static final String ACTIVATE_SMALL = "activate";
+
+    private static final String DEACTIVATE_SMALL = "deactivate";
+
+    private static final String ACTIVATE_CAPITALS = StringUtils.upperCase(ACTIVATE_SMALL);
+
+    private static final String DEACTIVATE_CAPITALS = StringUtils.upperCase(DEACTIVATE_SMALL);
 
     @Rule
     public final OsgiContext osgiContext = new OsgiContext();
@@ -63,94 +80,103 @@ public class BrandPortalSyncProcessTest {
     @Rule
     public final SlingContext context = new SlingContext(ResourceResolverType.RESOURCERESOLVER_MOCK);
 
-    @Mock
-    DAMSyncService damSyncService;
-
-    @Mock
-    WorkflowHelper workflowHelper;
-
-    @Mock
-    WorkflowPackageManager workflowPackageManager;
-
-    @Mock
-    Asset asset;
-
-    @Mock
-    WorkItem workItem;
-
-    @Mock
-    WorkflowData workflowData;
-
-    @Mock
-    WorkflowSession workflowSession;
-
     @InjectMocks
-    BrandPortalSyncProcess workflowProcess = new BrandPortalSyncProcess();
+    private final BrandPortalSyncProcess workflowProcess = new BrandPortalSyncProcess();
 
-    MetaDataMap metadataMap;
+    private final MetaDataMap metadataMap = new SimpleMetaDataMap();
 
-    List<String> paths;
+    private final List<String> paths = new ArrayList<>();
 
-    String assetPath = "/content/dam/foo.png";
+    @Mock
+    private DAMSyncService damSyncService;
+
+    @Mock
+    private WorkflowHelper workflowHelper;
+
+    @Mock
+    private WorkflowPackageManager workflowPackageManager;
+
+    @Mock
+    private Asset asset;
+
+    @Mock
+    private WorkItem workItem;
+
+    @Mock
+    private WorkflowData workflowData;
+
+    @Mock
+    private WorkflowSession workflowSession;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws RepositoryException {
         PowerMockito.mockStatic(DamUtil.class);
 
-        metadataMap = new SimpleMetaDataMap();
-        paths = new ArrayList<>();
-        paths.add(assetPath);
+        paths.add(ASSET_PATH);
 
         when(workItem.getWorkflowData()).thenReturn(workflowData);
-        when(workflowData.getPayload()).thenReturn(assetPath);
+        when(workflowData.getPayload()).thenReturn(ASSET_PATH);
 
         when(workflowHelper.getResourceResolver(workflowSession)).thenReturn(context.resourceResolver());
         when(workflowPackageManager.getPaths(eq(context.resourceResolver()), anyString())).thenReturn(paths);
-        when(asset.getPath()).thenReturn(assetPath);
+        when(asset.getPath()).thenReturn(ASSET_PATH);
         when(DamUtil.resolveToAsset(any(Resource.class))).thenReturn(asset);
     }
 
     @Test
-    public void execute_activate() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "activate");
+    public void execute_activate() throws WorkflowException {
+        putProcessArgs(ACTIVATE_SMALL);
         workflowProcess.execute(workItem, workflowSession, metadataMap);
         verify(damSyncService, times(1)).publishResourcesToMP(any(List.class), eq(context.resourceResolver()));
     }
 
     @Test
-    public void execute_deactivate() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "deactivate");
+    public void execute_deactivate() throws WorkflowException {
+        putProcessArgs(DEACTIVATE_SMALL);
         workflowProcess.execute(workItem, workflowSession, metadataMap);
         verify(damSyncService, times(1)).unpublishResourcesFromMP(any(List.class), eq(context.resourceResolver()));
     }
 
     @Test
-    public void getReplicationActionType_activate() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "activate");
-        assertEquals(ReplicationActionType.ACTIVATE, workflowProcess.getReplicationActionType(metadataMap));
+    public void getReplicationActionType_activate()  {
+        putProcessArgs(ACTIVATE_SMALL);
+        assertReplicationActionType(ReplicationActionType.ACTIVATE);
     }
 
     @Test
-    public void getReplicationActionType_activate_caps() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "ACTIVATE");
-        assertEquals(ReplicationActionType.ACTIVATE, workflowProcess.getReplicationActionType(metadataMap));
+    public void getReplicationActionType_activate_caps() {
+        putProcessArgs(ACTIVATE_CAPITALS);
+        assertReplicationActionType(ReplicationActionType.ACTIVATE);
     }
 
     @Test
-    public void getReplicationActionType_deactivate() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "DEACTIVATE");
-        assertEquals(ReplicationActionType.DEACTIVATE, workflowProcess.getReplicationActionType(metadataMap));
+    public void getReplicationActionType_deactivate() {
+        putProcessArgs(DEACTIVATE_CAPITALS);
+        assertReplicationActionType(ReplicationActionType.DEACTIVATE);
     }
 
     @Test
-    public void getReplicationActionType_deactivate_caps() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "DEACTIVATE");
-        assertEquals(ReplicationActionType.DEACTIVATE, workflowProcess.getReplicationActionType(metadataMap));
+    public void getReplicationActionType_deactivate_caps() {
+        putProcessArgs(DEACTIVATE_CAPITALS);
+        assertReplicationActionType(ReplicationActionType.DEACTIVATE);
     }
 
     @Test
-    public void getReplicationActionType_null() throws Exception {
-        metadataMap.put("PROCESS_ARGS", "null");
-        assertNull(workflowProcess.getReplicationActionType(metadataMap));
+    public void getReplicationActionType_null() {
+        putProcessArgs("null");
+        assertNull(getReplicationActionType());
     }
+
+    private void putProcessArgs(final String args) {
+        metadataMap.put(PROCESS_ARGS, args);
+    }
+
+    private void assertReplicationActionType(final ReplicationActionType type) {
+        assertEquals(ReplicationActionType.ACTIVATE, getReplicationActionType());
+    }
+
+    private ReplicationActionType getReplicationActionType() {
+        return workflowProcess.getReplicationActionType(metadataMap);
+    }
+
 }
