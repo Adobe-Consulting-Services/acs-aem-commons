fe133a6fb531becaebb2abddd8dbbe7713494c7f
Jiang <jilong@adobe.com>
2019-11-06 15:39:01 +0900

    Revert "Automated Package Replicator Touch UI Support. Fix #2087"
    This reverts commit 544d3064b0d731439a9231e8d1fd7d4d75f64e1b.

--- ui.apps/src/main/content/jcr_root/apps/acs-commons/components/utilities/automatic-package-replication/clientlibs/app.js
+++ ui.apps/src/main/content/jcr_root/apps/acs-commons/components/utilities/automatic-package-replication/clientlibs/app.js
@@ -18,13 +18,17 @@
  * #L%
  */
 /*global angular: false */
-angular.module('acs-commons-automatic-package-replication-item-page-app', ['acsCoral', 'ACS.Commons.notifications'])
+angular.module('acs-commons-automatic-package-replication-app', ['acsCoral', 'ACS.Commons.notifications'])
     .controller('MainCtrl', ['$scope', '$http', '$timeout', 'NotificationsService',
     function ($scope, $http, $timeout, NotificationsService) {
 
         $scope.save = function () {
+        	
 			NotificationsService.running(true);
+            
+
             var $form = $('#fn-acsCommons-APR-form');
+
             $.post($form.attr('action'), $form.serialize(), function() {
                location.reload(true);
             });
