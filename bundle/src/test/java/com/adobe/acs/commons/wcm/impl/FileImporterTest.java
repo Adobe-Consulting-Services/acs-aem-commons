5f14ed9455a4ece76f14336b50da7620eb46bc84
Jörg Hoh <jhoh@adobe.com>
2019-11-10 13:35:33 +0100

    convert unittests to SlingContext

--- bundle/src/test/java/com/adobe/acs/commons/wcm/impl/FileImporterTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/wcm/impl/FileImporterTest.java
@@ -1,26 +1,10 @@
-/*
- * #%L
- * ACS AEM Commons Bundle
- * %%
- * Copyright (C) 2014 Adobe
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
 package com.adobe.acs.commons.wcm.impl;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -33,10 +17,11 @@ import javax.jcr.Session;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.commons.JcrUtils;
+import org.apache.sling.api.resource.NonExistingResource;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.commons.mime.MimeTypeService;
-import org.apache.sling.commons.testing.jcr.RepositoryProvider;
-import org.junit.After;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.apache.sling.testing.mock.sling.junit.SlingContext;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -44,10 +29,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+
 @RunWith(MockitoJUnitRunner.class)
 public class FileImporterTest {
 
-    private RepositoryProvider provider;
+	
+	private final SlingContext context = new SlingContext(ResourceResolverType.JCR_OAK);
 
     @InjectMocks
     private FileImporter importer = new FileImporter();
@@ -63,28 +50,19 @@ public class FileImporterTest {
 
     @Before
     public void setup() throws Exception {
-        provider = RepositoryProvider.instance();
+
         importer.activate(Collections.<String, Object> emptyMap());
         testFile = new File("src/test/resources/com/adobe/acs/commons/email/impl/emailTemplate.txt");
         when(mimeTypeService.getMimeType("emailTemplate.txt")).thenReturn("text/plain");
 
-        session = provider.getRepository().loginAdministrative(null);
+        session = context.resourceResolver().adaptTo(Session.class);
         folder = session.getRootNode().addNode(RandomStringUtils.randomAlphabetic(10), JcrConstants.NT_FOLDER);
         session.save();
     }
 
-    @After
-    public void teardown() {
-        if (session != null) {
-            session.logout();
-            session = null;
-        }
-    }
-
     @Test
     public void testImportToFolder() throws Exception {
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(folder);
+    	Resource resource = context.resourceResolver().getResource(folder.getPath());
         importer.importData("file", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -100,8 +78,7 @@ public class FileImporterTest {
 
         session.save();
         
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(folder);
+        Resource resource = context.resourceResolver().getResource(folder.getPath());
         importer.importData("file", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -118,8 +95,7 @@ public class FileImporterTest {
 
         session.save();
         
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(folder);
+        Resource resource = context.resourceResolver().getResource(folder.getPath());
         importer.importData("file", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -138,8 +114,7 @@ public class FileImporterTest {
 
         session.save();
 
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(file);
+        Resource resource = context.resourceResolver().getResource(file.getPath());
         importer.importData("file", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -156,8 +131,7 @@ public class FileImporterTest {
 
         session.save();
 
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(file);
+        Resource resource = context.resourceResolver().getResource(file.getPath());
         importer.importData("file", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -169,8 +143,7 @@ public class FileImporterTest {
 
     @Test
     public void testWrongScheme() throws Exception {
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(folder);
+    	Resource resource = context.resourceResolver().getResource(folder.getPath());
         importer.importData("file2", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -179,8 +152,7 @@ public class FileImporterTest {
 
     @Test
     public void testNullAdaptation() throws Exception {
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(null);
+    	Resource resource = new NonExistingResource(context.resourceResolver(), "/var/non/existing/resource");
         importer.importData("file", testFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
@@ -190,8 +162,7 @@ public class FileImporterTest {
     @Test
     public void testImportNoSuchFile() throws Exception {
         File badFile = new File("src/test/resources/NONEXISTING.txt");
-        Resource resource = mock(Resource.class);
-        when(resource.adaptTo(Node.class)).thenReturn(folder);
+        Resource resource = context.resourceResolver().getResource(folder.getPath());
         importer.importData("file", badFile.getAbsolutePath(), resource);
 
         assertFalse(session.hasPendingChanges());
