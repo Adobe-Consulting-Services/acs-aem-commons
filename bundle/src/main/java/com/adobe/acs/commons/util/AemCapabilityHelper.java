e270f3928cd9e0cb7e47ddd568e9de06d9aafaaa
david g <davidjgonzalez@gmail.com>
2020-02-19 07:37:26 -0500

    #2190 -  POC; Simplification to cloud-ready (true/false) and consolidated into existing OSGi Component

--- bundle/src/main/java/com/adobe/acs/commons/util/AemCapabilityHelper.java
+++ bundle/src/main/java/com/adobe/acs/commons/util/AemCapabilityHelper.java
@@ -21,11 +21,55 @@ package com.adobe.acs.commons.util;
 
 import javax.jcr.RepositoryException;
 
+import com.adobe.acs.commons.util.compatability.AemCompatibility;
 import org.osgi.annotation.versioning.ProviderType;
 
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * This OSGi Service can be used in 2 different ways....
+ *
+ * 1. It can be used In-line in code to detect the Hosting type (Cloud or Non-Cloud)
+ *
+ * @Reference
+ * AemCapabilityHelper aemCapabilityHelper;
+ * ...
+ * if ((aemCapabilityHelper.isCloudReady()) {
+ *     .. Do something that only works for the Cloud ..
+ * } else {
+ *     .. Do something for AMS / On-Prem ..
+ * }
+ *
+ * 2. It can be used to allow/stop OSGi Components from starting entirely
+ *
+ *
+ * @Component
+ * public class IOnlyWorkOnTheCloud implements Foo {
+ *
+ *    @Reference(
+ *        target="&(cloud-ready=true)"
+ *        scope = ReferenceCardinality.MANDATORY
+ *    )
+ *    AemCapabilityHelper aemCapabilityHelper;
+ *    ...
+ * }
+ *
+ * ... OR ...
+ *
+ * @Component
+ *   public class IOnlyWorkOnAmsOrOnPrem implements Bar {
+ *
+ *      @Reference(
+ *          target="&(cloud-ready=false)"
+ *          scope = ReferenceCardinality.MANDATORY
+ *      )
+ *      AemCapabilityHelper aemCapabilityHelper;
+ *      ...
+ *  }
+ */
 @ProviderType
 public interface AemCapabilityHelper {
-
     /**
      * Determines if the AEM installation is running on an Apache Jackrabbit Oak-based repository.
      * 
@@ -33,7 +77,14 @@ public interface AemCapabilityHelper {
      * so the usage of this method is no longer required.
      * @return true is running on Oak
      * @throws RepositoryException
-     * @Deprecated
+     * @Deprecated All ACS AEM Commons supported Repos are Oak now; This should always return true.
      */
     boolean isOak() throws RepositoryException;
+
+    /**
+     * Checks if the AEM running is "AEM as a Cloud Service" or an AMS/On-Prem/6.x version of AEM
+     *
+     * @return true if the AEM is running as AEM as a Cloud Service (either in Adobe Cloud as AEM as a CLoud Service OR AEM as a Cloud Service SDK QuickStart Jar)
+     */
+    boolean isCloudReady();
 }
--- bundle/src/main/java/com/adobe/acs/commons/util/impl/AemCapabilityHelperImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/util/impl/AemCapabilityHelperImpl.java
@@ -21,21 +21,89 @@ package com.adobe.acs.commons.util.impl;
 
 import javax.jcr.RepositoryException;
 
+import com.adobe.granite.license.ProductInfo;
+import com.adobe.granite.license.ProductInfoProvider;
 import org.apache.commons.lang.StringUtils;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.jcr.api.SlingRepository;
 
 import com.adobe.acs.commons.util.AemCapabilityHelper;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.Version;
+import org.osgi.service.component.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  * ACS AEM Commons - AEM Capability Helper
+ *
  * Provides information about the current AEM installation and what it can and can't do.
  */
 @Component
-@Service
 public class AemCapabilityHelperImpl implements AemCapabilityHelper {
+    private static final Logger log = LoggerFactory.getLogger(AemCapabilityHelperImpl.class);
+    private static final String PN_CLOUD_READY = "cloud-ready";
+    private static final String PN_VERSION = "version";
+
+    // This is the first Major/Minor GA Version of AEM as a Cloud Service
+    private static final Version originalCloudServiceVersion = new Version(2019, 12,   0);
+
+    @Reference(
+            policy = ReferencePolicy.DYNAMIC,
+            policyOption = ReferencePolicyOption.GREEDY
+    )
+    private transient ProductInfoProvider productInfoProvider;
+    private transient ProductInfo productInfo;
+
+    @Override
+    public boolean isCloudReady() {
+        if (productInfo.getVersion().compareTo(originalCloudServiceVersion) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private transient ServiceRegistration serviceRegistration;
+
+    @Activate
+    protected void activate(final BundleContext bundleContext) {
+        productInfo = productInfoProvider.getProductInfo();
+
+        final Dictionary<String, String> properties = new Hashtable<>();
+
+        String cloudReady;
+        String version = productInfo.getShortVersion();
+
+        if (isCloudReady()) {
+            cloudReady = Boolean.TRUE.toString();
+        } else {
+            cloudReady = Boolean.FALSE.toString();
+            log.debug("Registering [ {} ] as an OSGi Service so it be be used to enable/disable other OSGi Components", this.getClass().getSimpleName());
+        }
+
+        properties.put(PN_CLOUD_READY, cloudReady);
+        properties.put(PN_VERSION, version);
+
+        serviceRegistration = bundleContext.registerService(this.getClass().getName(), this, new Hashtable<>());
+
+        log.debug("Registering [ AemCapabilityHelper ] as an OSGi Service with OSGi properties [ cloud-ready = {}, version = {} ] so it be be used to enable/disable other OSGi Components",
+                properties.get(PN_CLOUD_READY), properties.get(PN_VERSION));
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        productInfo = null;
+
+        if (serviceRegistration != null) {
+            serviceRegistration.unregister();
+        }
+    }
+
+    /** Deprecated **/
 
     @Reference
     private SlingRepository slingRepository;
