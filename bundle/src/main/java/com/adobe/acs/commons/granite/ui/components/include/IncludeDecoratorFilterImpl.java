ed737b409de435ff14bde8eab531684552d0858a
Niek Raaijmakers <niek@adobe.com>
2020-03-05 12:52:33 +0100

    CodeClimate fixes

--- bundle/src/main/java/com/adobe/acs/commons/granite/ui/components/include/IncludeDecoratorFilterImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/granite/ui/components/include/IncludeDecoratorFilterImpl.java
@@ -6,6 +6,25 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.engine.EngineConstants;
 import org.osgi.service.component.annotations.Component;
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2017 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 
 import javax.annotation.CheckForNull;
@@ -30,7 +49,7 @@ public class IncludeDecoratorFilterImpl implements Filter {
     
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        // no-op
     }
 
     @Override
@@ -90,6 +109,6 @@ public class IncludeDecoratorFilterImpl implements Filter {
     
     @Override
     public void destroy() {
-
+        // no-op
     }
 }
--- bundle/src/main/java/com/adobe/acs/commons/granite/ui/components/include/NamespaceDecoratedValueMapBuilder.java
+++ bundle/src/main/java/com/adobe/acs/commons/granite/ui/components/include/NamespaceDecoratedValueMapBuilder.java
@@ -1,6 +1,26 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2017 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.granite.ui.components.include;
 
 import com.adobe.acs.commons.util.TypeUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
@@ -126,7 +146,9 @@ public class NamespaceDecoratedValueMapBuilder {
             String requestParamValue = (request.getAttribute(PREFIX + paramKey) != null) ? request.getAttribute(PREFIX + paramKey).toString() : null;
             String chosenValue = defaultString(requestParamValue, defaultValue);
             
-            if(chosenValue == null) chosenValue = "";
+            if(chosenValue == null){
+                chosenValue = StringUtils.EMPTY;
+            }
             
             matcher.appendReplacement(buffer, chosenValue);
             
