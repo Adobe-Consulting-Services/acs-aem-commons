22623fbd1b30491168b4ac351a2eed9a272b6b2f
Rajeev Yadav <Rajeev.X.Yadav@kp.org>
2020-03-08 12:07:19 -0400

    2199 : Added /conf permissions for email-service user using rep:policy node

--- bundle/src/main/java/com/adobe/acs/commons/users/impl/ProtectedAuthorizables.java
+++ bundle/src/main/java/com/adobe/acs/commons/users/impl/ProtectedAuthorizables.java
@@ -151,6 +151,7 @@ public final class ProtectedAuthorizables {
                 "acs-commons-bulk-workflow-service",
                 "acs-commons-error-page-handler-service",
                 "acs-commons-wcm-inbox-cleanup-service",
+                "acs-commons-email-service",
                 "acs-commons-package-replication-status-event-service",
                 "acs-commons-component-error-handler-service",
                 "acs-commons-dispatcher-flush-service",
