f4d26d6952573478371ff614ee9df954ba07e7da
Mark Adamcin <adamcin@gmail.com>
2020-04-28 09:25:45 -0700

    bring ProcessErrorReportExcelServlet to full coverage

--- bundle/src/test/java/com/adobe/acs/commons/mcp/impl/ProcessErrorReportExcelServletTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/mcp/impl/ProcessErrorReportExcelServletTest.java
@@ -21,18 +21,30 @@ package com.adobe.acs.commons.mcp.impl;
 
 import com.adobe.acs.commons.mcp.model.ArchivedProcessFailure;
 import com.adobe.acs.commons.mcp.model.ManagedProcess;
+import com.adobe.acs.commons.util.impl.ActivatorHelper;
+import io.wcm.testing.mock.aem.junit.AemContext;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import javax.servlet.ServletException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -41,12 +53,20 @@ import static org.mockito.Mockito.when;
  */
 public class ProcessErrorReportExcelServletTest {
 
+    private static final String RESOURCE_PATH = "/content/child";
+
     public ProcessErrorReportExcelServletTest() {
     }
 
     ManagedProcess process;
     ProcessErrorReportExcelServlet servlet;
     List<ArchivedProcessFailure> failures;
+
+    private ActivatorHelper activatorHelper = new ActivatorHelper();
+
+    @Rule
+    public final AemContext context = new AemContext(activatorHelper.afterSetup(), activatorHelper.beforeTeardown(),
+            ResourceResolverType.JCR_MOCK);
 
     @Before
     public void setUp() {
@@ -71,8 +91,8 @@ public class ProcessErrorReportExcelServletTest {
      */
     @Test
     public void createSpreadsheet() throws Exception {
-        final int overB2Width = 256 * 20;
-        final int overC3Width = 256 * 121;
+        final int overB2Width = servlet.getColumnBlockSize() * servlet.getMinColumnBlockCount();
+        final int overC3Width = servlet.getColumnBlockSize() * (servlet.getMaxColumnBlockCount() + 1);
         addFailure("path1", "error1", "stacktrace1");
         addFailure("path2", repeatUntilMinLengthReached("error2", overB2Width), "stacktrace2");
         addFailure("path3", "error3", repeatUntilMinLengthReached("stacktrace3", overC3Width));
@@ -92,8 +112,6 @@ public class ProcessErrorReportExcelServletTest {
 
         assertTrue("expect C column width is less than requested: " + cWidth + " < " + overC3Width,
                 cWidth < overC3Width);
-
-
     }
 
     static String repeatUntilMinLengthReached(final String value, final int minLength) {
@@ -102,4 +120,54 @@ public class ProcessErrorReportExcelServletTest {
         return StringUtils.repeat(value, repeat);
     }
 
+    @Test
+    public void testAutosizeException() {
+        final Sheet sheet = mock(Sheet.class);
+        final CompletableFuture<Integer> newWidth = new CompletableFuture<>();
+        final int autosizeCol = 1;
+
+        doThrow(new RuntimeException("This is a totally expected mock error")).when(sheet).autoSizeColumn(anyInt());
+        doAnswer(call -> {
+            if (call.getArgument(0, Integer.class).equals(autosizeCol)) {
+                return newWidth.getNow(servlet.getColumnBlockSize() * (servlet.getMaxColumnBlockCount() + 1));
+            }
+            return servlet.getColumnBlockSize() * servlet.getMinColumnBlockCount();
+        }).when(sheet).getColumnWidth(anyInt());
+        doAnswer(call -> {
+            if (call.getArgument(0, Integer.class).equals(autosizeCol)) {
+                newWidth.complete(call.getArgument(1));
+            }
+            return true;
+        }).when(sheet).setColumnWidth(anyInt(), anyInt());
+
+        final int expectAutosizedWith = servlet.getColumnBlockSize() * servlet.getMaxColumnBlockCount();
+        servlet.autosize(sheet, autosizeCol);
+        assertTrue("width was set", newWidth.isDone());
+        assertEquals("expect width", expectAutosizedWith, sheet.getColumnWidth(autosizeCol));
+    }
+
+    @Test
+    public void testDoGet() throws Exception {
+        this.context.create().resource(RESOURCE_PATH, "prop", "resourceval");
+        this.context.registerAdapter(Resource.class, ManagedProcess.class, process);
+        this.context.currentResource(RESOURCE_PATH);
+        servlet.doGet(this.context.request(), this.context.response());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testDoGet_innerThrow() throws Exception {
+        this.context.create().resource(RESOURCE_PATH, "prop", "resourceval");
+        this.context.registerAdapter(Resource.class, ManagedProcess.class, process);
+        this.context.currentResource(RESOURCE_PATH);
+        SlingHttpServletResponse response = mock(SlingHttpServletResponse.class);
+        doThrow(IllegalStateException.class).when(response).getOutputStream();
+        servlet.doGet(this.context.request(), response);
+    }
+
+    @Test(expected = ServletException.class)
+    public void testDoGet_outerThrow() throws Exception {
+        this.context.create().resource(RESOURCE_PATH, "prop", "resourceval");
+        this.context.currentResource(RESOURCE_PATH);
+        servlet.doGet(this.context.request(), this.context.response());
+    }
 }
