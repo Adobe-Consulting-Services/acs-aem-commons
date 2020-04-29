b5fdd615442d434d20d9a08c45006db8c02f1bda
ADOBENET\jasiwal <jasiwal@adobe.com>
2020-04-29 22:05:14 +0530

    Reverting the "immediate=true" in service.

--- bundle/src/main/java/com/adobe/acs/commons/indesign/dynamicdeckdynamo/services/impl/DynamicDeckConfigurationServiceImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/indesign/dynamicdeckdynamo/services/impl/DynamicDeckConfigurationServiceImpl.java
@@ -24,7 +24,7 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.metatype.annotations.Designate;
 
-@Component(service = DynamicDeckConfigurationService.class)
+@Component(service = DynamicDeckConfigurationService.class, immediate=true)
 @Designate(ocd = DynamicDeckConfiguration.class)
 public class DynamicDeckConfigurationServiceImpl implements DynamicDeckConfigurationService {
 
