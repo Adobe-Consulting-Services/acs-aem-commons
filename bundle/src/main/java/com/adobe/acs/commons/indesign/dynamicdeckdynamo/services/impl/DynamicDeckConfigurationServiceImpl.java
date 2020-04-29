e329780a61702d60820fbfefac7b09832c0d5ba6
David Gonzalez <davidjgonzalez@gmail.com>
2020-04-29 12:13:52 -0400

    Removed "immediate = true" from service; activate when used.

--- bundle/src/main/java/com/adobe/acs/commons/indesign/dynamicdeckdynamo/services/impl/DynamicDeckConfigurationServiceImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/indesign/dynamicdeckdynamo/services/impl/DynamicDeckConfigurationServiceImpl.java
@@ -24,7 +24,7 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.metatype.annotations.Designate;
 
-@Component(service = DynamicDeckConfigurationService.class, immediate = true)
+@Component(service = DynamicDeckConfigurationService.class)
 @Designate(ocd = DynamicDeckConfiguration.class)
 public class DynamicDeckConfigurationServiceImpl implements DynamicDeckConfigurationService {
 
