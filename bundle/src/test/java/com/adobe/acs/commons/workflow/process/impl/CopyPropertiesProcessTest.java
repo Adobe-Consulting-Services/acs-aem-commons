549b6db61ae4d9ca56c419dc2fab5388d77ceda2
David Gonzalez <davidjgonzalez@gmail.com>
2020-05-09 11:39:12 -0400

    Dynamic Deck Dynamo - Added missing package-info.java to utils package

---
+++ bundle/src/test/java/com/adobe/acs/commons/workflow/process/impl/CopyPropertiesProcessTest.java
@@ -0,0 +1,17 @@
+package com.adobe.acs.commons.workflow.process.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CopyPropertiesProcessTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void execute() {
+    }
+}
