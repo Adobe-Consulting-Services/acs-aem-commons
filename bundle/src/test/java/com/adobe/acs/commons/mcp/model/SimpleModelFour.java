0e186149a160cc2b22f23981f7d09a0c62a4f039
Joerg Hoh <joerg@joerghoh.de>
2020-05-21 11:39:22 +0200

    enumerate the model classes we will work with in the test, extend test coverage a bit

---
+++ bundle/src/test/java/com/adobe/acs/commons/mcp/model/SimpleModelFour.java
@@ -0,0 +1,41 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2019 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.adobe.acs.commons.mcp.model;
+
+import com.adobe.acs.commons.mcp.form.DialogProvider;
+import com.adobe.acs.commons.mcp.form.FormField;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+/**
+ * Simple sling model with a resource type declared via a getter function.
+ * This should be detected via the DialogProvider annotation.
+ */
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class})
+@DialogProvider
+public class SimpleModelFour {
+    @FormField(name = "Field 1")
+    private String field1;
+
+    public String getResourceType() {
+        return "test/model4";
+    }
+}
