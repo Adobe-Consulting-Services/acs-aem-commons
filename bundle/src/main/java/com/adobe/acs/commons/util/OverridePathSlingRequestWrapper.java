b8a7b4574576f1460b62458a05bfe43ea5bd0677
Brett Birschbach <brett.birschbach@bounteous.com>
2020-07-30 12:45:19 -0500

    - Fixed issue where sling model injection by @ChildResourceFromRequestInjector was not including all sling model bindings, thus resulting in models being injected with incorrect values for thing like currentStyle

--- bundle/src/main/java/com/adobe/acs/commons/util/OverridePathSlingRequestWrapper.java
+++ bundle/src/main/java/com/adobe/acs/commons/util/OverridePathSlingRequestWrapper.java
@@ -28,10 +28,18 @@ import org.apache.sling.api.adapter.AdapterManager;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.scripting.SlingBindings;
 import org.apache.sling.api.wrappers.SlingHttpServletRequestWrapper;
+import org.apache.sling.scripting.api.AbstractScriptEngineFactory;
+import org.apache.sling.scripting.api.BindingsValuesProvider;
+import org.apache.sling.scripting.api.BindingsValuesProvidersByContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.SimpleBindings;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -51,7 +59,7 @@ public class OverridePathSlingRequestWrapper extends SlingHttpServletRequestWrap
     private final Map<Class<?>, Object> adaptersCache = new HashMap<>();
 
     /**
-     * Constructor.
+     * Constructor for generic request wrapper.
      *
      * @param request A valid sling request.
      * @param path    Path to represent by this request wrapper.
@@ -70,6 +78,7 @@ public class OverridePathSlingRequestWrapper extends SlingHttpServletRequestWrap
         this.myBindings.put(WCMBindings.PROPERTIES, this.resource.getValueMap());
         this.myBindings.put(SlingBindings.RESOURCE, this.resource);
         this.myBindings.put(SlingBindings.REQUEST, this);
+        this.myBindings.put(SlingBindings.RESOLVER, this.resource.getResourceResolver());
 
         Page currentPage = null;
         PageManager pageManager = getSlingRequest().getResourceResolver().adaptTo(PageManager.class);
@@ -77,6 +86,30 @@ public class OverridePathSlingRequestWrapper extends SlingHttpServletRequestWrap
             currentPage = pageManager.getContainingPage(this.resource);
         }
         this.myBindings.put(WCMBindings.CURRENT_PAGE, currentPage);
+    }
+
+    /**
+     * Constructor for request wrapper to be used for sling model injection.
+     *
+     * Ensures the wrapped request is created with all standard sling model bindings.
+     *
+     * @param request                          A valid sling request.
+     * @param path                             Path to represent by this request wrapper.
+     * @param bindingsValuesProvidersByContext Service reference to list of bindings providers.
+     */
+    public OverridePathSlingRequestWrapper(final SlingHttpServletRequest request, final String path, BindingsValuesProvidersByContext bindingsValuesProvidersByContext) {
+        this(request, path);
+
+        SimpleBindings additionalBindings = new SimpleBindings();
+        additionalBindings.putAll(this.myBindings);
+
+        Collection<BindingsValuesProvider> bindingsValuesProviders = bindingsValuesProvidersByContext.getBindingsValuesProviders(new SlingModelsScriptEngineFactory(), "request");
+        Iterator<BindingsValuesProvider> bindingsValuesProviderIterator = bindingsValuesProviders.iterator();
+        while (bindingsValuesProviderIterator.hasNext()) {
+            BindingsValuesProvider provider = bindingsValuesProviderIterator.next();
+            provider.addBindings(additionalBindings);
+        }
+        this.myBindings.putAll(additionalBindings);
     }
 
     @Override
@@ -115,6 +148,24 @@ public class OverridePathSlingRequestWrapper extends SlingHttpServletRequestWrap
             }
 
             return result;
+        }
+    }
+
+    private static class SlingModelsScriptEngineFactory extends AbstractScriptEngineFactory implements ScriptEngineFactory {
+        SlingModelsScriptEngineFactory() {
+            this.setNames(new String[]{"sling-models-exporter", "sling-models"});
+        }
+
+        public String getLanguageName() {
+            return null;
+        }
+
+        public String getLanguageVersion() {
+            return null;
+        }
+
+        public ScriptEngine getScriptEngine() {
+            return null;
         }
     }
 }
