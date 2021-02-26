2eb80574ae0a319386c6ffcae5afae2ceaaba1c0
YentlFrickx <frickx.yentl@gmail.com>
2021-02-26 10:12:24 +0100

    Fix codeclimate issue, add changes to changelog

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/DataImporter.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/DataImporter.java
@@ -31,7 +31,13 @@ import com.adobe.acs.commons.mcp.form.RadioComponent;
 import com.adobe.acs.commons.mcp.model.GenericReport;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
