7d7e2220ad9aca2674bcdf12117a7c6e5f219e69
Yegor Kozlov <yegor.kozlov@gmail.com>
2021-03-17 18:11:14 +0300

    Issue 2548 (#2549)
    * issue #2548: RedirectFilter requires an OSGi config to start, also make urlAdjuster volatile

--- ui.apps/src/main/content/jcr_root/apps/acs-commons/components/utilities/manage-redirects/serverlibs/context.js
+++ ui.apps/src/main/content/jcr_root/apps/acs-commons/components/utilities/manage-redirects/serverlibs/context.js
@@ -2,11 +2,13 @@ use(function () {
     var RedirectFilter = Packages.com.adobe.acs.commons.redirects.filter.RedirectFilter;
     var ResourceUtil = Packages.org.apache.sling.api.resource.ResourceUtil;
 
-    var redirectHome;
+    var redirectHome = "/conf/acs-commons/redirects";
+    var enabled = false;
     var filters = sling.getServices(Packages.javax.servlet.Filter, null);
     for(var i=0; i < filters.length; i++ ){
         if(filters[i] instanceof RedirectFilter){
             redirectHome = filters[i].storagePath;
+            enabled = true;
         }
     }
 
@@ -18,6 +20,7 @@ use(function () {
         "nt:unstructured", true);
 
     return {
+        disabled: !enabled, // will trigger an alert on manage-redirects.html
         redirectResource: redirectResource
      };
 });
