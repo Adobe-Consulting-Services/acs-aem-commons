f8680a5b9f7237b7288b08a34c90263c4876c75b
Yegor Kozlov <yegor.kozlov@gmail.com>
2021-04-09 09:49:45 +0300

    fix failing  ScrMetadataIT.test

--- bundle/src/test/java/com/adobe/acs/commons/it/build/ScrMetadataIT.java
+++ bundle/src/test/java/com/adobe/acs/commons/it/build/ScrMetadataIT.java
@@ -139,6 +139,8 @@ public class ScrMetadataIT {
         COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.granite.ui.components.impl.include.IncludeDecoratorFilterImpl:sling.filter.resourceTypes");
 
         COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.redirects.filter.RedirectFilter:storagePath");
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.redirects.filter.RedirectFilter:mapUrls");
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.redirects.filter.RedirectFilter:enabled");
 
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE = new HashSet<>();
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE.add("com.adobe.acs.commons.fam.impl.ThrottledTaskRunnerImpl:max.cpu");
