88b958e6ced5817d6d53da70599ff63ca68f1382
David Gonzalez <davidjgonzalez@gmail.com>
2021-08-12 15:43:01 -0400

    Fixed failing Scr test

--- bundle/src/test/java/com/adobe/acs/commons/it/build/ScrMetadataIT.java
+++ bundle/src/test/java/com/adobe/acs/commons/it/build/ScrMetadataIT.java
@@ -142,6 +142,9 @@ public class ScrMetadataIT {
         COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.redirects.filter.RedirectFilter:mapUrls");
         COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.redirects.filter.RedirectFilter:enabled");
 
+        // https://github.com/Adobe-Consulting-Services/acs-aem-commons/pull/2667
+        COMPONENT_PROPERTIES_TO_IGNORE.add("com.adobe.acs.commons.fam.impl.ThrottledTaskRunnerImpl:task.timeout");
+        
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE = new HashSet<>();
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE.add("com.adobe.acs.commons.fam.impl.ThrottledTaskRunnerImpl:max.cpu");
         COMPONENT_PROPERTIES_TO_IGNORE_FOR_TYPE_CHANGE.add("com.adobe.acs.commons.fam.impl.ThrottledTaskRunnerImpl:max.heap");
