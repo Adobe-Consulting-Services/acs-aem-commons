e562e05299a23092cbd1af6f79d35b121e9a3898
Nicholaus Chipping <nicholaus.chipping@bounteous.com>
2021-12-02 17:09:24 -0700

    Adding some test cases to increase coverage.

--- bundle/src/test/java/com/adobe/acs/commons/workflow/synthetic/impl/cq/SyntheticWorkflowSessionTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/workflow/synthetic/impl/cq/SyntheticWorkflowSessionTest.java
@@ -20,23 +20,43 @@
 
 package com.adobe.acs.commons.workflow.synthetic.impl.cq;
 
+import com.adobe.acs.commons.workflow.synthetic.SyntheticWorkflowModel;
 import com.adobe.acs.commons.workflow.synthetic.impl.SyntheticWorkflowData;
 import com.adobe.acs.commons.workflow.synthetic.impl.SyntheticWorkflowRunnerImpl;
+import com.day.cq.workflow.model.WorkflowModel;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.jcr.Session;
 
+import java.util.Map;
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SyntheticWorkflowSessionTest {
+    final String UNSUPPORTED_EXCEPTION = "Operation not supported by Synthetic Workflow";
+
+    @Mock
+    ResourceResolver resourceResolver;
+
+    @Mock
+    SyntheticWorkflowSession session;
+
+    @Before
+    public void setUp() {
+        session = new SyntheticWorkflowSession(new SyntheticWorkflowRunnerImpl(), resourceResolver.adaptTo(Session.class));
+    }
+
     @Test
     public void test_updateWorkflowData() throws Exception {
-        final SyntheticWorkflowSession session = new SyntheticWorkflowSession(new SyntheticWorkflowRunnerImpl(), mock(Session.class));
         final SyntheticWorkflowData workflowData = new SyntheticWorkflowData("JCR_PATH", "/content/test");
 
         workflowData.getMetaDataMap().put("cat", "meow");
@@ -53,5 +73,42 @@ public class SyntheticWorkflowSessionTest {
 
         Assert.assertEquals(3, workflowData.getMetaDataMap().size());
         Assert.assertEquals("woof", workflowData.getMetaDataMap().get("dog"));
+    }
+
+    @Test
+    public void test_getWorkflowService() throws Exception {
+        Assert.assertNotNull(session.getWorkflowService());
+    }
+
+    @Test
+    public void test_getSession() throws Exception {
+        Assert.assertNull(session.getSession());
+    }
+
+    @Test
+    public void test_getUser() throws Exception {
+        Assert.assertNull(session.getUser());
+    }
+
+    @Test
+    public void test_isSuperuser() throws Exception {
+        Assert.assertTrue(session.isSuperuser());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void test_evaluate() throws Exception {
+        final SyntheticWorkflowData workflowData = new SyntheticWorkflowData("JCR_PATH", "/content/test");
+
+        Assert.assertSame(UNSUPPORTED_EXCEPTION, session.evaluate(workflowData, "test"));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void test_createNewModel() throws Exception {
+        Assert.assertSame(UNSUPPORTED_EXCEPTION, session.createNewModel("test"));
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void test_createNewModelTwoStrings() throws Exception {
+        Assert.assertSame(UNSUPPORTED_EXCEPTION, session.createNewModel("test", "test"));
     }
 }
