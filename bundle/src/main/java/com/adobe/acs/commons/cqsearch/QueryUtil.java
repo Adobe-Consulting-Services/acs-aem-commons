9d7fffaa09c863a38cf4ae4de3d9e727b03b80bb
Mark Adamcin <adamcin@gmail.com>
2022-01-14 08:18:12 -0800

    code climate fixes

--- bundle/src/main/java/com/adobe/acs/commons/cqsearch/QueryUtil.java
+++ bundle/src/main/java/com/adobe/acs/commons/cqsearch/QueryUtil.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2022 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.cqsearch;
 
 import com.day.cq.search.Query;
@@ -10,8 +29,15 @@ import java.lang.reflect.Field;
 /**
  * Simple utility to use as an alternative to deprecated CloseableQuery in older versions of AEM.
  */
-public class QueryUtil {
+public final class QueryUtil {
     private static final Logger log = LoggerFactory.getLogger(QueryUtil.class);
+
+    /**
+     * No constructor.
+     */
+    private QueryUtil() {
+        // private constructor
+    }
 
     /**
      * Uses reflection to forcibly set the Query object's ResourceResolver to the provided.
