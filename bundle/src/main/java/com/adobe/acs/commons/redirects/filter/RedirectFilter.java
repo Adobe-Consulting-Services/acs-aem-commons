a9426e4e73e1bb174f2bb55c2fdd3a3d57b359fe
Bart Thierens <bart.thierens@acolad.com>
2022-03-18 13:26:47 +0100

    removed unused import

--- bundle/src/main/java/com/adobe/acs/commons/redirects/filter/RedirectFilter.java
+++ bundle/src/main/java/com/adobe/acs/commons/redirects/filter/RedirectFilter.java
@@ -49,7 +49,6 @@ import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import javax.annotation.Nonnull;
 import javax.management.NotCompliantMBeanException;
 import javax.management.openmbean.CompositeDataSupport;
 import javax.management.openmbean.CompositeType;
