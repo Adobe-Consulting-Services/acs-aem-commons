39b478b8b14a337097f7459e99a7115ed7f8d046
Dan Klco <klco@adobe.com>
2022-06-23 19:02:17 -0400

    Improving the process for configuring the marketo integration, fixes #2874

--- bundle/src/main/java/com/adobe/acs/commons/marketo/impl/MarketoClientConfigurationImpl.java
+++ bundle/src/main/java/com/adobe/acs/commons/marketo/impl/MarketoClientConfigurationImpl.java
@@ -19,6 +19,9 @@
  */
 package com.adobe.acs.commons.marketo.impl;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
@@ -26,82 +29,111 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import com.adobe.acs.commons.marketo.MarketoClientConfiguration;
 
 /**
- * A Model retrieving the configuration for interacting with the Marketo REST API
+ * A Model retrieving the configuration for interacting with the Marketo REST
+ * API
  */
-@Model(adaptables = Resource.class, adapters=MarketoClientConfiguration.class)
+@Model(adaptables = Resource.class, adapters = MarketoClientConfiguration.class)
 public class MarketoClientConfigurationImpl implements MarketoClientConfiguration {
 
-  @ValueMapValue
-  private String clientId;
+    private final String clientId;
 
-  @ValueMapValue
-  private String clientSecret;
+    private final String clientSecret;
 
-  @ValueMapValue
-  private String endpointHost;
+    private final String endpointHost;
 
-  @ValueMapValue
-  private String munchkinId;
+    private final String munchkinId;
 
-  @ValueMapValue
-  private String serverInstance;
+    private final String serverInstance;
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
+    @Inject
+    public MarketoClientConfigurationImpl(@ValueMapValue @Named("clientId") String clientId,
+            @ValueMapValue @Named("clientSecret") String clientSecret,
+            @ValueMapValue @Named("endpointHost") String endpointHost,
+            @ValueMapValue @Named("munchkinId") String munchkinId,
+            @ValueMapValue @Named("serverInstance") String serverInstance) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+        this.endpointHost = endpointHost;
+        this.munchkinId = munchkinId;
+        this.serverInstance = serverInstance;
     }
-    if (obj == null) {
-      return false;
+
+    private String getHost(String url) {
+        int start = url.indexOf("://");
+        if (url.startsWith("//")) { // handle the special case of starting with //
+            start = 2;
+        } else if (start < 0) { // no protocol
+            start = 0;
+        } else { // has protocol
+            start += 3;
+        }
+        int end = url.indexOf('/', start);
+        if (end < 0) {
+            end = url.length();
+        }
+        String domainName = url.substring(start, end);
+
+        // handle port
+        int port = domainName.indexOf(':');
+        if (port >= 0) {
+            domainName = domainName.substring(0, port);
+        }
+        return domainName;
     }
-    if (getClass() != obj.getClass()) {
-      return false;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MarketoClientConfigurationImpl other = (MarketoClientConfigurationImpl) obj;
+        if (clientId == null) {
+            if (other.clientId != null) {
+                return false;
+            }
+        } else if (!clientId.equals(other.clientId)) {
+            return false;
+        }
+        return true;
     }
-    MarketoClientConfigurationImpl other = (MarketoClientConfigurationImpl) obj;
-    if (clientId == null) {
-      if (other.clientId != null) {
-        return false;
-      }
-    } else if (!clientId.equals(other.clientId)) {
-      return false;
+
+    @Override
+    public String getClientId() {
+        return clientId;
     }
-    return true;
-  }
 
-  @Override
-  public String getClientId() {
-    return clientId;
-  }
-
-  @Override
-  public String getClientSecret() {
-    return clientSecret;
-  }
-
-  @Override
-  public String getEndpointHost() {
-    if(endpointHost.startsWith("https://")){
-      return endpointHost.substring("https://".length());
+    @Override
+    public String getClientSecret() {
+        return clientSecret;
     }
-    return endpointHost;
-  }
 
-  @Override
-  public String getMunchkinId() {
-    return munchkinId;
-  }
+    @Override
+    public String getEndpointHost() {
+        return getHost(endpointHost);
+    }
 
-  @Override
-  public String getServerInstance() {
-    return serverInstance;
-  }
+    @Override
+    public String getMunchkinId() {
+        return munchkinId;
+    }
 
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
-    return result;
-  }
+    @Override
+    public String getServerInstance() {
+        return getHost(serverInstance);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((clientId == null) ? 0 : clientId.hashCode());
+        return result;
+    }
 
 }
