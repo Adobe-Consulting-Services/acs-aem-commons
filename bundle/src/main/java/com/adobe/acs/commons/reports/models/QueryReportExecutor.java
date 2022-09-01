2a2736b2c6cb0ec2f2d120f0d5db16eb9637aa9f
Dan Klco <klco@adobe.com>
2022-09-01 13:46:53 -0400

    Fixes #2941 - Adding support for QueryBuilder queries in report builder

--- bundle/src/main/java/com/adobe/acs/commons/reports/models/QueryReportExecutor.java
+++ bundle/src/main/java/com/adobe/acs/commons/reports/models/QueryReportExecutor.java
@@ -34,6 +34,7 @@ import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+import javax.inject.Inject;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
@@ -45,20 +46,29 @@ import javax.jcr.query.QueryResult;
 import javax.jcr.query.Row;
 import javax.jcr.query.RowIterator;
 
-import com.adobe.acs.commons.reports.api.ReportException;
-import com.adobe.acs.commons.reports.api.ReportExecutor;
-import com.adobe.acs.commons.reports.api.ResultsPage;
-import com.github.jknack.handlebars.Handlebars;
-import com.github.jknack.handlebars.Template;
-
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.adobe.acs.commons.reports.api.ReportException;
+import com.adobe.acs.commons.reports.api.ReportExecutor;
+import com.adobe.acs.commons.reports.api.ResultsPage;
+import com.adobe.acs.commons.util.ParameterUtil;
+import com.adobe.acs.commons.util.impl.QueryHelperImpl;
+import com.day.cq.search.PredicateGroup;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.SearchResult;
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
 
 /**
  * Model for executing report requests.
@@ -66,144 +76,212 @@ import org.slf4j.LoggerFactory;
 @Model(adaptables = SlingHttpServletRequest.class)
 public class QueryReportExecutor implements ReportExecutor {
 
-  private static final Logger log = LoggerFactory.getLogger(QueryReportExecutor.class);
+    private static final Logger log = LoggerFactory.getLogger(QueryReportExecutor.class);
 
-  private QueryReportConfig config;
+    private QueryReportConfig config;
 
-  private int page;
+    private int page;
 
-  private SlingHttpServletRequest request;
+    private final SlingHttpServletRequest request;
+    private final QueryBuilder queryBuilder;
 
-  private String statement;
-
-  public QueryReportExecutor(SlingHttpServletRequest request) {
-    this.request = request;
-  }
-
-  private ResultsPage fetchResults(int limit, int offset) throws ReportException {
-    prepareStatement();
-    try {
-      ResourceResolver resolver = request.getResourceResolver();
-      QueryManager queryMgr = Optional.ofNullable(resolver.adaptTo(Session.class))
-          .orElseThrow(() -> new ReportException("Failed to get JCR Session")).getWorkspace().getQueryManager();
-
-      Query query = queryMgr.createQuery(statement, config.getQueryLanguage());
-
-      if (page != -1) {
-        log.debug("Fetching results with limit {} and offset {}", limit, offset);
-        query.setLimit(limit);
-        query.setOffset(offset);
-      } else {
-        log.debug("Fetching all results");
-      }
-      QueryResult result = query.execute();
-
-      NodeIterator nodes = result.getNodes();
-
-      Spliterator<Node> spliterator = Spliterators.spliteratorUnknownSize(nodes,
-          Spliterator.ORDERED | Spliterator.NONNULL);
-
-      Stream<Resource> results = StreamSupport.stream(spliterator, false).map(n -> getResource(n, resolver));
-
-      return new ResultsPage(results, config.getPageSize(), page, nodes.getSize());
-    } catch (RepositoryException re) {
-      throw new ReportException("Exception executing search results", re);
-    }
-  }
-
-  @Override
-  public ResultsPage getAllResults() throws ReportException {
-    return fetchResults(Integer.MAX_VALUE, 0);
-  }
-
-  @Override
-  public String getDetails() throws ReportException {
-    Map<String, String> details = new LinkedHashMap<>();
-    details.put("Language", config.getQueryLanguage());
-    details.put("Page", Integer.toString(page));
-    details.put("Page Size", Integer.toString(config.getPageSize()));
-    details.put("Query", statement);
-
-    try {
-      final QueryManager queryManager = Optional.ofNullable(request.getResourceResolver().adaptTo(Session.class))
-          .orElseThrow(() -> new ReportException("Failed to get JCR Session")).getWorkspace().getQueryManager();
-      final Query query = queryManager.createQuery("explain " + statement, config.getQueryLanguage());
-      final QueryResult queryResult = query.execute();
-
-      final RowIterator rows = queryResult.getRows();
-      while (rows.hasNext()) {
-        final Row row = rows.nextRow();
-
-        String[] cols = queryResult.getColumnNames();
-        Value[] values = row.getValues();
-
-        for (int i = 0; i < cols.length; i++) {
-          details.put(cols[i], values[i].getString());
-        }
-      }
-
-    } catch (RepositoryException re) {
-      throw new ReportException("Exception getting details", re);
-    }
-    StringBuilder sb = new StringBuilder();
-    for (Entry<String, String> entry : details.entrySet()) {
-      sb.append("<dt>" + StringEscapeUtils.escapeHtml(entry.getKey()) + "</dt>");
-      sb.append("<dd>" + StringEscapeUtils.escapeHtml(entry.getValue()) + "</dd>");
+    @Inject
+    public QueryReportExecutor(@Self SlingHttpServletRequest request, @OSGiService QueryBuilder queryBuilder) {
+        this.request = request;
+        this.queryBuilder = queryBuilder;
     }
 
-    return "<dl>" + sb.toString() + "</dl>";
-  }
+    private ResultsPage fetchResults(int limit, int offset) throws ReportException {
 
-  @Override
-  public String getParameters() throws ReportException {
-    List<String> params = new ArrayList<>();
-    Enumeration<String> keys = request.getParameterNames();
-    while (keys.hasMoreElements()) {
-      String key = keys.nextElement();
-      for (String value : request.getParameterValues(key)) {
         try {
-          params.add(URLEncoder.encode(key, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8"));
-        } catch (UnsupportedEncodingException e) {
-          throw new ReportException("UTF-8 encoding available", e);
+            final Pair<Stream<Resource>, Long> results;
+            if (isQueryBuilder()) {
+                results = getResultsFromQueryBuilder(limit, offset);
+            } else {
+                results = getResultsFromQuery(limit, offset);
+            }
+            return new ResultsPage(results.getLeft(), config.getPageSize(), page, results.getRight());
+        } catch (RepositoryException re) {
+            throw new ReportException("Exception executing search results", re);
         }
-      }
     }
-    return StringUtils.join(params, "&");
-  }
 
-  private Resource getResource(Node node, ResourceResolver resolver) {
-    try {
-      return resolver.getResource(node.getPath());
-    } catch (RepositoryException e) {
-      log.warn("Failed to get path from node: {}", node, e);
-      return null;
+    private boolean isQueryBuilder() {
+        return QueryHelperImpl.QUERY_BUILDER.equals(config.getQueryLanguage());
     }
-  }
 
-  @Override
-  public ResultsPage getResults() throws ReportException {
-    return fetchResults(config.getPageSize(), config.getPageSize() * page);
-  }
-
-  private void prepareStatement() throws ReportException {
-    try {
-      Map<String, String> parameters = getParamPatternMap(request);
-      Template template = new Handlebars().compileInline(config.getQuery());
-      statement = template.apply(parameters);
-      log.trace("Loaded statement: {}", statement);
-    } catch (IOException ioe) {
-      throw new ReportException("Exception templating query", ioe);
+    private Session getSession() throws ReportException {
+        return Optional.ofNullable(request.getResourceResolver().adaptTo(Session.class))
+                .orElseThrow(() -> new ReportException("Failed to get JCR Session"));
     }
-  }
 
-  @Override
-  public void setConfiguration(Resource config) {
-    this.config = config.adaptTo(QueryReportConfig.class);
-  }
+    private com.day.cq.search.Query prepareQueryBuilderQuery() throws ReportException {
 
-  @Override
-  public void setPage(int page) {
-    this.page = page;
-  }
+        Session session = getSession();
+        final Map<String, String> params = ParameterUtil.toMap(prepareStatement().split("\n\n?"), "=", false, null,
+                true);
+
+        return queryBuilder.createQuery(PredicateGroup.create(params),
+                session);
+    }
+
+    private Pair<Stream<Resource>, Long> getResultsFromQueryBuilder(
+            int limit, int offset) throws ReportException {
+
+        com.day.cq.search.Query query = prepareQueryBuilderQuery();
+        if (page != -1) {
+            log.debug("Fetching results with limit {} and offset {}", limit, offset);
+            query.setHitsPerPage(limit);
+            query.setStart(offset);
+        } else {
+            log.debug("Fetching all results");
+        }
+
+        SearchResult result = query.getResult();
+        Spliterator<Resource> spliterator = Spliterators.spliteratorUnknownSize(result.getResources(),
+                Spliterator.ORDERED | Spliterator.NONNULL);
+        long count = -1;
+        if (limit <= 100) {
+            count = result.getHits().size();
+        }
+        return ImmutablePair.of(StreamSupport.stream(spliterator, false), count);
+
+    }
+
+    private Pair<Stream<Resource>, Long> getResultsFromQuery(int limit, int offset)
+            throws RepositoryException, ReportException {
+        String statement = prepareStatement();
+        QueryManager queryMgr = getSession().getWorkspace().getQueryManager();
+        Query query = queryMgr.createQuery(statement, config.getQueryLanguage());
+
+        if (page != -1) {
+            log.debug("Fetching results with limit {} and offset {}", limit, offset);
+            query.setLimit(limit);
+            query.setOffset(offset);
+        } else {
+            log.debug("Fetching all results");
+        }
+        QueryResult result = query.execute();
+
+        NodeIterator nodes = result.getNodes();
+
+        Spliterator<Node> spliterator = Spliterators.spliteratorUnknownSize(nodes,
+                Spliterator.ORDERED | Spliterator.NONNULL);
+
+        ResourceResolver resolver = request.getResourceResolver();
+        return ImmutablePair.of(StreamSupport.stream(spliterator, false).map(n -> getResource(n, resolver)),
+                nodes.getSize());
+
+    }
+
+    @Override
+    public ResultsPage getAllResults() throws ReportException {
+        return fetchResults(Integer.MAX_VALUE, 0);
+    }
+
+    private void addQueryDetails(Map<String, String> details) throws ReportException {
+        try {
+            final QueryManager queryManager = getSession().getWorkspace()
+                    .getQueryManager();
+            final Query query = queryManager.createQuery("explain " + prepareStatement(), config.getQueryLanguage());
+            final QueryResult queryResult = query.execute();
+
+            final RowIterator rows = queryResult.getRows();
+            while (rows.hasNext()) {
+                final Row row = rows.nextRow();
+
+                String[] cols = queryResult.getColumnNames();
+                Value[] values = row.getValues();
+
+                for (int i = 0; i < cols.length; i++) {
+                    details.put(cols[i], values[i].getString());
+                }
+            }
+        } catch (RepositoryException re) {
+            throw new ReportException("Exception getting details", re);
+        }
+    }
+
+    private void addQueryBuilderDetails(Map<String, String> details) throws ReportException {
+        SearchResult result = prepareQueryBuilderQuery().getResult();
+        details.put("XPath Query", result.getQueryStatement());
+        details.put("Filtering Predicates", result.getFilteringPredicates());
+    }
+
+    @Override
+    public String getDetails() throws ReportException {
+        String statement = prepareStatement();
+        Map<String, String> details = new LinkedHashMap<>();
+        details.put("Language", config.getQueryLanguage());
+        details.put("Page", Integer.toString(page));
+        details.put("Page Size", Integer.toString(config.getPageSize()));
+        details.put("Query", statement);
+
+        if (isQueryBuilder()) {
+            addQueryBuilderDetails(details);
+        } else {
+            addQueryDetails(details);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Entry<String, String> entry : details.entrySet()) {
+            sb.append("<dt>" + StringEscapeUtils.escapeHtml(entry.getKey()) + "</dt>");
+            sb.append("<dd>" + StringEscapeUtils.escapeHtml(entry.getValue()) + "</dd>");
+        }
+
+        return "<dl>" + sb.toString() + "</dl>";
+    }
+
+    @Override
+    public String getParameters() throws ReportException {
+        List<String> params = new ArrayList<>();
+        Enumeration<String> keys = request.getParameterNames();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            for (String value : request.getParameterValues(key)) {
+                try {
+                    params.add(URLEncoder.encode(key, "UTF-8") + "=" + URLEncoder.encode(value, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    throw new ReportException("UTF-8 encoding available", e);
+                }
+            }
+        }
+        return StringUtils.join(params, "&");
+    }
+
+    private Resource getResource(Node node, ResourceResolver resolver) {
+        try {
+            return resolver.getResource(node.getPath());
+        } catch (RepositoryException e) {
+            log.warn("Failed to get path from node: {}", node, e);
+            return null;
+        }
+    }
+
+    @Override
+    public ResultsPage getResults() throws ReportException {
+        return fetchResults(config.getPageSize(), config.getPageSize() * page);
+    }
+
+    private String prepareStatement() throws ReportException {
+        try {
+            Map<String, String> parameters = getParamPatternMap(request);
+            Template template = new Handlebars().compileInline(config.getQuery());
+            String statement = template.apply(parameters);
+            log.trace("Loaded statement: {}", statement);
+            return statement;
+        } catch (IOException ioe) {
+            throw new ReportException("Exception templating query", ioe);
+        }
+    }
+
+    @Override
+    public void setConfiguration(Resource config) {
+        this.config = config.adaptTo(QueryReportConfig.class);
+    }
+
+    @Override
+    public void setPage(int page) {
+        this.page = page;
+    }
 
 }
---
+++ bundle/src/test/java/com/adobe/acs/commons/reports/models/QueryReportExecutorTest.java
@@ -0,0 +1,137 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2022 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.adobe.acs.commons.reports.models;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.adobe.acs.commons.reports.api.ReportException;
+import com.adobe.acs.commons.reports.api.ResultsPage;
+import com.day.cq.search.Query;
+import com.day.cq.search.QueryBuilder;
+import com.day.cq.search.result.SearchResult;
+
+import io.wcm.testing.mock.aem.junit.AemContext;
+
+@RunWith(Parameterized.class)
+public class QueryReportExecutorTest {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "SELECT * FROM [nt:unstructured] WHERE ISDESCENDANTNODE([/test])", "JCR-SQL2" },
+                { "/jcr:root/test//* order by @jcr:score", "xpath" },
+                { "path=/test", "queryBuilder" }
+        });
+    }
+
+    @Rule
+    public final AemContext context = new AemContext(ResourceResolverType.JCR_OAK);
+
+    private final String statement;
+    private final String language;
+
+    private QueryBuilder queryBuilder;
+
+    public QueryReportExecutorTest(String statement, String language) {
+        this.statement = statement;
+        this.language = language;
+    }
+
+    @Before
+    public void before() {
+        context.create().resource("/test",
+                Collections.singletonMap(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED));
+        context.create().resource("/test/item1",
+                Collections.singletonMap(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED));
+        context.create().resource("/test/item2",
+                Collections.singletonMap(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED));
+
+        queryBuilder = mock(QueryBuilder.class);
+        Query query = mock(Query.class);
+        SearchResult result = mock(SearchResult.class);
+
+        List<Resource> resources = new ArrayList<>();
+        resources.add(context.resourceResolver().getResource("/test/item1"));
+        resources.add(context.resourceResolver().getResource("/test/item2"));
+        when(result.getResources()).thenReturn(resources.iterator());
+
+        when(query.getResult()).thenReturn(result);
+        when(queryBuilder.createQuery(any(), any())).thenReturn(query);
+
+    }
+
+    private Resource createConfig(int pageSize, String query, String queryLanguage) {
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED);
+        properties.put("pageSize", pageSize);
+        properties.put("query", query);
+        properties.put("queryLanguage", queryLanguage);
+
+        return context.create().resource("/conf/" + UUID.randomUUID().toString(), properties);
+    }
+
+    @Test
+    public void supportsQueries() throws ReportException {
+        QueryReportExecutor executor = new QueryReportExecutor(context.request(), queryBuilder);
+        executor.setConfiguration(
+                createConfig(10, statement, language));
+        executor.setPage(0);
+
+        ResultsPage results = executor.getResults();
+        assertNotNull(results);
+    }
+
+    @Test
+    public void supportsDetails() throws ReportException {
+        QueryReportExecutor executor = new QueryReportExecutor(context.request(), queryBuilder);
+        executor.setConfiguration(
+                createConfig(10, statement, language));
+        executor.setPage(0);
+
+        String details = executor.getDetails();
+        assertNotNull(details);
+        assertTrue(details.contains(language));
+    }
+
+}
