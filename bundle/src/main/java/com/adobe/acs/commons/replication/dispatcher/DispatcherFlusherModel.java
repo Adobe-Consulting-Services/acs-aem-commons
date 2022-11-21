a24bfb9bb61b6c91ed37988989b1d243ba3e96a1
david g <davidjgonzalez@gmail.com>
2022-11-21 12:07:49 -0500

    Updated Dispatcher Flusher to use TouchUI

--- bundle/src/main/java/com/adobe/acs/commons/replication/dispatcher/DispatcherFlusherModel.java
+++ bundle/src/main/java/com/adobe/acs/commons/replication/dispatcher/DispatcherFlusherModel.java
@@ -1,2 +1,28 @@
-package com.adobe.acs.commons.replication.dispatcher;public class DispatcherFlusherModel {
+package com.adobe.acs.commons.replication.dispatcher;
+
+import com.day.cq.replication.Agent;
+import org.osgi.annotation.versioning.ProviderType;
+
+import java.util.Collection;
+import java.util.List;
+
+@ProviderType
+public interface DispatcherFlusherModel {
+
+    String getActionType();
+
+    Collection<String> getPaths();
+
+    Collection<Agent> getAgents();
+
+    boolean isFailure();
+
+    List<String> getResults();
+
+    /**
+     * @return true if the dispatcher flusher is ready to be used.
+     */
+    boolean isReady();
 }
+
+
