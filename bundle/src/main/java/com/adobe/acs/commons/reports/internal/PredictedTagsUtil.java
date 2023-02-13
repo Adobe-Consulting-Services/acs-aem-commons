ccf6601289bd9e940148b25683caf92c8b6992bc
Patrick Hupe <hupe@adobe.com>
2023-02-13 17:32:08 +0100

    style: code climate corrections

--- bundle/src/main/java/com/adobe/acs/commons/reports/internal/PredictedTagsUtil.java
+++ bundle/src/main/java/com/adobe/acs/commons/reports/internal/PredictedTagsUtil.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2023 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.adobe.acs.commons.reports.internal;
 
 import com.adobe.acs.commons.reports.models.PredictedTag;
@@ -12,7 +31,9 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PredictedTagsUtil {
 
@@ -115,8 +136,8 @@ public class PredictedTagsUtil {
         return lowerConfidenceThresholdValue != null
                 && !lowerConfidenceThresholdValue.isNaN()
                 && !lowerConfidenceThresholdValue.isInfinite()
-                && lowerConfidenceThresholdValue >= MINIMUM_LOWER_CONFIDENCE_THRESHOLD_VALUE ?
-                lowerConfidenceThresholdValue :
-                MINIMUM_LOWER_CONFIDENCE_THRESHOLD_VALUE;
+                && lowerConfidenceThresholdValue >= MINIMUM_LOWER_CONFIDENCE_THRESHOLD_VALUE
+                ? lowerConfidenceThresholdValue
+                : MINIMUM_LOWER_CONFIDENCE_THRESHOLD_VALUE;
     }
 }
