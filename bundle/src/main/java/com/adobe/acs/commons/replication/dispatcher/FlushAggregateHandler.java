5eb4fb5351ba1ef091fe72aa8a985e88867c077b
david g <davidjgonzalez@users.noreply.github.com>
2023-04-21 09:06:01 -0400

    Update FlushAggregateHandler.java

--- bundle/src/main/java/com/adobe/acs/commons/replication/dispatcher/FlushAggregateHandler.java
+++ bundle/src/main/java/com/adobe/acs/commons/replication/dispatcher/FlushAggregateHandler.java
@@ -1,7 +1,20 @@
 /*
- * work-around for ACS AEM Commons issue 3045
+ * ACS AEM Commons
+ *
+ * Copyright (C) 2013 - 2023 Adobe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.adobe.acs.commons.replication.dispatcher;
 
 import com.day.cq.replication.AggregateHandler;
@@ -22,6 +35,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Implements an aggregate handler that detects hierarchy nodes as aggregate root.
+ * Work-around for ACS AEM Commons issue #3045.
  */
 public class FlushAggregateHandler implements AggregateHandler 
 {
@@ -71,5 +85,4 @@ public class FlushAggregateHandler implements AggregateHandler
 		throws ReplicationException 
 	{
     }
-
-} // public class FlushAggregateHandler implements AggregateHandler 
+}
