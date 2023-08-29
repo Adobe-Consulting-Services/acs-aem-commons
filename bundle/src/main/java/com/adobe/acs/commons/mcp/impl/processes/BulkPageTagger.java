989d8df3c44d64561d001fac820fdef389bf060d
Harwinder Singh <adobe57138@gmail.com>
2023-08-29 14:59:24 -0400

    Added support for multiline tags input along with semicolon tags separator

--- bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/BulkPageTagger.java
+++ bundle/src/main/java/com/adobe/acs/commons/mcp/impl/processes/BulkPageTagger.java
@@ -17,23 +17,6 @@
  * limitations under the License.
  * #L%
  */
-/*
- * ACS AEM Commons
- *
- * Copyright (C) 2013 - 2023 Adobe
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.adobe.acs.commons.mcp.impl.processes;
 
 import com.adobe.acs.commons.fam.ActionManager;
@@ -68,6 +51,7 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -174,7 +158,9 @@ public class BulkPageTagger extends ProcessDefinition implements Serializable {
                         String[] tagIds = Stream.of(existingPageTags)
                                 .map(Tag::getTagID)
                                 .toArray(String[]::new);
-                        Set<String> updatedTags = new HashSet<>(Arrays.asList(value.split(";")));
+                        Set<String> updatedTags = Arrays.stream(value.split("[;\n]"))
+                                .map(String::trim)
+                                .collect(Collectors.toSet());
                         updatedTags.addAll(Arrays.asList(tagIds));
                         String[] updatedTagsArray = updatedTags.stream().toArray(String[]::new);
 
