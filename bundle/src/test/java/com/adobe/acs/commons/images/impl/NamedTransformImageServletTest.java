c6f5e542d88a5aae2233d2941beed27d7fa308e3
aretakia <149517484+aretakia@users.noreply.github.com>
2024-03-20 20:46:59 +0000

    Change Matchers import

--- bundle/src/test/java/com/adobe/acs/commons/images/impl/NamedTransformImageServletTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/images/impl/NamedTransformImageServletTest.java
@@ -21,7 +21,7 @@ package com.adobe.acs.commons.images.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
