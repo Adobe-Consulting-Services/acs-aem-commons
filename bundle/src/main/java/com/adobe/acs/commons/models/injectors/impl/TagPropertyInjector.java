55ef26c858309f88a2b0cc880b3dd095c7c7b019
Niek Raaijmakers <raaijmak@adobe.com>
2024-03-30 08:49:15 +0100

    Add tag injector

--- bundle/src/main/java/com/adobe/acs/commons/models/injectors/impl/TagPropertyInjector.java
+++ bundle/src/main/java/com/adobe/acs/commons/models/injectors/impl/TagPropertyInjector.java
@@ -19,17 +19,32 @@
  */
 package com.adobe.acs.commons.models.injectors.impl;
 
-import com.adobe.acs.commons.models.injectors.annotation.JsonValueMapValue;
 import com.adobe.acs.commons.models.injectors.annotation.TagProperty;
+import com.adobe.acs.commons.util.impl.ReflectionUtil;
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.inherit.InheritanceValueMap;
+import com.day.cq.tagging.Tag;
+import com.day.cq.tagging.TagManager;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.spi.DisposalCallbackRegistry;
 import org.apache.sling.models.spi.Injector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static com.adobe.acs.commons.models.injectors.impl.InjectorUtils.getResource;
+import static com.adobe.acs.commons.models.injectors.impl.InjectorUtils.getResourceResolver;
+import static org.apache.commons.lang3.StringUtils.defaultIfEmpty;
 
 @Component(
         service = {Injector.class},
@@ -38,6 +53,9 @@ import java.lang.reflect.Type;
         }
 )
 public class TagPropertyInjector implements Injector {
+
+    private static final Logger logger = LoggerFactory.getLogger(TagPropertyInjector.class);
+
     @NotNull
     @Override
     public String getName() {
@@ -50,9 +68,47 @@ public class TagPropertyInjector implements Injector {
 
         if (element.isAnnotationPresent(TagProperty.class)) {
 
-            // only inject directly
+            // only inject directly. @ValueMapValue will handle the rest
             TagProperty annotation = element.getAnnotation(TagProperty.class);
-             return annotation.value();
+
+            ResourceResolver resourceResolver = getResourceResolver(adaptable);
+
+            if(resourceResolver == null) {
+                logger.error("ResourceResolver is null, cannot inject tag property. Are you adapting from a resource or SlingHttpServletRequest?");
+                return null;
+            }
+
+            TagManager tagManager = resourceResolver.adaptTo(TagManager.class);
+            if(tagManager == null) {
+                logger.error("TagManager is null, cannot inject tag property. Are you adapting from a resource or SlingHttpServletRequest?");
+                return null;
+            }
+            String key =  defaultIfEmpty(annotation.value(), name);;
+            final String[] tagKeys;
+            if(annotation.inherit()){
+                InheritanceValueMap inheritanceValueMap = new HierarchyNodeInheritanceValueMap(getResource(adaptable));
+                tagKeys = inheritanceValueMap.getInherited(key, String[].class);
+            }else{
+                tagKeys = getResource(adaptable).getValueMap().get(key, String[].class);
+            }
+            if(tagKeys == null || tagKeys.length == 0){
+                return null;
+            }
+
+            final Stream<Tag> tagStream = Arrays.stream(tagKeys).map(tagManager::resolve);
+
+            if(ReflectionUtil.isSetType(declaredType) && ReflectionUtil.getGenericParameter(declaredType).equals(Tag.class)){
+                return tagStream.collect(Collectors.toSet());
+            }else if((ReflectionUtil.isListType(declaredType) || ReflectionUtil.isCollectionType(declaredType)) && ReflectionUtil.getGenericParameter(declaredType).equals(Tag.class)){
+                return tagStream.collect(Collectors.toList());
+            }else if(ReflectionUtil.isArray(declaredType) && ReflectionUtil.getClassOrGenericParam(declaredType).equals(Tag.class)) {
+                // array
+                return tagStream.toArray(Tag[]::new);
+            }else{
+                // single value, resolve first tag
+                return tagStream.filter(Objects::nonNull).findFirst().orElse(null);
+            }
+
         }
 
         return null;
