4105c3ce5c4081c7aca7a95531f0f18b0ef8328c
Niek Raaijmakers <niekraaijmakers@gmail.com>
2024-04-02 22:53:36 +0200

    Content policy injector (#3305)
    * Add content policy injector / via provider
    
    ---------
    
    Co-authored-by: Niek Raaijmakers <raaijmak@adobe.com>
    Co-authored-by: david g <davidjgonzalez@users.noreply.github.com>

---
+++ bundle/src/main/java/com/adobe/acs/commons/models/injectors/annotation/ContentPolicyValue.java
@@ -0,0 +1,53 @@
+/*-
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 - 2024 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.adobe.acs.commons.models.injectors.annotation;
+
+import org.apache.sling.models.annotations.Source;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.spi.injectorspecific.InjectAnnotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@InjectAnnotation
+@Source(ContentPolicyValue.SOURCE)
+public @interface ContentPolicyValue {
+
+    /**
+     * Source value used for this annotation.
+     * @see Source
+     */
+    String SOURCE = "design";
+
+    String name() default "";
+
+    /**
+     * Whether to use the page policy (true) or the component policy (false).
+     * @return
+     */
+    boolean usePagePolicy() default false;
+
+    InjectionStrategy injectionStrategy() default InjectionStrategy.DEFAULT;
+
+}
---
+++ bundle/src/main/java/com/adobe/acs/commons/models/injectors/impl/ContentPolicyValueInjector.java
@@ -0,0 +1,75 @@
+/*-
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2013 - 2024 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.adobe.acs.commons.models.injectors.impl;
+
+import com.adobe.acs.commons.models.injectors.annotation.ContentPolicyValue;
+import com.adobe.acs.commons.util.impl.ReflectionUtil;
+import com.day.cq.wcm.api.designer.Designer;
+import com.day.cq.wcm.api.designer.Style;
+import com.day.cq.wcm.api.policies.ContentPolicy;
+import com.day.cq.wcm.api.policies.ContentPolicyManager;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.scripting.SlingBindings;
+import org.apache.sling.models.spi.DisposalCallbackRegistry;
+import org.apache.sling.models.spi.Injector;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Type;
+
+import static com.adobe.acs.commons.models.injectors.impl.InjectorUtils.*;
+import static com.adobe.acs.commons.util.impl.ReflectionUtil.convertValueMapValue;
+
+@Component(
+        property = {
+                Constants.SERVICE_RANKING + ":Integer=5500"
+        },
+        service = Injector.class
+)
+public class ContentPolicyValueInjector  implements Injector {
+
+    @NotNull
+    @Override
+    public String getName() {
+        return ContentPolicyValue.SOURCE;
+    }
+
+    @Nullable
+    @Override
+    public Object getValue(Object adaptable, String name, Type declaredType, AnnotatedElement element,
+                           DisposalCallbackRegistry callbackRegistry) {
+        ContentPolicy policy = getContentPolicy(adaptable);
+
+        if(policy != null){
+            return convertValueMapValue(policy.getProperties(), name, declaredType);
+        }
+
+        return null;
+    }
+
+
+}
