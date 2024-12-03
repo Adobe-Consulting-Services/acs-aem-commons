3cad1f52ca847efc8b3e59b1cf61223fa3a351e7
Krassimir Boyanov <kboyanov@linkedin.com>
2024-12-03 11:11:43 -0800

    Fixing Qlty plugin issues - comments on the new unit test and decalaration of valiable too far form usage.

--- bundle/src/test/java/com/adobe/acs/commons/wcm/impl/PublishUrlServletTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/wcm/impl/PublishUrlServletTest.java
@@ -1,3 +1,23 @@
+/*-
+ * #%L
+ * ACS AEM Commons Bundle
+ * %%
+ * Copyright (C) 2024 Adobe
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 package com.adobe.acs.commons.wcm.impl;
 
 import com.day.cq.commons.Externalizer;
@@ -37,7 +57,6 @@ class PublishUrlServletTest {
     @Test
     void testDoGet() throws IOException {
         context.registerAdapter(ResourceResolver.class, Externalizer.class, externalizer);
-        MockSlingHttpServletResponse response = context.response();
         MockSlingHttpServletRequest request = context.request();
         Map<String, Object> requestParams = new HashMap<>();
         requestParams.put("path", "/content/we-retail/us/en/experience");
@@ -49,6 +68,7 @@ class PublishUrlServletTest {
                 .thenReturn("https://aem.publish.someorganization.com/content/we-retail/us/en/experience.html")
                 .thenReturn("https://www.someorganization.com/experience");
         publishUrlServlet.activate(config);
+        MockSlingHttpServletResponse response = context.response();
         publishUrlServlet.doGet(request, response);
         try(InputStream inputStream = getClass().getResourceAsStream("PublishUrlServletResponse.json")) {
             assert inputStream != null;
