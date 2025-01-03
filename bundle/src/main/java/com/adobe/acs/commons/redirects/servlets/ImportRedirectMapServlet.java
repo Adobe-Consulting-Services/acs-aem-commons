9c2bae447ff7d37741d3dd4528548e858945f00d
Yegor Kozlov <yegor.kozlov@gmail.com>
2025-01-03 14:25:34 +0100

    unused imports

--- bundle/src/main/java/com/adobe/acs/commons/redirects/servlets/ImportRedirectMapServlet.java
+++ bundle/src/main/java/com/adobe/acs/commons/redirects/servlets/ImportRedirectMapServlet.java
@@ -58,7 +58,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 import java.util.LinkedHashMap;
 import java.util.Calendar;
 import java.util.HashSet;
