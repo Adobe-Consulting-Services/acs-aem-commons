9df4c7535c2c72c63f2518c75e52c04b7a39a13b
srma4tech <srmag1295@gmail.com>
2026-07-13 18:59:13 +0530

    Fix shared component provider strict Mockito tests

--- bundle/src/test/java/com/adobe/acs/commons/wcm/properties/shared/impl/SharedComponentPropertiesBindingsValuesProviderTest.java
+++ bundle/src/test/java/com/adobe/acs/commons/wcm/properties/shared/impl/SharedComponentPropertiesBindingsValuesProviderTest.java
@@ -17,94 +17,78 @@
  */
 package com.adobe.acs.commons.wcm.properties.shared.impl;
 
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 import javax.script.Bindings;
 import javax.script.SimpleBindings;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.scripting.SlingBindings;
-import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import com.adobe.acs.commons.wcm.PageRootProvider;
+import com.adobe.acs.commons.testing.PrivateAccessor;
 import com.adobe.acs.commons.wcm.properties.shared.SharedComponentProperties;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SharedComponentPropertiesBindingsValuesProviderTest {
 
-    private PageRootProvider pageRootProvider;
     private Resource resource;
-    private Resource sharedPropsResource;
-    private Resource globalPropsResource;
     private SlingHttpServletRequest request;
     private Bindings bindings;
-    private ResourceResolver resourceResolver;
-    private ValueMap sharedProps;
-    private ValueMap globalProps;
+    private SharedComponentProperties sharedComponentProperties;
 
     @Before
     public void setUp() {
 
         resource = mock(Resource.class);
-        pageRootProvider = mock(PageRootProvider.class);
         bindings = new SimpleBindings();
-        sharedPropsResource = mock(Resource.class);
-        globalPropsResource = mock(Resource.class);
-        resourceResolver = mock(ResourceResolver.class);
         request = mock(SlingHttpServletRequest.class);
+        sharedComponentProperties = mock(SharedComponentProperties.class);
 
         bindings.put(SlingBindings.REQUEST, request);
         bindings.put(SlingBindings.RESOURCE, resource);
 
-        when(resource.getResourceResolver()).thenReturn(resourceResolver);
-        when(resource.getPath()).thenReturn("/content/test");
-        when(pageRootProvider.getRootPagePath(anyString()))
-                .thenReturn("/content/test");
-
-        sharedProps = new ValueMapDecorator(new HashMap<>());
-        globalProps = new ValueMapDecorator(new HashMap<>());
-
-        when(globalPropsResource.getValueMap()).thenReturn(globalProps);
-        when(sharedPropsResource.getValueMap()).thenReturn(sharedProps);
         when(resource.getValueMap()).thenReturn(ValueMap.EMPTY);
     }
 
     @Test
-    public void testDisabled() {
+    public void testDisabled() throws Exception {
 
         SharedComponentPropertiesBindingsValuesProvider provider =
                 new SharedComponentPropertiesBindingsValuesProvider();
 
+        PrivateAccessor.setField(provider, "sharedComponentProperties", sharedComponentProperties);
         activate(provider, false);
 
         provider.addBindings(bindings);
 
+        verifyNoInteractions(sharedComponentProperties);
         assertEquals(ValueMap.EMPTY,
                 bindings.get(SharedComponentProperties.GLOBAL_PROPERTIES));
     }
 
     @Test
-    public void testEnabled() {
+    public void testEnabled() throws Exception {
 
         SharedComponentPropertiesBindingsValuesProvider provider =
                 new SharedComponentPropertiesBindingsValuesProvider();
 
+        PrivateAccessor.setField(provider, "sharedComponentProperties", sharedComponentProperties);
         activate(provider, true);
 
         provider.addBindings(bindings);
 
+        verify(sharedComponentProperties).getSharedPropertiesPagePath(resource);
         assertNotNull(bindings.get(SharedComponentProperties.GLOBAL_PROPERTIES));
     }
 
@@ -120,13 +104,4 @@ public class SharedComponentPropertiesBindingsValuesProviderTest {
         provider.activate(config);
     }
 
-    private void setField(Object target, String fieldName, Object value) {
-        try {
-            Field field = target.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set(target, value);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
